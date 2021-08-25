1e08c4792b14a779a5ef686d7c9e987aa7285611
gnii <chochain@yahoo.com>
2021-08-25 16:39:05 -0700

    ~/src/cuef.cu implemente CueForth class

--- src/cuef.cu
+++ src/cuef.cu
@@ -2,37 +2,56 @@
   @brief
   cueForth value definitions non-optimized
 */
-#include <stdio.h>
 #include "cuef.h"
 
 // forward declaration for implementation
-extern "C" __GPU__  void cuef_mmu_init(void *ptr, U32 sz);
-extern "C" __GPU__  void cuef_core_init(void);
-extern "C" __GPU__  void cuef_console_init(U8 *buf, U32 sz);
+extern "C" __KERN__ void mmu_init(void *ptr, U32 sz);
+extern "C" __KERN__ void eforth_init(U8 *cin, U8 *cout);
 
-CuefSes *_ses_list = NULL; 	  // session linked-list
+__KERN__ void eforth_init(U8 *cin, U8 *cout) {
+	//if (threadId.x!=0 || blockId.x!=0) return;
+    //string cmd;
+	return;
+}
+
+CueForth::CueForth(istream &in, ostream &out) : cin(in), cout(out) {}
+
+__HOST__ void*
+CueForth::_malloc(int sz, int type)
+{
+	void *mem;
+
+	// TODO: to add texture memory
+	switch (type) {
+	case 0: 	cudaMalloc(&mem, sz); break;			// allocate device memory
+	default: 	cudaMallocManaged(&mem, sz);			// managed (i.e. paged) memory
+	}
+    if (cudaSuccess != cudaGetLastError()) return NULL;
+
+    return mem;
+}
+
+__HOST__ void
+CueForth::_free(void *mem) {
+	cudaFree(mem);
+}
 
 __HOST__ int
-cuef_setup(int step, int trace)
+CueForth::setup(int step, int trace)
 {
 	cudaDeviceReset();
 
-	debug_init(trace);												// initialize logger
-	debug_log("cueF initializing...");
+	PRINTF("cueForth initializing...");
 
-	U8 *mem = cuef_host_heap = (U8*)cuda_malloc(CUEF_HEAP_SIZE, 1);	// allocate main block (i.e. RAM)
-	if (!mem) 				return -11;
-	U8 *out = _cuef_out = (U8*)cuda_malloc(OUTPUT_BUF_SIZE, 1);		// allocate output buffer
-	if (!_cuef_out) 		return -12;
-	if (vm_pool_init(step)) return -13;								// allocate VM pool
+	//U8 *mem  = cuef_host_heap = (U8*)cuda_malloc(CUEF_HEAP_SIZE, 1);	// allocate main block (i.e. RAM)
+	//if (!mem)  return -10;
+	ibuf = (U8*)_malloc(CUEF_IBUF_SIZE, 1);					// allocate main block (i.e. RAM)
+	if (!ibuf)  return -11;
+	obuf = (U8*)_malloc(CUEF_OBUF_SIZE, 1);					// allocate output buffer
+	if (!obuf) return -12;
 
-	_ses_list = NULL;
-
-	cuef_mmu_init<<<1,1>>>(mem, CUEF_HEAP_SIZE);			// setup memory management
-	cuef_core_init<<<1,1>>>();								// setup basic classes	(TODO: => ROM)
-#if CUEF_USE_CONSOLE
-	cuef_console_init<<<1,1>>>(out, OUTPUT_BUF_SIZE);		// initialize output buffer
-#endif
+	//mmu_init<<<1,1>>>(mem, CUEF_HEAP_SIZE);				// setup memory management
+	eforth_init<<<1,1>>>(ibuf, obuf);						// setup basic classes	(TODO: => ROM)
 	GPU_SYNC();
 
     U32 sz0, sz1;
@@ -40,60 +59,28 @@ cuef_setup(int step, int trace)
 	cudaDeviceSetLimit(cudaLimitStackSize, (size_t)sz0*4);
 	cudaDeviceGetLimit((size_t *)&sz1, cudaLimitStackSize);
 
-	debug_log("cuef initialized, ready to go...");
+	PRINTF("cueForth initialized, ready to go...");
 
-	return 0;
+    return 0;
 }
 
 __HOST__ int
-cuef_load(char *rite_name)
+CueForth::run()
 {
-	CuefSession ses = new CuefSession(cin, cout);
-
-	char *ins = _fetch_bytecode(rite_name);
-	if (!ins) return -22;		// bytecode memory allocation error
-
-	int id = ses->id = vm_get(ins);
-	cuda_free(ins);
-
-	if (id<0) return id;
-
-	ses->next = _ses_list;		// add to linked-list
-	_ses_list = ses;
-
-	return 0;
-}
-
-__HOST__ int
-cuef_run()
-{
-	debug_log("cuef session starting...");
-	debug_mmu_stat();
-
-	// parse BITE code into each vm
-	// TODO: work producer (enqueue)
-	for (cuef_ses *ses=_ses_list; ses!=NULL; ses=ses->next) {
-		int id = vm_ready(ses->id);
-		if (id) return id;
-	}
+	PRINTF("cuef session starting...");
 	// kick up main loop until all VM are done
-	vm_main_start();
+    string idiom;
+	while (cin >> idiom) {
+        printf("%s=>", idiom.c_str());
+    }
 
-	debug_mmu_stat();
-	debug_log("cuef session completed.");
+	PRINTF("cuef session completed.");
 
 	return 0;
 }
 
 __HOST__ void
-cuef_teardown(int sig)
+CueForth::teardown(int sig)
 {
 	cudaDeviceReset();
-
-	cuef_ses *tmp, *ses = _ses_list;
-	while (ses) {
-		tmp = ses;
-		ses = ses->next;
-		free(tmp);
-	}
 }
