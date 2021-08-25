94404d2374ed9951f2d1d0dfac8c7c3322c8f764
gnii <chochain@yahoo.com>
2021-08-25 16:38:29 -0700

    ~/src/cuef.h add CueForth class

--- src/cuef.h
+++ src/cuef.h
@@ -4,30 +4,24 @@
 */
 #ifndef CUEF_SRC_CUEF_H_
 #define CUEF_SRC_CUEF_H_
-#include <stdint.h>
+//#include <cstdio>
+//#include <cstdint>
+#include <cuda.h>
+#include <sstream>
 #include "cuef_config.h"
 
-#if CUEF_USE_CONSOLE		// use guru local implemented print functions (in puts, sprintf.cu)
-#define PRINTF				cuef_printf
-#else						// use CUDA printf function
-#include <stdio.h>
-#define PRINTF				printf
-#endif // CUEF_USE_CONSOLE
-#define NA(msg)				({ PRINTF("method not supported: %s\n", msg); })
+using namespace std;
 
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
+#define PRINTF				printf
+#define NA(msg)				({ PRINTF("method not supported: %s\n", msg); })
 
 #if defined(__CUDACC__)
 
-#define __CUEF__ 			__device__
+#define __GPU__ 			__device__
 #define __HOST__			__host__
-#define __GPU__				__global__
-//#define __INLINE__
+#define __KERN__			__global__
 #define __INLINE__			__forceinline__
-#define __UCODE__ 			__CUEF__ void
-#define __CFUNC__			__CUEF__ void
+    
 #define MUTEX_LOCK(p)  		while (atomicCAS((int *)&p, 0, 1)!=0)
 #define MUTEX_FREE(p)  		atomicExch((int *)&p, 0)
 
@@ -41,33 +35,28 @@ extern "C" {
 
 #else  // defined(__CUDACC__)
 
-#define __CUEF__
-#define __INLINE__ 			inline
-#define __HOST__
 #define __GPU__
+#define __KERN__
+#define __HOST__
+#define __INLINE__ 			inline
 #define ALIGN(sz) 			((sz) + (-(sz) & 3))
 #define ASSERT(X) 			assert(x)
 
 #endif // defined(__CUDACC__)
 
-#define GT_BOOL(v)		((v) ? GT_TRUE : GT_FALSE)
-
 // short notation
-typedef uint64_t    U64;
+typedef uint64_t    U64;                    // unsigned integers
 typedef uint32_t	U32;
 typedef uint16_t    U16;
 typedef uint8_t		U8;
 
-typedef int32_t     S32;					// signed integer
+typedef int32_t     S32;					// signed integers
 typedef int16_t		S16;
 typedef int8_t		S8;
-typedef uintptr_t   U32A;					// pointer address
 
+typedef uintptr_t   U32A;					// pointer address
 typedef double		F64;					// double precision float
 typedef float       F32;					// single precision float
-
-typedef uint32_t    U32;
-typedef uint8_t     U8;
 
 //===============================================================================
 // cueForth simple types (non struct)
@@ -76,16 +65,24 @@ typedef F32	 		GF;						// float
 typedef U16			GS;						// symbol index
 typedef S32			GP;						// offset, i.e. object pointer
 
-#ifdef __cplusplus
-}
-#endif // __cplusplus
+// pointer arithmetic, this will not work in multiple segment implementation
+#define U8PADD(p, n)	((U8*)(p) + (n))	// add
+#define U8PSUB(p, n)	((U8*)(p) - (n))	// sub
 
-class CuefSession {			    // 16-byte
-	istringstream	&stdin;		// input stream
-	ostringstream	&stdout;	// output stream
-	U16 			id;
-	U16 			trace;
-	struct RSes 	*next;
+class CueForth {
+	istream &cin;
+	ostream &cout;
+
+	U8 *ibuf;
+	U8 *obuf;
+
+    __HOST__ void* _malloc(int sz, int type);
+    __HOST__ void  _free(void *mem);
+
+public:
+    CueForth(istream &in, ostream &out);
+    __HOST__ int   setup(int set, int trace);
+    __HOST__ int   run();
+    __HOST__ void  teardown(int sig);
 };
-
 #endif // CUEF_SRC_CUEF_H_
