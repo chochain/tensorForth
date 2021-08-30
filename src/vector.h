78b331f54047527d4a20b09a412cde1a28391f76
gnii <chochain@yahoo.com>
2021-08-30 11:22:59 -0700

    unit test for ~/src/vector.h

--- src/vector.h
+++ src/vector.h
@@ -8,48 +8,47 @@ namespace cuef {
 ///
 template<class T>
 struct vector {
-    T *v;               /// use proxy pattern
-    int n  =0;          /// number of elements stored
-    int sz =0;          /// allocated size
+    T   *_v;            /// use proxy pattern
+    int _n  =0;         /// number of elements stored
+    int _sz =0;         /// allocated size
 
-    __device__ ~vector() { if (v) free(v); }
+    __GPU__ ~vector() { if (_v) free(_v); }
     //
     // operator overloading
     //
-    __device__ T&      operator[](int i) { return i < 0 ? v[n + i] : v[i]; }
-    __device__ vector& operator+=(vector& a) {
-    	for (int i=0; i<a.size(); i++) push(a[i]);
-    	return *this;
-    }
-    __device__ vector& merge(T *a, int len) {
-    	for (int i=0; i<len; i++) push(*a++);
-    	return *this;
-    }
-    __device__ vector& merge(vector<T> a) {
-    	for (int i=0; i<a.n; i++) push(a[i]);
-    	return *this;
-    }
-
-    __device__ int     size() { return n; }
-    __device__ vector& push(T t) {
-        if ((n+1) > sz) resize(n + 4);
-        v[n++] = t;
+    __GPU__ T&      operator[](int i) { return i < 0 ? _v[_n + i] : _v[i]; }
+    __GPU__ vector& merge(T *a, int len) {
+        for (int i=0; i<len; i++) push(a[i]);
         return *this;
     }
-    __device__ T& pop()   { return n>0 ? v[--n] : v[0]; }
-    __device__ T  dec_i() { return v[n - 1] -= 1; }    /// decrement stack top
-    __device__ vector& clear(int i=0)  { if (i<n) n = i; return *this; }
-    __device__ vector& resize(int nsz) {
-        int x = 0;
-        if      (nsz > sz) x = ALIGN4(nsz);     // need bigger?
-        else if (n >= sz)  x = ALIGN4(n + 4);	// auto allocate extra 4 elements
-        if (x==0) return *this; 			    // no resizing needed
+    __GPU__ vector& merge(vector<T>& a) {
+        for (int i=0; i<a.size(); i++) push(a[i]);
+        return *this;
+    }
+    __GPU__ vector& operator+=(vector<T>& a) { merge(a); return *this; }
 
-        T *nv = (T*)malloc(sizeof(T)*x);	    // allocate new block of memory
-        MEMCPY(nv, v, sizeof(T)*sz);    	    // deep copy
-        free(v);
-        v  = nv;
-        sz = x;
+    __GPU__ int     size() { return _n; }
+    __GPU__ vector& push(T t) {
+        if ((_n+1) > _sz) resize(_n + 4);
+        _v[_n++] = t;
+        return *this;
+    }
+    __GPU__ T&  pop()  { return _n>0 ? _v[--_n] : _v[0]; }
+    __GPU__ T&  dec_i() { return _v[_n - 1] -= 1; } /// decrement stack top
+    __GPU__ vector& clear(int i=0)  { if (i<_n) _n = i; return *this; }
+    __GPU__ vector& resize(int nsz) {
+        int x = 0;
+        if      (nsz >  _sz) x = ALIGN4(nsz);      // need bigger?
+        else if (_n >= _sz)  x = ALIGN4(_n + 4);   // auto allocate extra 4 elements
+        if (x==0) return *this;                    // no resizing needed
+
+        T *nv = (T*)malloc(sizeof(T)*x);           // allocate new block of memory
+        if (_v) {
+            memcpy(nv, _v, sizeof(T)*_n);          // deep copy
+            free(_v);
+        }
+        _v  = nv;
+        _sz = x;
         return *this;
     }
 };
---
+++ tests/test_vector.cpp
@@ -0,0 +1,124 @@
+///
+/// Unit Test - cueForth vector
+/// 
+///> g++ -std=c++14 -Wall -L/home/gnii/devel/catch2 -lcatch test_vector.cpp && a.out
+///
+#include <sstream>
+#define  CATCH_CONFIG_MAIN
+#include "../../../catch2/catch.hpp"
+
+#define __GPU__
+#include "../src/vector.h"
+
+using namespace cuef;
+
+TEST_CASE("vector class")
+{
+    auto dump = [](vector<int>& a) {
+        std::stringstream ss("");
+        for (int i=0; i<a.size(); i++) ss << a[i] << '|';
+        return ss.str();
+    };
+    int aa[] = { 111, 222, 333, 444, 555, 666 };
+    int nn = sizeof(aa)/sizeof(int);
+    std::string rst[nn+1] = {
+        "",
+        "111|",
+        "111|222|",
+        "111|222|333|",
+        "111|222|333|444|",
+        "111|222|333|444|555|",
+        "111|222|333|444|555|666|"
+    };
+    SECTION("constructor/destructor/resize") {
+        vector<int> v1;
+        REQUIRE(v1._n==0);
+        REQUIRE(v1._sz==0);
+        REQUIRE(v1.size()==0);
+        v1.resize(4);
+        REQUIRE(v1._n==0);
+        REQUIRE(v1._sz==4);
+        REQUIRE(v1.size()==0);
+    }
+    SECTION("push/pop") {
+        vector<int> v1;
+        v1.push(aa[0]);
+        REQUIRE(v1._n==1);
+        REQUIRE(v1._sz==4);
+        REQUIRE(v1.size()==1);
+        REQUIRE(dump(v1)==rst[1]);
+        int x = v1.pop();
+        REQUIRE(v1._n==0);
+        REQUIRE(v1._sz==4);
+        REQUIRE(v1.size()==0);
+        REQUIRE(x==aa[0]);
+        REQUIRE(dump(v1)==rst[0]);
+    }
+    SECTION("multiple pushs") {
+        vector<int> v1;
+        for (int i=0, j=1; i<nn; i++, j++) {
+            v1.push(aa[i]);
+            REQUIRE(v1._n==j);
+            REQUIRE(v1._sz==4*((i/4)+1));
+            REQUIRE(v1.size()==j);
+            REQUIRE(dump(v1)==rst[j]);
+        }
+    }
+    SECTION("concate pushs/pops") {
+        vector<int> v1;
+        v1.push(aa[0]).push(aa[1]).push(aa[2]).push(aa[3]).push(aa[4]).push(aa[5]);
+        REQUIRE(v1._n==6);
+        REQUIRE(v1._sz==8);
+        REQUIRE(v1.size()==6);
+        REQUIRE(dump(v1)==rst[6]);
+        int x;
+        for (int i=nn-1; i>=0; i--) {
+            x = v1.pop();
+            REQUIRE(x==aa[i]);
+            REQUIRE(v1._n==i);
+            REQUIRE(v1._sz==8);
+            REQUIRE(v1.size()==i);
+            REQUIRE(dump(v1)==rst[i]);
+        }
+    }
+    SECTION("merge, +=") {
+        vector<int> v1;
+        v1.merge(aa, nn);
+        REQUIRE(v1._sz==8);
+        REQUIRE(v1.size()==nn);
+        REQUIRE(dump(v1)==rst[nn]);
+        vector<int> v2;
+        v2.merge(v1);
+        REQUIRE(v2._sz==8);
+        REQUIRE(v2.size()==nn);
+        REQUIRE(dump(v2)==rst[nn]);
+        vector<int> v3;
+        v3 += v1;
+        REQUIRE(v3._sz==8);
+        REQUIRE(v3.size()==nn);
+        REQUIRE(dump(v3)==rst[nn]);
+    }
+    SECTION("dec_i") {
+        vector<int> v1;
+        std::string r("111|222|333|444|555|665|");
+        v1.merge(aa, nn);
+        int x = v1.dec_i();
+        REQUIRE(x==665);
+        REQUIRE(v1._sz==8);
+        REQUIRE(v1.size()==nn);
+        REQUIRE(dump(v1)==r);
+    }
+    SECTION("clear") {
+        vector<int> v1;
+        std::string r("111|222|333|");
+        v1.merge(aa, nn);
+        v1.clear(3);
+        REQUIRE(v1._sz==8);
+        REQUIRE(v1.size()==3);
+        REQUIRE(dump(v1)==rst[3]);
+        v1.clear();
+        REQUIRE(v1._sz==8);
+        REQUIRE(v1.size()==0);
+        REQUIRE(dump(v1)==rst[0]);
+    }
+}
