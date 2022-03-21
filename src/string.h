2f67a684159a9c767cfc5c24f7f7142e0328cde6
gnii <chochain@yahoo.com>
2022-03-20 19:32:37 -0700

    ~/src/string rename string class to StrBuf to avoid collision with STD

--- src/string.h
+++ src/string.h
@@ -1,72 +1,68 @@
-#ifndef __EFORTH_SRC_STRING_H
-#define __EFORTH_SRC_STRING_H
+#ifndef __EFORTH_SRC_STRBUF_H
+#define __EFORTH_SRC_STRBUF_H
 #include "vector.h"
 #include <stdio.h>
 
 #define CUEF_FLOAT_PRECISION  1000000     /* 6-digit */
-#define STRING_BUF_SIZE       8
-
-namespace cuef {
+#define STRBUF_SIZE           8
 ///
-/// stringbuf class
+/// string buffer class
 ///
-struct string : public vector<char>
+struct StrBuf : public Vector<char>
 {
     ///
     /// constructors
     ///
-	__GPU__ string(int asz=STRING_BUF_SIZE) {
-		if (asz>0) _v = new char[_sz=ALIGN4(asz)];
-	}
-    __GPU__ string(const char *s) {
+    __GPU__ StrBuf(int asz=STRBUF_SIZE) {
+        if (asz>0) _v = new char[_sz=ALIGN4(asz)];
+    }
+    __GPU__ StrBuf(const char *s) {
         _n = STRLENB(s);
         _v = new char[_sz=ALIGN4(_n+1)];
         MEMCPY(_v, s, _n);
     }
-    __GPU__ string(string& s) {
+    __GPU__ StrBuf(StrBuf& s) {
         _n = s._n;
         _v = new char[_sz=ALIGN4(_n+1)];
         MEMCPY(_v, s.c_str(), _n);
     }
     ///
-    /// string export
+    /// StrBuf export
     ///
-    __GPU__ __INLINE__ string& str() { _v[_n]='\0'; return *this; }
-	__GPU__ __INLINE__ char *c_str() { _v[_n]='\0'; return _v;    }
-	__GPU__ string& substr(int i) {
+    __GPU__ __INLINE__ StrBuf& str() { _v[_n]='\0'; return *this; }
+    __GPU__ __INLINE__ char *c_str() { _v[_n]='\0'; return _v;    }
+    __GPU__ StrBuf& substr(int i) {
         _v[_n] = '\0';
-        string& s = *new string(&_v[i]);
+        StrBuf& s = *new StrBuf(&_v[i]);
         return s;
     }
     ///
     /// compare
     ///
-	__GPU__ bool operator==(const char *s2)   { return memcmp(_v, s2, _n)==0; }
-	__GPU__ bool operator==(const string& s2) { return memcmp(_v, s2._v, _n)==0; }
+    __GPU__ bool operator==(const char *s2)   { return memcmp(_v, s2, _n)==0; }
+    __GPU__ bool operator==(const StrBuf& s2) { return memcmp(_v, s2._v, _n)==0; }
     ///
     /// assignment
     ///
-	__GPU__ string& operator<<(const char c)  { push(c); return *this; }
-	__GPU__ string& operator<<(const char *s) { merge((char*)s, STRLENB(s)); return *this; }
-	__GPU__ string& operator<<(string& s)     { merge(s._v, s._n); return *this; }
-	__GPU__ string& operator<<(int i) {
+    __GPU__ StrBuf& operator<<(const char c)  { push(c); return *this; }
+    __GPU__ StrBuf& operator<<(const char *s) { merge((char*)s, STRLENB(s)); return *this; }
+    __GPU__ StrBuf& operator<<(StrBuf& s)     { merge(s._v, s._n); return *this; }
+    __GPU__ StrBuf& operator<<(int i) {
         char s[36];
         int n = ITOA(i, s, 10);
         merge(s, n);
         return *this;
     }
-	__GPU__ string& operator<<(float f) {
-		if (f < 0) { f = -f; push('-'); }
-		int i = static_cast<int>(f);
+    __GPU__ StrBuf& operator<<(float f) {
+        if (f < 0) { f = -f; push('-'); }
+        int i = static_cast<int>(f);
         int d = static_cast<int>(round(CUEF_FLOAT_PRECISION*(f - i)));
         return *this << i << '.' << d;
-	}
+    }
     ///
     /// conversion
     ///
-	__GPU__ int   to_i(char **p, int base=10) { return (int)STRTOL(_v, p, base); }
+    __GPU__ int   to_i(char **p, int base=10) { return (int)STRTOL(_v, p, base); }
     __GPU__ float to_f(char **p)              { return (float)STRTOF(_v, p);     }
 };
-    
-} // namespace cuef
-#endif // __EFORTH_SRC_STRING_H
+#endif // __EFORTH_SRC_STRBUF_H
