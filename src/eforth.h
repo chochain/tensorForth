66a4fd3a1f109b69d2bd629f5c2c951bc834da0c
gnii <chochain@yahoo.com>
2022-03-21 08:40:26 -0700

    ~/src/eforth.h replace std::string with char* (enable kernel access)

--- src/eforth.h
+++ src/eforth.h
@@ -1,8 +1,8 @@
 #ifndef __EFORTH_SRC_EFORTH_H
 #define __EFORTH_SRC_EFORTH_H
-#include "cuef.h"
+#include "cuef_types.h"
 #include "vector.h"         // cueForth vector
-#include "sstream.h"		// cueForth sstream
+//#include "sstream.h"		// cueForth sstream
 
 #define ENDL            "\n"
 #define millis()        clock()
@@ -11,8 +11,6 @@
 
 typedef GF    DTYPE;
 #define DVAL  0.0f
-
-namespace cuef {
 
 class Code;                                 /// forward declaration
 struct fop {                                /// alternate solution for function
@@ -27,27 +25,27 @@ struct function : fop {
 
 class Code {
 public:
-    string name;                            /// name of word
+    const char   *name;                     /// name of word
     int    token = 0;                       /// dictionary order token
     bool   immd  = false;                   /// immediate flag
     int    stage = 0;                       /// branching stage
     fop    *xt   = NULL;                    /// primitive function
-    string literal;                         /// string literal
+    const char   *literal;                  /// StrBuf literal
 
-    vector<Code*> pf;
-    vector<Code*> pf1;
-    vector<Code*> pf2;
-    vector<DTYPE> qf;
+    Vector<Code*> pf;
+    Vector<Code*> pf1;
+    Vector<Code*> pf2;
+    Vector<DTYPE> qf;
 
     template<typename F>
-    __GPU__ Code(string n, F fn, bool im=false);	  /// primitive
-    __GPU__ Code(string n, bool f=false);             /// new colon word or temp
+    __GPU__ Code(const char *n, F fn, bool im=false); /// primitive
+    __GPU__ Code(const char *n, bool f=false);        /// new colon word or temp
     __GPU__ Code(Code *c,  DTYPE d);                  /// dolit, dovar
-    __GPU__ Code(Code *c,  string s=string(""));      /// dotstr
+    __GPU__ Code(Code *c,  const char *s="");         /// dotstr
 
     __GPU__ Code     *addcode(Code *w);               /// append colon word
-    __GPU__ string&  to_s();                          /// debugging
-    __GPU__ string&  see(int dp);
+    __GPU__ char     &to_s();                         /// debugging
+    __GPU__ char     &see(int dp);
     __GPU__ void     nest();                          /// execute word
 };
 ///
@@ -55,20 +53,20 @@ public:
 ///
 class ForthVM {
 public:
-    istream       &cin;                     /// stream input
-	ostream       &cout;				    /// stream output
+//  istream       &cin;                     /// stream input
+//	ostream       &cout;				    /// stream output
 
-    vector<DTYPE> rs;                       /// return stack
-    vector<DTYPE> ss;                       /// parameter stack
-    vector<Code*> prim;                     /// primitives
-    vector<Code*> dict;						/// dictionary
+    Vector<DTYPE> rs;                       /// return stack
+    Vector<DTYPE> ss;                       /// parameter stack
+    Vector<Code*> prim;                     /// primitives
+    Vector<Code*> dict;						/// dictionary
 
     bool  compile = false;                  /// compiling flag
     int   base    = 10;                     /// numeric radix
     int   WP      = 0;                      /// instruction and parameter pointers
     DTYPE top     = DVAL;                   /// cached top of stack
 
-    __GPU__ ForthVM(istream &in, ostream &out);
+ //   __GPU__ ForthVM(istream &in, ostream &out);
 
     __GPU__ void init();
     __GPU__ void outer();
@@ -78,16 +76,12 @@ private:
     __GPU__ DTYPE PUSH(DTYPE v);
     
     __GPU__ Code *find(const char *s);              /// search dictionary reversely
-    __GPU__ Code *find(string &s);                  /// search dictionary reversely
-    __GPU__ string& next_idiom(char delim=0);
+    __GPU__ char *next_idiom(char delim=0);
     __GPU__ void call(Code *c);                     /// execute a word
-    __GPU__ void call(vector<Code*> pf);
+    __GPU__ void call(Vector<Code*> pf);
     
     __GPU__ void dot_r(int n, DTYPE v);
     __GPU__ void ss_dump();
     __GPU__ void words();
 };
-
-} // namespace cuef
-
 #endif // __EFORTH_SRC_EFORTH_H
