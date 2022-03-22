d7e8786cc6ed6ca9de4d33dd0b6e360da7e5460d
gnii <chochain@yahoo.com>
2022-03-21 18:00:59 -0700

    ~/src/eforth.h merge ceforth8

--- src/eforth.h
+++ src/eforth.h
@@ -2,6 +2,7 @@
 #define __EFORTH_SRC_EFORTH_H
 #include "cuef_types.h"
 #include "vector.h"         // cueForth vector
+#include "strbuf.h"
 //#include "sstream.h"		// cueForth sstream
 
 #define ENDL            "\n"
@@ -25,46 +26,58 @@ struct function : fop {
 
 class Code {
 public:
-    const char   *name;                     /// name of word
-    int    token = 0;                       /// dictionary order token
-    bool   immd  = false;                   /// immediate flag
-    int    stage = 0;                       /// branching stage
-    fop    *xt   = NULL;                    /// primitive function
-    const char   *literal;                  /// StrBuf literal
-
-    Vector<Code*> pf;
-    Vector<Code*> pf1;
-    Vector<Code*> pf2;
-    Vector<DTYPE> qf;
-
-    template<typename F>
-    __GPU__ Code(const char *n, F fn, bool im=false); /// primitive
-    __GPU__ Code(const char *n, bool f=false);        /// new colon word or temp
-    __GPU__ Code(Code *c,  DTYPE d);                  /// dolit, dovar
-    __GPU__ Code(Code *c,  const char *s="");         /// dotstr
-
-    __GPU__ Code     *addcode(Code *w);               /// append colon word
-    __GPU__ char     &to_s();                         /// debugging
-    __GPU__ char     &see(int dp);
-    __GPU__ void     nest();                          /// execute word
+    const char *name = 0;   /// name field
+    union {                 /// either a primitive or colon word
+        fop *xt = 0;        /// lambda pointer
+        struct {            /// a colon word
+            U16 def:  1;    /// colon defined word
+            U16 immd: 1;    /// immediate flag
+            U16 len:  14;   /// len of pfa
+            IU  pfa;        /// offset to pmem space
+        };
+    };
+    template<typename F>    /// template function for lambda
+    __GPU__ Code(const char *n, F f, bool im=false) : name(n) {
+        xt = new function<F>(f);
+        immd = im ? 1 : 0;
+    }
+    __GPU__ Code() {}       /// create a blank struct (for initilization)
 };
 ///
-/// Forth virtual machine variables
+/// Forth Virtual Machine operational macros
+///
+#define STRLEN(s) (ALIGN(d_strlen(s,0)+1))  /** calculate string size with alignment     */
+#define XIP       (dict[-1].len)            /** parameter field tail of latest word      */
+#define PFA(w)    ((U8*)&pmem[dict[w].pfa]) /** parameter field pointer of a word        */
+#define PFLEN(w)  (dict[w].len)             /** parameter field length of a word         */
+#define CELL(a)   (*(DU*)&pmem[a])          /** fetch a cell from parameter memory       */
+#define STR(a)    ((char*)&pmem[a])         /** fetch string pointer to parameter memory */
+#define JMPIP     (IP0 + *(IU*)IP)          /** branching target address                 */
+#define SETJMP(a) (*(IU*)(PFA(-1) + (a)))   /** address offset for branching opcodes     */
+#define HERE      (pmem.idx)                /** current parameter memory index           */
+#define IPOFF     ((IU)(IP - PMEM0))        /** IP offset relative parameter memory root */
+#define CALL(c)\
+	if (dict[c].def) nest(c);\
+    else (*(fop*)(((uintptr_t)dict[c].xt)&~0x3))(c)
+///
+/// Forth virtual machine class
 ///
 class ForthVM {
 public:
 //  istream       &cin;                     /// stream input
 //	ostream       &cout;				    /// stream output
 
-    Vector<DTYPE> rs;                       /// return stack
-    Vector<DTYPE> ss;                       /// parameter stack
-    Vector<Code*> prim;                     /// primitives
-    Vector<Code*> dict;						/// dictionary
+    Vector<DU,   64>      rs;               /// return stack
+    Vector<DU,   64>      ss;               /// parameter stack
+    Vector<Code, 1024>    dict;				/// dictionary
+    Vector<U8,   48*1024> pmem;             /// primitives
 
-    bool  compile = false;                  /// compiling flag
+    bool  compile = false, ucase = true;    /// compiling flag
     int   base    = 10;                     /// numeric radix
-    int   WP      = 0;                      /// instruction and parameter pointers
-    DTYPE top     = DVAL;                   /// cached top of stack
+    DU    top     = DVAL;                   /// cached top of stack
+    IU    WP      = 0;                      /// instruction and parameter pointers
+    U8    *PMEM0  = &pmem[0];
+    U8    *IP = PMEM0, *IP0 = PMEM0;
 
  //   __GPU__ ForthVM(istream &in, ostream &out);
 
@@ -72,16 +85,33 @@ public:
     __GPU__ void outer();
 
 private:
-    __GPU__ DTYPE POP();
-    __GPU__ DTYPE PUSH(DTYPE v);
+    __GPU__ DU   POP()        { DU n=top; top=ss.pop(); return n; }
+    __GPU__ DU   PUSH(DU v)   { ss.push(top); top = v; }
     
-    __GPU__ Code *find(const char *s);              /// search dictionary reversely
-    __GPU__ char *next_idiom(char delim=0);
-    __GPU__ void call(Code *c);                     /// execute a word
-    __GPU__ void call(Vector<Code*> pf);
-    
-    __GPU__ void dot_r(int n, DTYPE v);
-    __GPU__ void ss_dump();
+    __GPU__ int  streq(const char *s1, const char *s2);
+    __GPU__ int  find(const char *s);      /// search dictionary reversely
+    ///
+    /// Forth compiler functions
+    ///
+    __GPU__ void add_iu(IU i);             /// add an instruction into pmem
+    __GPU__ void add_du(DU v);             /// add a cell into pmem
+    __GPU__ void add_str(const char *s);   /// add a string to pmem
+    __GPU__ void colon(const char *name);
+    ///
+    /// Forth inner interpreter
+    ///
+    __GPU__ char *next_word();
+    __GPU__ char *scan();
+    __GPU__ void nest(IU c);
+    __GPU__ void call(Code *c);             /// execute a word
+    ///
+    /// debug functions
+    ///
+    __GPU__ void dot_r(int n, DU v);
+    __GPU__ void to_s(IU c);
+    __GPU__ void see(IU *cp, IU *ip, int dp=0);
     __GPU__ void words();
+    __GPU__ void ss_dump();
+    __GPU__ void mem_dump(IU p0, DU sz);
 };
 #endif // __EFORTH_SRC_EFORTH_H
