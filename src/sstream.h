b213290ca8da1695b0f34ff4d059d4cb3d4045d6
gnii <chochain@yahoo.com>
2022-03-24 08:18:25 -0700

    ~/src/sstream.h print_node=>obuf_node

--- src/sstream.h
+++ src/sstream.h
@@ -20,7 +20,7 @@ extern __GPU__ U8  *_output_buf;
 extern __GPU__ U8  *_output_ptr;
 
 __KERN__ void        stream_init(U8 *buf, int sz);
-__HOST__ print_node* stream_print(print_node *node, int trace);
+__HOST__ obuf_node*  stream_print(obuf_node *node, int trace);
 __HOST__ void        stream_flush(U8 *output_buf, int trace);
 
 #endif // CUEF_SRC_SSTREAM_H_
