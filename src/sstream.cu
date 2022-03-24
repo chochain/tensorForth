c22b752dad676eb55efe65e35e2e0e2b35ae0fa4
gnii <chochain@yahoo.com>
2022-03-24 08:18:48 -0700

    ~/src/sstream.cu untabify; print_node=>obuf_node

--- src/sstream.cu
+++ src/sstream.cu
@@ -12,67 +12,65 @@
 #include <cstdio>
 #include "sstream.h"
 
-__GPU__ volatile int cuef::_mutex_ss;			// for ostream module
+__GPU__ volatile int _mutex_ss;                 // for ostream module
 
 __GPU__ int _output_size;
 __GPU__ U8  *_output_buf;
-__GPU__ U8  *_output_ptr;						// global output buffer for now, per session later
+__GPU__ U8  *_output_ptr;                       // global output buffer for now, per session later
 
 __KERN__ void
 stream_init(U8 *buf, int sz)
 {
-	if (threadIdx.x!=0 || blockIdx.x!=0) return;
+    if (threadIdx.x!=0 || blockIdx.x!=0) return;
 
-	print_node *node = (print_node *)buf;
-	node->gt = GT_EMPTY;
+    obuf_node *node = (obuf_node *)buf;
+    node->gt = GT_EMPTY;
 
-	_output_buf = _output_ptr = buf;
+    _output_buf = _output_ptr = buf;
 
-	if (sz) _output_size = sz;					// set to new size
+    if (sz) _output_size = sz;                  // set to new size
 }
 
-#define NEXTNODE(n)	((print_node *)(node->data + node->size))
+#define NEXTNODE(n) ((obuf_node *)(node->data + node->size))
 
-__HOST__ print_node*
-stream_print(print_node *node, int trace)
+__HOST__ obuf_node*
+stream_print(obuf_node *node, int trace)
 {
-	U8 buf[80];									// check buffer overflow
+    U8 buf[80];                                 // check buffer overflow
 
-	if (trace) printf("<%d>", node->id);
+    if (trace) printf("<%d>", node->id);
 
-	switch (node->gt) {
-	case GT_INT:
-		printf("%d", *((GI*)node->data));
-		break;
-	case GT_HEX:
-		printf("%x", *((GI*)node->data));
-		break;
-	case GT_FLOAT:
-		printf("%g", *((GF*)node->data));
-		break;
-	case GT_STR:
-		memcpy(buf, (U8*)node->data, node->size);
-		printf("%s", (U8*)buf);
-		break;
-	default: printf("print node type not supported: %d", node->gt); break;
-	}
+    switch (node->gt) {
+    case GT_INT:
+        printf("%d", *((GI*)node->data));
+        break;
+    case GT_HEX:
+        printf("%x", *((GI*)node->data));
+        break;
+    case GT_FLOAT:
+        printf("%g", *((GF*)node->data));
+        break;
+    case GT_STR:
+        memcpy(buf, (U8*)node->data, node->size);
+        printf("%s", (U8*)buf);
+        break;
+    default: printf("print node type not supported: %d", node->gt); break;
+    }
 
-	if (trace) printf("</%d>\n", node->id);
+    if (trace) printf("</%d>\n", node->id);
 
-	return node;
+    return node;
 }
 
 __HOST__ void
 stream_flush(U8 *output_buf, int trace)
 {
-	GPU_SYNC();
-	print_node *node = (print_node *)output_buf;
-	while (node->gt != GT_EMPTY) {			// 0
-		node = stream_print(node, trace);
-		node = NEXTNODE(node);
-	}
-	stream_init<<<1,1>>>(output_buf, 0);
-	GPU_SYNC();
+    GPU_SYNC();
+    obuf_node *node = (obuf_node *)output_buf;
+    while (node->gt != GT_EMPTY) {          // 0
+        node = stream_print(node, trace);
+        node = NEXTNODE(node);
+    }
+    stream_init<<<1,1>>>(output_buf, 0);
+    GPU_SYNC();
 }
-
-
