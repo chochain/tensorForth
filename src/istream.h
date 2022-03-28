1ce77f312df5820d78fb415c7c6cde694f9db020
gnii <chochain@yahoo.com>
2022-03-27 17:39:44 -0700

    ~/src/istream.h add get_idiom; update getline

--- src/istream.h
+++ src/istream.h
@@ -16,6 +16,7 @@
 /// istream class
 ///
 class Istream {
+public:
     char *_buf = NULL;  /// input buffer
     int  _idx  = 0;     /// current buffer index
     int  _gn   = 0;     /// number of byte processed
@@ -28,14 +29,13 @@ class Istream {
         return nidx;
     }
 
-public:
-    __GPU__  Istream(char *s)  { _buf = s; }
-    __GPU__  Istream(int sz=0) { if (sz) _buf = new char[_gn=ALIGN4(sz)]; }
-    __GPU__  ~Istream()        { if (_buf) delete[] _buf; }
+    __GPU__ Istream(char *s)  { _buf = s; }
+    __GPU__ Istream(int sz=0) { if (sz) _buf = new char[_gn=ALIGN4(sz)]; }
+    __GPU__ ~Istream()        { if (_buf) delete[] _buf; }
     ///
     /// intialize by a given string
     ///
-    __GPU__  Istream& str(const char *s, int sz=0) {
+    __GPU__ Istream& str(const char *s, int sz=0) {
         if (_buf) delete[] _buf;
         _buf = new char[_gn = ALIGN4(sz ? sz : STRLENB(s))];
         MEMCPY(_buf, s, _gn);
@@ -50,7 +50,7 @@ public:
     //
     /// parser
     ///
-    __GPU__ Istream& getline(char *s, char delim=' ') {
+    __GPU__ Istream& get_idiom(char *s, char delim=' ') {
         int nidx = _tok(delim);             // index to next token
         if (nidx==0) return *this;          // no token processed
         MEMCPY(s, &_buf[_idx], _gn);
@@ -58,14 +58,17 @@ public:
         _idx = nidx;                        // advance index
         return *this;
     }
-    __GPU__  int operator>>(char *s)   { getline(s); return _gn; }
+    __GPU__ Istream& getline(char *s, int sz, char delim='\n') {
+    	return get_idiom(s, delim);
+    }
+    __GPU__ int operator>>(char *s)   { get_idiom(s); return _gn; }
 
 #if CUEF_USE_STRBUF
 #include "strbuf.h"
     __GPU__  Istream& str(string& s) {
         str(s.c_str(), s.size()); return *this;
     }
-    __GPU__ Istream& getline(string& s, char delim=' ') {
+    __GPU__ Istream& get_idiom(string& s, char delim=' ') {
         int nidx = _tok(delim);
         if (nidx==0) return *this;
         s._n = 0;
@@ -74,7 +77,7 @@ public:
         _idx = nidx;
         return *this;
     }
-    __GPU__  int operator>>(string& s) { getline(s); return _gn; }
+    __GPU__  int operator>>(string& s) { get_idiom(s); return _gn; }
 #endif // CUEF_USE_STRBUF
 };
 #endif // CUEF_SRC_ISTREAM_H_
