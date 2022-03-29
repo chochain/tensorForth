50eade048b6b242f4c9bb265ca0b4c88ec838077
gnii <chochain@yahoo.com>
2022-03-28 18:34:17 -0700

    ~/src/cueforth.h refactor io streams to aio class, drop malloc/free

--- src/cueforth.h
+++ src/cueforth.h
@@ -4,23 +4,15 @@
 */
 #ifndef CUEF_SRC_CUEFORTH_H_
 #define CUEF_SRC_CUEFORTH_H_
-#include "cuef_config.h"
-#include "cuef_types.h"
+#include "aio.h"
 
 class CueForth {
-	U8   *_heap;
-    U8   *_ibuf;
-    U8   *_obuf;
     AIO  *aio;
 
-    __HOST__ void* _malloc(int sz, int type);
-    __HOST__ void  _free(void *mem);
-
 public:
-    CueForth();
+    CueForth(bool trace);
     ~CueForth();
 
-    __HOST__ int   setup(int step=0, int trace=0);
     __HOST__ int   is_running();
     __HOST__ int   run();
     __HOST__ void  teardown(int sig=0);
