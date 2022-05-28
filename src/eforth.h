834fae4799744ecf40c4018da5f9635cb4b290ad
gnii <chochain@yahoo.com>
2022-05-28 13:44:34 -0700

    ~/src/eforth.h update comment for doxygen

--- src/eforth.h
+++ src/eforth.h
@@ -7,47 +7,50 @@
 #define TEN4_SRC_EFORTH_H
 #include "vector.h"         // Forth vector (includes util.h)
 #include "aio.h"            // Forth async IO (includes Istream, Ostream)
-
+///
+///@name Cross platform support
+///@{
 #define ENDL            '\n'
 #define millis()        (clock()/1530000.0f)
 #define delay(ms)       { clock_t t = clock()+ms; while (clock()<t); }
 #define yield()
+///@}
 ///
 /// Forth virtual machine class
 ///
 typedef enum { VM_READY=0, VM_RUN, VM_WAIT, VM_STOP } vm_status;
-
-class Dict;
+///
+/// Forth Virtual Machine
+///
 class ForthVM {
 public:
-    vm_status     status = VM_READY;        /// VM status
-    DU    top    = DU0;                     /// cached top of stack
-    Vector<DU,   T4_RS_SZ> rs;              /// return stack
-    Vector<DU,   0>        ss;              /// parameter stack (setup in ten4.cu)
+    vm_status     status = VM_READY;        ///< VM status
+    DU    top    = DU0;                     ///< cached top of stack
+    Vector<DU,   T4_RS_SZ> rs;              ///< return stack
+    Vector<DU,   0>        ss;              ///< parameter stack (setup in ten4.cu)
 
     __GPU__ ForthVM(Istream *istr, Ostream *ostr, MMU *mmu);
     __GPU__ void init();
     __GPU__ void outer();
 
 private:
-    Istream       &fin;                     /// VM stream input
-    Ostream       &fout;                    /// VM stream output
-    MMU           &mmu;                     /// memory managing unit
-    Code          *dict;                    /// dictionary array
+    Istream       &fin;                     ///< VM stream input
+    Ostream       &fout;                    ///< VM stream output
+    MMU           &mmu;                     ///< memory managing unit
+    Code          *dict;                    ///< dictionary array
 
-    bool  ucase   = true;                   /// case insensitive
-    bool  compile = false;                  /// compiling flag
-    int   radix   = 10;                     /// numeric radix
-    IU    WP      = 0;                      /// word and parameter pointers
-    IU    IP      = 0;                      /// instruction pointer
-    IU    NXT;                              /// cached DONEXT xt address, used in nest()
+    bool  ucase   = true;                   ///< case insensitive
+    bool  compile = false;                  ///< compiling flag
+    int   radix   = 10;                     ///< numeric radix
+    IU    WP      = 0;                      ///< word and parameter pointers
+    IU    IP      = 0;                      ///< instruction pointer
 
-    char  idiom[T4_STRBUF_SZ];              /// terminal input buffer
+    char  idiom[T4_STRBUF_SZ];              ///< terminal input buffer
 
     __GPU__ __INLINE__ DU POP()        { DU n=top; top=ss.pop(); return n; }
     __GPU__ __INLINE__ void PUSH(DU v) { ss.push(top); top = v; }
 
-    __GPU__ int  find(const char *s);       /// search dictionary reversely
+    __GPU__ int  find(const char *s);       ///< search dictionary reversely
     ///
     /// Forth inner interpreter
     ///
@@ -57,11 +60,11 @@ private:
     ///
     /// compiler proxy funtions to reduce verbosity
     ///
-    __GPU__ void add_iu(IU i);              /// append an instruction unit to parameter memory
-    __GPU__ void add_du(DU d);              /// append a data unit to pmem
-    __GPU__ void add_str(const char *s);    /// append a string to pmem
-    __GPU__ void add_w(IU w);               /// append a word pfa to pmem
-    __GPU__ void call(IU w);
+    __GPU__ void add_w(IU w);               ///< append a word pfa to pmem
+    __GPU__ void add_iu(IU i);              ///< append an instruction unit to parameter memory
+    __GPU__ void add_du(DU d);              ///< append a data unit to pmem
+    __GPU__ void add_str(const char *s);    ///< append a string to pmem
+    __GPU__ void call(IU w);                ///< execute word by index
     ///
     /// output functions
     ///
