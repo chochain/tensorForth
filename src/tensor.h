ab4f6aa298c188e5393318a081d026af97ee2669
gnii <chochain@yahoo.com>
2022-05-31 18:45:00 -0700

    ~/src/tensor.h rename INLINE to XLINE to debug

--- src/tensor.h
+++ src/tensor.h
@@ -18,6 +18,7 @@
 ///@}
 ///@name tensorForth complex data object
 ///@{
+#define __XNLINE__
 typedef struct Tensor {
     union {
         F32 f;
@@ -28,41 +29,41 @@ typedef struct Tensor {
     };
     __BOTH__ Tensor()      : f(DU0) { r = 0; }
     __BOTH__ Tensor(F32 f0): f(f0)  { r = 0; }
-    __BOTH__ __INLINE__ Tensor &operator=(int i)     { f = static_cast<F32>(i); return *this; }
-    __BOTH__ __INLINE__ Tensor &operator=(F32 f0)    { f = f0;  return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator=(S32 i)     { f = static_cast<F32>(i); return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator=(F32 f0)    { f = f0; r = 0; return *this; }
 	///
     /// tensor arithmetics
     ///
-    __BOTH__ __INLINE__ Tensor &operator+=(Tensor &t){ f += t.f; return *this; }
-    __BOTH__ __INLINE__ Tensor &operator-=(Tensor &t){ f -= t.f; return *this; }
-    __BOTH__ __INLINE__ F32    operator+(Tensor &t)  { return f + t.f; }
-    __BOTH__ __INLINE__ F32    operator-(Tensor &t)  { return f - t.f; }
-    __BOTH__ __INLINE__ F32    operator*(Tensor &t)  { return f * t.f; }
-    __BOTH__ __INLINE__ F32    operator/(Tensor &t)  { return f / t.f; }
-    __BOTH__ __INLINE__ F32    operator%(Tensor &t)  { return fmod(f, t.f); }
+    __BOTH__ __XNLINE__ Tensor &operator+=(Tensor &t){ f += t.f; return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator-=(Tensor &t){ f -= t.f; return *this; }
+    __BOTH__ __XNLINE__ F32    operator+(Tensor &t)  { return f + t.f; }
+    __BOTH__ __XNLINE__ F32    operator-(Tensor &t)  { return f - t.f; }
+    __BOTH__ __XNLINE__ F32    operator*(Tensor &t)  { return f * t.f; }
+    __BOTH__ __XNLINE__ F32    operator/(Tensor &t)  { return f / t.f; }
+    __BOTH__ __XNLINE__ F32    operator%(Tensor &t)  { return fmod(f, t.f); }
 	///
     /// tensor logical ops
     ///
-    __BOTH__ __INLINE__ bool   operator<(Tensor &t)  { return (f - t.f) <  -DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator>(Tensor &t)  { return (f - t.f) >   DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator<=(Tensor &t) { return (f - t.f) <= -DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator>=(Tensor &t) { return (f - t.f) >=  DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator==(Tensor &t) { return fabs(f - t.f) <  DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator!=(Tensor &t) { return fabs(f - t.f) >= DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator<(Tensor &t)  { return (f - t.f) <  -DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator>(Tensor &t)  { return (f - t.f) >   DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator<=(Tensor &t) { return (f - t.f) <= -DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator>=(Tensor &t) { return (f - t.f) >=  DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator==(Tensor &t) { return fabs(f - t.f) <  DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator!=(Tensor &t) { return fabs(f - t.f) >= DU_EPS; }
 	///
     /// float arithmetics
     ///
-    __BOTH__ __INLINE__ Tensor &operator+=(F32 f0)   { f += f0; return *this; }
-    __BOTH__ __INLINE__ Tensor &operator-=(F32 f0)   { f -= f0; return *this; }
-    __BOTH__ __INLINE__ Tensor &operator*=(F32 f0)   { f *= f0; return *this; }
-    __BOTH__ __INLINE__ Tensor &operator/=(F32 f0)   { f /= f0; return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator+=(F32 f0)   { f += f0; r = 0; return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator-=(F32 f0)   { f -= f0; r = 0; return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator*=(F32 f0)   { f *= f0; r = 0; return *this; }
+    __BOTH__ __XNLINE__ Tensor &operator/=(F32 f0)   { f /= f0; r = 0; return *this; }
     ///
     /// float logical ops
     ///
-    __BOTH__ __INLINE__ bool   operator<(F32 f0)     { return (f - f0) <  -DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator>(F32 f0)     { return (f - f0) >   DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator>=(F32 f0)    { return (f - f0) >=  DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator==(F32 f0)    { return fabs(f - f0)  <  DU_EPS; }
-    __BOTH__ __INLINE__ bool   operator!=(F32 f0)    { return fabs(f - f0)  >= DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator<(F32 f0)     { return (f - f0) <  -DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator>(F32 f0)     { return (f - f0) >   DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator>=(F32 f0)    { return (f - f0) >=  DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator==(F32 f0)    { return fabs(f - f0)  <  DU_EPS; }
+    __BOTH__ __XNLINE__ bool   operator!=(F32 f0)    { return fabs(f - f0)  >= DU_EPS; }
 } DU;
 #endif // TEN4_SRC_TENSOR_H_
