017712f61ce97e2c0b01f9b8af3ec5f3329559f9
gnii <chochain@yahoo.com>
2022-06-13 19:44:05 -0700

    ~/src add tensor.cu

---
+++ src/tensor.cu
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * @brief tensorForth tensor class implementation
+ *
+ * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
+ */
+#include "tensor.h"
+__GPU__ Tensor&
+Tensor::gemm(Tensor &A, Tensor &B, Tensor &C) {
+    // D = alpha * A * B + beta * C;
+}
