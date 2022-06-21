1208d99bca61d5000b783fdce650a35a57892d4f
gnii <chochain@yahoo.com>
2022-06-20 19:48:17 -0700

    ~/src/tensor.cu reorder NHWC to HWNC (shape index)

--- src/tensor.cu
+++ src/tensor.cu
@@ -28,11 +28,11 @@ Tensor::Tensor(U16 n, U16 h, U16 w, U16 c) :
     dsize(sizeof(DU)),
     rank(4),
     stride({1,1,1,1}),
-    shape({n, h, w, c})
+    shape({h, w, n, c})
 {
     cudaMallocManaged((void**)&data, size);
     GPU_CHK();
-    printf("tensor(%d,%d,%d,%d) allocated\n", shape[0], shape[1], shape[2], shape[3]);
+    printf("tensor(%d,%d,%d,%d) allocated\n", shape[2], shape[0], shape[1], shape[3]);
 }
 
 __HOST__
@@ -40,12 +40,12 @@ Tensor::Tensor(U16 h, U16 w) :
     size(h * w * sizeof(DU)),
     dsize(sizeof(DU)),
     rank(2),
-    stride({0,1,1,0}),
-    shape({0, h, w, 0})
+    stride({1,1,0,0}),
+    shape({h, w, 0, 0})
 {
     cudaMallocManaged((void**)&data, size);
     GPU_CHK();
-    printf("matrix(%d,%d) allocated\n", shape[1], shape[2]);
+    printf("matrix(%d,%d) allocated\n", shape[0], shape[1]);
 }
 
 __HOST__
@@ -54,8 +54,8 @@ Tensor::~Tensor()
     if (!data) return;
     cudaFree((void*)data);
     switch (rank) {
-    case 2: printf("matrix(%d,%d) freed\n", shape[1], shape[2]); break;
-    case 4: printf("tensor(%d,%d,%d,%d) freed\n", shape[0], shape[1], shape[2], shape[3]); break;
+    case 2: printf("matrix(%d,%d) freed\n", shape[0], shape[1]); break;
+    case 4: printf("tensor(%d,%d,%d,%d) freed\n", shape[2], shape[0], shape[1], shape[3]); break;
     default: printf("~Tensor error: rank=%d\n", rank);
     }
 }
