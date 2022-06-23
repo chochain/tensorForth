7f40b1392f641d3d3153c6ac148311688afc6820
gnii <chochain@yahoo.com>
2022-06-22 19:52:35 -0700

    ~/src/tensor.cu implement reset reshape and constructors

--- src/tensor.cu
+++ src/tensor.cu
@@ -23,29 +23,47 @@ __KERN__ void k_matrix_randomize(U8 *mat, int nrow, int ncol, int seed=0)
 }
 
 __HOST__
-Tensor::Tensor(U16 n, U16 h, U16 w, U16 c) :
-    size(n * h * w * c * sizeof(DU)),
+Tensor::Tensor() :
+    size(0),
     dsize(sizeof(DU)),
-    rank(4),
-    stride{1, 1, 1, 1},
-    shape{h, w, n, c}
-{
+    rank(0),
+    stride{0, 0, 0, 0},
+    shape{0, 0, 0, 0} {}
+
+__HOST__
+Tensor::Tensor(U64 sz) :
+    size(sz),
+    dsize(sizeof(DU)),
+    rank(1),
+    stride{0, 0, 0, 0},
+    shape{0, 0, 0, 0} {
     cudaMallocManaged((void**)&data, size);
     GPU_CHK();
-    printf("tensor(%d,%d,%d,%d) allocated\n", shape[2], shape[0], shape[1], shape[3]);
+    printf("tensor[%ld] allocated\n", size);
 }
 
 __HOST__
 Tensor::Tensor(U16 h, U16 w) :
-    size(h * w * sizeof(DU)),
+    size(sizeof(DU) * h * w),
     dsize(sizeof(DU)),
     rank(2),
     stride{1, 1, 0, 0},
-    shape{h, w, 0, 0}
-{
-    cudaMallocManaged((void**)&data, size);
+    shape{h, w, 0, 0} {
+    cudaMallocManaged((void**)&data, (size_t)size);
     GPU_CHK();
     printf("matrix(%d,%d) allocated\n", shape[0], shape[1]);
+}
+
+__HOST__
+Tensor::Tensor(U16 n, U16 h, U16 w, U16 c) :
+    size(sizeof(DU) * n * h * w * c),
+    dsize(sizeof(DU)),
+    rank(4),
+    stride{1, 1, 1, 1},
+    shape{h, w, n, c} {
+    cudaMallocManaged((void**)&data, (size_t)size);
+    GPU_CHK();
+    printf("tensor(%d,%d,%d,%d) allocated\n", shape[2], shape[0], shape[1], shape[3]);
 }
 
 __HOST__
@@ -58,6 +76,47 @@ Tensor::~Tensor()
     case 4: printf("tensor(%d,%d,%d,%d) freed\n", shape[2], shape[0], shape[1], shape[3]); break;
     default: printf("~Tensor error: rank=%d\n", rank);
     }
+}
+
+__HOST__ Tensor&
+Tensor::reset(U8 *mptr, U64 sz) {
+    size   = sz;
+    dsize  = sizeof(DU);
+    rank   = 1;
+    // stride, shape not used
+    data   = mptr;
+    printf("tensor reset(%p, %ld)\n", mptr, sz);
+    return *this;
+}
+
+__HOST__ Tensor&
+Tensor::reshape(U16 h, U16 w) {
+    U64 sz = sizeof(DU) * h * w;
+    if (sz == size) {
+        rank   = 2;
+        U16 t[4] = {1, 1, 0, 0}; memcpy(stride, t, sizeof(t));
+        U16 s[4] = {h, w, 0, 0}; memcpy(shape,  s, sizeof(s));
+        printf("tensor reshaped(%d,%d)\n", shape[0], shape[1]);
+    }
+    else {
+        printf("reshape sz != size (%ld != %ld)", sz, size);
+    }
+    return *this;
+}
+
+__HOST__ Tensor&
+Tensor::reshape(U16 n, U16 h, U16 w, U16 c) {
+    U64 sz = sizeof(DU) * n * h * w * c;
+    if (sz == size) {
+        rank   = 4;
+        U16 t[4] = {1, 1, 1, 1}; memcpy(stride, t, sizeof(t));
+        U16 s[4] = {h, w, n, c}; memcpy(shape,  s, sizeof(s));
+        printf("tensor reshaped(%d,%d,%d,%d)\n", shape[2], shape[0], shape[1], shape[3]);
+    }
+    else {
+        printf("reshape sz != size (%ld != %ld)", sz, size);
+    }
+    return *this;
 }
 
 __HOST__ Tensor&
