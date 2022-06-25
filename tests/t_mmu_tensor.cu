fc053f4fcbfe8a2cc3bbde845de03e6fbbf75354
gnii <chochain@yahoo.com>
2022-06-25 10:39:15 -0700

    ~/tests/t_mmu_tensor add kernel timing; add fill.random

--- tests/t_mmu_tensor.cu
+++ tests/t_mmu_tensor.cu
@@ -12,7 +12,7 @@ using namespace std;
 #include "../src/ten4_types.h"
 #include "../src/mmu.h"
 
-__KERN__ void ReferenceGemm_kernel(
+__KERN__ void k_GEMM(
     int M, int N, int K,
     DU *A, DU *B, DU *C,   /* MxK, KxN, MxN */
     DU alpha, DU beta)
@@ -32,25 +32,31 @@ __KERN__ void ReferenceGemm_kernel(
 // dynamic parallel
 //
 __KERN__ void
-test_mmu_gemm(MMU *mmu, U16 M, U16 N, U16 K, DU alpha, DU beta) {
+test_mmu_gemm(
+    MMU *mmu, int khz,
+    U16 M, U16 N, U16 K, DU alpha, DU beta) {
     if (threadIdx.x != 0 || blockIdx.x != 0) return;
 
-    Tensor &A = mmu->tensor(M, K);
-    Tensor &B = mmu->tensor(K, N);
-    Tensor &C = mmu->tensor(M, N);
+    Tensor &A = mmu->tensor(M, K); A.fill(0).random(0);
+    Tensor &B = mmu->tensor(K, N); B.fill(0).random(17);
+    Tensor &C = mmu->tensor(M, N); C.fill(0).random(101);
 
     int m = C.H(), n = C.W(), k = A.W();
     printf("\nRef.GEMM M=%d, N=%d, K=%d", m, n, k);
     
     dim3 block(16, 16);         /* 256 threads */
     dim3 grid((n + block.x - 1) / block.x, (m + block.y - 1) / block.y);
-    ReferenceGemm_kernel<<<grid, block>>>(
+
+    clock_t start = clock();
+    k_GEMM<<<grid, block>>>(
         m, n, k,
         (DU*)A.data, (DU*)B.data, (DU*)C.data,
         alpha, beta);
+    cudaDeviceSynchronize();
+    printf(" (k_GEMM in %0.2f ms @ %d khz)", ((float)(clock() - start))/khz, khz);
 }
 
-void benchmark(MMU *mmu) {
+void benchmark(MMU *mmu, int khz) {
     cudaEvent_t events[2];
     float       runtime_ms;
     for (auto & event : events) {
@@ -58,8 +64,8 @@ void benchmark(MMU *mmu) {
         GPU_CHK();
     }
     cudaEventRecord(events[0]);
-
-    test_mmu_gemm<<<1,1>>>(mmu, 1024, 512, 2048, 1.5, 1.5);
+    
+    test_mmu_gemm<<<1,1>>>(mmu, khz, 1024, 512, 2048, 1.5, 1.5);
     GPU_CHK();
 
     // Wait for work on the device to complete.
@@ -67,7 +73,7 @@ void benchmark(MMU *mmu) {
     cudaEventSynchronize(events[1]);
     // Measure eresultd runtime
     cudaEventElapsedTime(&runtime_ms, events[0], events[1]);
-    printf(" => %f ms\n", runtime_ms);
+    printf(" => %0.2f ms\n", runtime_ms);
 
     // Cleanup
     for (auto event : events) {
@@ -76,9 +82,13 @@ void benchmark(MMU *mmu) {
 }
 
 int main(int argc, char**argv) {
-    MMU *mmu = new MMU();
+    int device = 0;
+    int khz    = 0;
+    cudaDeviceGetAttribute(&khz, cudaDevAttrClockRate, device);
+    GPU_CHK();
 
-    benchmark(mmu);
+    MMU *mmu   = new MMU();
+    benchmark(mmu, khz);
 
     return 0;
 }
