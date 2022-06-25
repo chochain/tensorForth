c1e1e062b6cac050b50a3620fa8d47f3a28b5827
gnii <chochain@yahoo.com>
2022-06-24 20:03:45 -0700

    ~/src/ten4 add mmu tensor gemm tests

--- src/ten4.cu
+++ src/ten4.cu
@@ -183,7 +183,7 @@ void sigtrap() {
     sigaction(SIGSEGV, &sa, NULL);
 }
 
-int main(int argc, char**argv) {
+int main0(int argc, char**argv) {
     string app = string(T4_APP_NAME) + " " + MAJOR_VERSION + "." + MINOR_VERSION;
     sigtrap();
     TensorForth *f = new TensorForth();
@@ -196,3 +196,75 @@ int main(int argc, char**argv) {
 
     return 0;
 }
+
+typedef DU FP;
+typedef cudaError_t (*gemm_op)(Tensor &A, Tensor &B, Tensor &C, FP alpha, FP beta);
+
+__KERN__ void ReferenceGemm_kernel(
+    int M, int N, int K,
+    FP *A, FP *B, FP *C,   /* MxK, KxN, MxN */
+    FP  alpha, FP beta)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (i < N && j < M) {
+        FP acc = 0;
+        for (int k = 0; k < K; ++k) {
+            acc += A[k + j * K] * B[i + k * N];      /* row major */
+        }
+        C[i + j * N] = alpha * acc + beta * C[i + j * N];
+    }
+}
+
+__KERN__ void
+test_mmu_gemm(MMU *mmu, U16 M, U16 N, U16 K, FP alpha, FP beta) {
+    if (threadIdx.x != 0 || blockIdx.x != 0) return;
+
+    Tensor &A = mmu->tensor(M, K);
+    Tensor &B = mmu->tensor(K, N);
+    Tensor &C = mmu->tensor(M, N);
+
+    int m = C.H(), n = C.W(), k = A.W();
+    printf("\nRef.GEMM M=%d, N=%d, K=%d", m, n, k);
+    
+    dim3 block(16, 16);   /* 256 threads */
+    dim3 grid((n + block.x - 1) / block.x, (m + block.y - 1) / block.y);
+    
+    ReferenceGemm_kernel<<<grid, block>>>(m, n, k, (FP*)A.data, (FP*)B.data, (FP*)C.data, alpha, beta);
+}
+
+void benchmark(MMU *mmu) {
+    cudaEvent_t events[2];
+    float       runtime_ms;
+    for (auto & event : events) {
+        cudaEventCreate(&event);
+        GPU_CHK();
+    }
+    cudaEventRecord(events[0]);
+
+    test_mmu_gemm<<<1,1>>>(mmu, 1024, 512, 2048, 1.5, 1.5);
+    GPU_CHK();
+
+    // Wait for work on the device to complete.
+    cudaEventRecord(events[1]);
+    cudaEventSynchronize(events[1]);
+    // Measure eresultd runtime
+    cudaEventElapsedTime(&runtime_ms, events[0], events[1]);
+    printf(" => %f ms\n", runtime_ms);
+
+    // Cleanup
+    for (auto event : events) {
+        (void)cudaEventDestroy(event);
+    }
+}
+
+int main(int argc, char**argv) {
+    MMU *mmu = new MMU();
+
+    benchmark(mmu);
+
+    return 0;
+}
+
+    
