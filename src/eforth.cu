f8913a4c7d9acb71a01b4c9b8fd1ff38fbb92c5b
gnii <chochain@yahoo.com>
2022-07-06 16:47:54 -0700

    ~/src/eforth export vm, tenvm; add number, parse polymorph methods

--- src/eforth.cu
+++ src/eforth.cu
@@ -11,28 +11,20 @@
 ///
 ///@name Data conversion
 ///@{
-#define INT(f)    (static_cast<int>(f + 0.5))   /**< cast float to int                       */
-#define I2D(i)    (static_cast<DU>(i))          /**< cast int back to float                  */
 #define ABS(d)    (fabs(d))                     /**< absolute value                          */
 #define ZERO(d)   (ABS(d) < DU_EPS)             /**< zero check                              */
 #define MOD(t,n)  (fmod(t, n))                  /**< fmod two floating points                */
 #define BOOL(f)   ((f) ? -1 : 0)                /**< default boolean representation          */
-///@}
-///@name Tensor ops
-///@{
-#define DU_ONLY(v) (*(U32*)&(v) &= ~1)          /**< tensor flag mask for top                */
 ///@}
 ///@name Dictioanry access
 ///@{
 #define PFA(w)    (dict[(IU)(w)].pfa)           /**< PFA of given word id                    */
 #define HERE      (mmu.here())                  /**< current context                         */
 #define SETJMP(a) (mmu.setjmp(a))               /**< address offset for branching opcodes    */
-#define FIND(s)   (mmu.find(s, compile, ucase)) /**< find input idiom in dictionary          */
 #define EXEC(w)   ((*(dict[(IU)(w)].xt))())     /**< execute primitive word                  */
 ///@}
 ///@name Heap memory load/store macros
 ///@{
-#define POPi      (INT(POP()))                  /**< convert popped DU as an IU              */
 #define LDi(ip)   (mmu.ri((IU)(ip)))            /**< read an instruction unit from pmem      */
 #define LDd(ip)   (mmu.rd((IU)(ip)))            /**< read a data unit from pmem              */
 #define STd(ip,d) (mmu.wd((IU)(ip), (DU)(d)))   /**< write a data unit to pmem               */
@@ -40,21 +32,12 @@
 ///@}
 __GPU__
 ForthVM::ForthVM(int khz, Istream *istr, Ostream *ostr, MMU *mmu0)
-    : khz(khz), fin(*istr), fout(*ostr), mmu(*mmu0), dict(mmu0->dict()) {
-    INFO("\\  VM[%d] init with (dict=%p, mem=%p, vss=%p)\n",
-         blockIdx.x, dict, mmu.pmem(0), mmu.vss(blockIdx.x));
+    : VM(khz, istr, ostr, mmu0), dict(mmu0->dict()) {
+    INFO("\\  ::ForthVM[%d](dict=%p) sizeof(Code)=%ld\n", blockIdx.x, dict, sizeof(Code));
 }
 ///
 /// Forth inner interpreter (colon word handler)
 ///
-__GPU__ char*
-ForthVM::next_idiom()  {                             ///< get next idiom from input stream
-    fin >> idiom; return idiom;
-}
-__GPU__ char*
-ForthVM::scan(char delim) {                          ///< scan input stream for delimiter
-    fin.get_idiom(idiom, delim); return idiom;
-}
 __GPU__ void
 ForthVM::nest() {
     int dp = 0;                                      ///< iterator depth control
@@ -85,148 +68,19 @@ ForthVM::nest() {
 ///
 /// Dictionary compiler proxy macros to reduce verbosity
 ///
-__GPU__ __INLINE__ void ForthVM::add_w(IU w)  {
+__GPU__ void ForthVM::add_w(IU w)  {
     add_iu(w);
     WARN("add_w(%d) => %s\n", w, dict[w].name);
 }
-__GPU__ __INLINE__ void ForthVM::add_iu(IU i) { mmu.add((U8*)&i, sizeof(IU)); }
-__GPU__ __INLINE__ void ForthVM::add_du(DU d) { mmu.add((U8*)&d, sizeof(DU)); }
-__GPU__ __INLINE__ void ForthVM::add_str(const char *s) {
+__GPU__ void ForthVM::add_iu(IU i) { mmu.add((U8*)&i, sizeof(IU)); }
+__GPU__ void ForthVM::add_du(DU d) { mmu.add((U8*)&d, sizeof(DU)); }
+__GPU__ void ForthVM::add_str(const char *s) {
     int sz = STRLENB(s)+1; sz = ALIGN2(sz);          ///> calculate string length, then adjust alignment (combine?)
     mmu.add((U8*)s, sz);
-}
-__GPU__ __INLINE__ void ForthVM::add_tensor(DU n) {
-    DU *d = (DU*)mmu.du2ten(top).data;
-    d[ten_off++] = n;
 }
 __GPU__ __INLINE__ void ForthVM::call(IU w) {
     if (dict[w].def) { WP = w; IP = dict[w].pfa; nest(); }
     else (*(FPTR)((UFP)dict[w].xt & ~0x3))();        ///> execute function pointer (strip off immdiate bit)
-}
-///
-/// tensor methods
-///
-__GPU__ DU
-ForthVM::tadd(bool sub) {
-    if (!IS_TENSOR(ss[-1])) return sub ? ss.pop() - top : ss.pop() + top;
-    
-    Tensor &A = mmu.du2ten(ss[-1]);
-    Tensor &B = mmu.du2ten(top);
-    U16 h = A.H(), w = A.W();
-    if (h == B.H() && w == B.W()) {
-        Tensor &C = mmu.tensor(h, w);
-        Tensor::add(A, B, C, sub);
-        PUSH(C);
-    }
-    else ERROR("dim?");
-    return top;
-}
-/**
-  TODO: Matrix product of two Tensors.
-  The behavior depends on the dimensionality of the Tensors as follows:
-  - DONE: If both arguments are 2-dimensional, the matrix-matrix product is returned.
-  - DONE: If both Tensors are 1-dimensional, the dot product (scalar) is returned.
-  - TODO: If the first argument is 2-dimensional and the second argument is 1-dimensional,
-    the matrix-vector product is returned.
-  - TODO: If the first argument is 1-dimensional and the second argument is 2-dimensional,
-    a 1 is prepended to its dimension for the purpose of the matrix multiply.
-    After the matrix multiply, the prepended dimension is removed.
-  - TODO: If both arguments are at least 1-dimensional and at least one argument is
-    N-dimensional (where N > 2), then a batched matrix multiply is returned.  If the first
-    argument is 1-dimensional, a 1 is prepended to its dimension for the purpose of the
-    batched matrix multiply and removed after.  If the second argument is 1-dimensional, a
-    1 is appended to its dimension for the purpose of the batched matrix multiple and removed after.
-    The non-matrix (i.e. batch) dimensions are broadcasted (and thus
-    must be broadcastable).  For example, if tensor1 is a (j x 1 x n x m) Tensor
-    and tensor2 is a (k x m x p) Tensor, the returned tensor will be an (j x k x n x p) Tensor.
-*/
-__GPU__ DU
-ForthVM::tmul() {                                     ///< tensor multiplication
-    if (!IS_TENSOR(ss[-1])) return top * ss.pop();    /// * scaler * scaler
-    Tensor &A = mmu.du2ten(ss[-1]);
-    U16 m  = A.rank==1 ? 1 : A.H();
-    U16 ka = A.rank==1 ? A.size : A.W();
-    if (!IS_TENSOR(top)) {                            /// * tensor * scaler
-        Tensor &C = mmu.copy(A);                      /// * hard copy A tensor
-        WARN("A[%d,%d]=%p * %f => A'=%p\n", m, ka, &A, top, &C);
-        return mmu.ten2du(C.scale(top));              /// * resultant tensor on TOS
-    }
-    
-    Tensor &B = mmu.du2ten(top);
-    U16 kb = B.rank==1 ? B.size : B.H();
-    U16 n  = B.rank==1 ? 1      : B.W();
-    WARN("A[%d,%d]=%p x B[%d,%d]=%p ", m, ka, &A, kb, n, &B);
-    if (m==1 && n==1) {                               /// * array x array
-        PUSH(A.dot(B));                               /// * dot product on TOS
-        WARN(" => %f\n", top);
-    }
-    else if (ka == kb) {                              /// * tensor x tensor
-        Tensor &C = mmu.tensor(m, n);
-        WARN("=> C[%d,%d]=%p\n", C.H(), C.W(), &C);
-        Tensor::mm(A, B, C);
-        PUSH(mmu.ten2du(C));                          /// * resultant tensor on TOS
-    }
-    else ERROR("dim?");
-    
-    return top;
-}
-__GPU__ DU
-ForthVM::tdiv() {                                     ///< tensor division
-    if (!IS_TENSOR(ss[-1])) {
-        top /= ss.pop();                              /// * scaler / scaler
-        DU_ONLY(top);
-        return top;
-    }
-    Tensor &A = mmu.du2ten(ss[-1]);
-    U16 m  = A.rank==1 ? 1 : A.H();
-    U16 ka = A.rank==1 ? A.size : A.W();
-    if (!IS_TENSOR(top)) {                            /// * tensor / scaler
-        Tensor &C = mmu.copy(A);                      /// * hard copy A tensor
-        WARN("A[%d,%d]=%p / %f => A'=%p\n", m, ka, &A, top, &C);
-        return mmu.ten2du(C.scale(1.0/top));          /// * resultant tensor on TOS
-    }
-    return top;
-    /// TODO: tensor * inverse(tensor)
-}
-__GPU__ DU
-ForthVM::tinv() {                         ///< TODO: tensor inversion
-    return top;
-}
-__GPU__ DU
-ForthVM::ttrans() {
-    Tensor &A = mmu.du2ten(top);
-    U16 h = A.H(), w = A.W();
-    Tensor &B = mmu.tensor(w, h);
-    WARN("A[%d,%d]=%p => B[%d,%d]=%p", h, w, &A, B.H(), B.W(), &B);
-    Tensor::transpose(B, A);
-    PUSH(B);
-    return top;
-}
-__GPU__ void
-ForthVM::gemm() {                       ///< blas GEMM
-    Tensor &C = mmu.du2ten(top);
-    Tensor &B = mmu.du2ten(ss[-1]);
-    Tensor &A = mmu.du2ten(ss[-2]);
-    DU     b  = ss[-3];
-    DU     a  = ss[-4];
-    U16 m = A.H(), k = A.W(), n = B.W();
-    if (k == B.H() && m == C.H() && n == C.W()) {
-        Tensor::gemm(A, B, C, a, b);
-    }
-    else ERROR("dim?");
-}
-///==============================================================================
-///
-/// debug methods
-///
-__GPU__ __INLINE__ void ForthVM::dot(DU v) {
-    if (IS_TENSOR(v)) { fout << v; mmu.mark_free(v); }
-    else fout << " " << v;       // eForth has a space prefix
-}
-__GPU__ __INLINE__ void ForthVM::dot_r(int n, DU v) { fout << setw(n) << v; }
-__GPU__ __INLINE__ void ForthVM::ss_dump(int n) {
-    ss[T4_SS_SZ-1] = top;        // put top at the tail of ss (for host display)
-    fout << opx(OP_SS, n);
 }
 ///
 /// global memory access macros
@@ -237,7 +91,7 @@ __GPU__ __INLINE__ void ForthVM::ss_dump(int n) {
 /// dictionary initializer
 ///
 __GPU__ void
-ForthVM::init() {
+ForthVM::init_f() {
     const Code prim[] = {       /// singleton, build once only
     ///
     ///@defgroup Execution flow ops
@@ -265,35 +119,35 @@ ForthVM::init() {
          while (LDi(ip)) add_iu(LDi(ip))),                // copy&paste code
     CODE(">r",   rs.push(POP())),
     CODE("r>",   PUSH(rs.pop())),
-    CODE("r@",   PUSH(mmu.view(rs[-1]))),
+    CODE("r@",   PUSH(mmu.dup(rs[-1]))),
     ///@}
     ///@defgroup Stack ops
     ///@brief - opcode sequence can be changed below this line
     ///@{
-    CODE("dup",  PUSH(mmu.view(top))),                    // CC: new view created
-    CODE("drop", mmu.free(top); top = ss.pop()),          // free tensor or view
-    CODE("over", PUSH(mmu.view(ss[-1]))),                 // CC: new view created
+    CODE("dup",  PUSH(mmu.dup(top))),                     // CC: new view created
+    CODE("drop", mmu.drop(top); top = ss.pop()),          // free tensor or view
+    CODE("over", PUSH(mmu.dup(ss[-1]))),                  // CC: new view created
     CODE("swap", DU n = ss.pop(); PUSH(n)),
     CODE("rot",  DU n = ss.pop(); DU m = ss.pop(); ss.push(n); PUSH(m)),
-    CODE("pick", int i = INT(top); top = mmu.view(ss[-i])),
+    CODE("pick", int i = INT(top); top = mmu.dup(ss[-i])),
     ///@}
     ///@defgroup Stack double
     ///@{
-    CODE("2dup", PUSH(mmu.view(ss[-1])); PUSH(mmu.view(ss[-1]))),
+    CODE("2dup", PUSH(mmu.dup(ss[-1])); PUSH(mmu.dup(ss[-1]))),
     CODE("2drop",
-         DU s = ss.pop(); mmu.free(s); mmu.free(top);
+         DU s = ss.pop(); mmu.drop(s); mmu.drop(top);
          top = ss.pop()),
-    CODE("2over",PUSH(mmu.view(ss[-3])); PUSH(mmu.view(ss[-3]))),
+    CODE("2over",PUSH(mmu.dup(ss[-3])); PUSH(mmu.dup(ss[-3]))),
     CODE("2swap",
         DU n = ss.pop(); DU m = ss.pop(); DU l = ss.pop();
         ss.push(n); PUSH(l); PUSH(m)),
     ///@}
     ///@defgroup FPU ops
     ///@{
-    CODE("+",    top = tadd()),
-    CODE("*",    top = tmul()),
-    CODE("-",    top = tadd(true)),
-    CODE("/",    top = tdiv()),
+    CODE("+",    top += ss.pop()),
+    CODE("*",    top *= ss.pop()),
+    CODE("-",    top = ss.pop() - top),
+    CODE("/",    top = ss.pop() / top; DU_ONLY(top)),
     CODE("mod",  top =  MOD(ss.pop(), top)),             /// fmod = x - int(q)*y
     CODE("/mod",
         DU n = ss.pop(); DU t = top;
@@ -360,8 +214,8 @@ ForthVM::init() {
     ///@}
     ///@defgroup Literal ops
     ///@{
-    CODE("[", (IS_TENSOR(top) && ten_lvl > 0) ? ++ten_lvl : compile = false),
-    CODE("]", (IS_TENSOR(top) && ten_lvl > 0) ? --ten_lvl : compile = true),
+    CODE("[",       compile = false),
+    CODE("]",       compile = true),
     IMMD("(",       scan(')')),
     IMMD(".(",      fout << scan(')')),
     CODE("\\",      scan('\n')),
@@ -467,81 +321,11 @@ ForthVM::init() {
     CODE("clock", DU t = (DU)(clock64() / khz); PUSH(t)),
     CODE("delay", delay(POPi)),          // TODO: change to VM_WAIT
     ///@}
-    ///@defgroup Tensor creation ops
-    ///@brief - stick to PyTorch naming when possible
-    ///@{
-    CODE("array",                        ///< allocate an array
-        IU sz = POPi;
-        PUSH(mmu.tensor(sz))),
-    CODE("matrix",                       ///< allocate a matrix
-        IU w = POPi; IU h = POPi;
-        PUSH(mmu.tensor(h, w))),
-    CODE("tensor",                       ///< allocate a NHWC tensor
-        IU c = POPi; IU w = POPi; IU h = POPi; IU n = POPi;
-        PUSH(mmu.tensor(n, h, w, c))),
-    CODE("array[",                       ///< create an array with literals
-        IU sz = POPi;
-        PUSH(mmu.tensor(sz));
-        ten_off = 0; ten_lvl = 1),
-    CODE("matrix[",                      ///< create a matrix with literals
-        IU w = POPi; IU h = POPi;
-        PUSH(mmu.tensor(h, w));
-        ten_off = 0; ten_lvl = 1),
-    CODE("copy",    PUSH(mmu.copy(top))),
-    ///@}
-    ///@defgroup Tensor shape ops
-    ///@brief - stick to PyTorch naming when possible
-    ///@{
-    CODE("flatten",                      ///< reshape as an 1-D array
-        Tensor &t = mmu.du2ten(top);
-        t.reshape(t.size)),
-    CODE("reshape2",                     ///< reshape as matrix(h,w)
-        IU w = POPi; IU h = POPi;
-        mmu.du2ten(top).reshape(h, w)),
-    CODE("reshape4",                     ///< reshape as Tensor(NHWC)
-        IU c = POPi; IU w = POPi; IU h = POPi; IU n = POPi;
-        mmu.du2ten(top).reshape(n, h, w, c)),
-    ///@}
-    ///@defgroup Tensor fill ops
-    ///@brief - stick to PyTorch naming when possible
-    ///@{
-    CODE("T![",                          ///< (n -- ) or ( -- )
-         ten_off = IS_TENSOR(top) ? 0 : POPi;
-         ten_lvl = IS_TENSOR(top) ? 1 : 0),
-    CODE("zeros", if (IS_TENSOR(top)) mmu.du2ten(top).fill(0)),
-    CODE("ones",  if (IS_TENSOR(top)) mmu.du2ten(top).fill(1)),
-    CODE("full",  if (IS_TENSOR(ss[-1])) { DU d = POP(); mmu.du2ten(top).fill(d); }),
-    CODE("eye",   {}),                      ///< TODO
-    CODE("rand",  top = mmu.rand(top, UNIFORM)),  ///< uniform randomize a tensor or number
-    CODE("randn", top = mmu.rand(top, NORMAL)),   ///< normal dist. randomize a tensor
-    ///@}
-    ///@defgroup Tensor matrix ops
-    ///@brief - stick to PyTorch naming when possible
-    ///@{
-    CODE("inverse",   tinv()),           ///< (A -- A A')    matrix inversion
-    CODE("transpose", ttrans()),         ///< (A -- A At)    matrix transpose
-    CODE("matmul",    tmul()),           ///< (A B -- A B C) matrix multiplication
-    CODE("gemm",      gemm()),           ///< (a b A B C -- a b A B C') GEMM (C updated)
-    ///@}
     CODE("bye",   status = VM_STOP),
     CODE("boot",  mmu.clear(FIND("boot") + 1))
     };
-    int n  = sizeof(prim)/sizeof(Code); ///< number of primitive entries
-    UFP x0 = ~0;                        ///< base of xt   allocations
-    UFP n0 = ~0;                        ///< base of name allocations
-    for (int i=0; i<n; i++) {
-        mmu.add((Code*)&prim[i]);       ///> TODO: prevent deep copy
-        if ((UFP)dict[i].xt   < x0) x0 = (UFP)dict[i].xt;
-        if ((UFP)dict[i].name < n0) n0 = (UFP)dict[i].name;
-    }
-    for (int i=0; i<n; i++) {           ///< dump dictionary from device
-        WARN("%3d> xt=%4x:%p name=%4x:%p %s\n", i,
-            (U16)((UFP)dict[i].xt   - x0), dict[i].xt,
-            (U16)((UFP)dict[i].name - n0), dict[i].name,
-            dict[i].name);
-    }
-    DEBUG("\\  VM:%p dict[%d] sizeof(Code)=%d sizeof(Tensor)=%d\n",
-        this, n, (int)sizeof(Code), (int)sizeof(Tensor));
+    mmu.merge(prim, sizeof(prim)/sizeof(Code)); ///< append dictionary
+    mmu.status();
 
     status = VM_RUN;
 };
@@ -557,49 +341,44 @@ ForthVM::init() {
 ///    + number() and find() can run in parallel
 ///    - however, find() can run in serial only
 ///
-__GPU__ void
-ForthVM::outer() {
-    while (fin >> idiom) {                   /// loop throught tib
-        WARN("%d>> %-10s => ", blockIdx.x, idiom);
-        int w = FIND(idiom);                 /// * search through dictionary
-        if (w>=0) {                          /// * word found?
-            WARN("%4x:%p %s %d ",
-                dict[w].def ? dict[w].pfa : 0,
-                dict[w].xt, dict[w].name, w);
-            if (compile && !dict[w].immd) {  /// * in compile mode?
-                add_w((IU)w);                /// * add found word to new colon word
-            }
-            else {
-                WARN("=> call(%s)\n", dict[w].name);
-                call((IU)w);                 /// * execute forth word
-            }
-            continue;
-        }
-        // try as a number
-        char *p;
-        DU n = (STRCHR(idiom, '.'))
-                ? STRTOF(idiom, &p)
-                : STRTOL(idiom, &p, radix);
-        if (*p != '\0') {                    /// * not number
-            fout << idiom << "? " << ENDL;   ///> display error prompt
-            compile = false;                 ///> reset to interpreter mode
-            break;                           ///> skip the entire input buffer
-        }
-        // is a number
-        if (compile) {                       /// * add literal when in compile mode
-            WARN("%f\n", n);
-            add_w(DOLIT);                    ///> dovar (+parameter field)
-            add_du(n);                       ///> store literal
-        }
-        else if (ten_lvl > 0) {              /// * append literal into tensor storage
-            WARN("T[%d]=%f\n", ten_off, n);
-            add_tensor(n);
-        }
-        else {                               ///> or, add value onto data stack
-            WARN("ss.push(%08x)\n", *(U32*)&n);
-            PUSH(n);
-        }
+/// parse input idiom as a word
+///
+__GPU__ int
+ForthVM::parse(char *str) {
+    int w = FIND(str);                       /// * search through dictionary
+    if (w < 0) return 0;                     /// * word found!
+    WARN("%4x:%p %s %d ",
+         dict[w].def ? dict[w].pfa : 0,
+         dict[w].xt, dict[w].name, w);
+    if (compile && !dict[w].immd) {  /// * in compile mode?
+        add_w((IU)w);                /// * add found word to new colon word
     }
-    if (!compile) ss_dump(ss.idx);
+    else {
+        WARN("=> call(%s)\n", dict[w].name);
+        call((IU)w);                 /// * execute forth word
+    }
+    return 1;
+}    
+///
+/// parse input idiom as a number
+///
+__GPU__ int
+ForthVM::number(char *str) {
+    char *p;
+    DU n = (STRCHR(idiom, '.'))
+        ? STRTOF(idiom, &p)
+        : STRTOL(idiom, &p, radix);
+    if (*p != '\0') return 0;
+    // is a number
+    if (compile) {                       /// * add literal when in compile mode
+        WARN("%f\n", n);
+        add_w(DOLIT);                    ///> dovar (+parameter field)
+        add_du(n);                       ///> store literal
+    }
+    else {                               ///> or, add value onto data stack
+        WARN("ss.push(%08x)\n", *(U32*)&n);
+        PUSH(n);
+    }
+    return 1;
 }
 //=======================================================================================
--- src/eforth.h
+++ src/eforth.h
@@ -6,63 +6,47 @@
  */
 #ifndef TEN4_SRC_EFORTH_H
 #define TEN4_SRC_EFORTH_H
-#include "vector.h"         // Forth vector (includes util.h)
-#include "tensor.h"
-#include "aio.h"            // Forth async IO (includes Istream, Ostream)
+#include "vm.h"             // VM base class
 ///
 ///@name Cross platform support
 ///@{
-#define ENDL            '\n'
-#define yield()                              /** TODO: multi-VM */
-#define delay(ticks)    { clock_t t = clock()+ticks; while (clock()<t) yield(); }
+#define INT(f)       (static_cast<int>(f + 0.5))   /**< cast float to int              */
+#define I2D(i)       (static_cast<DU>(i))          /**< cast int back to float         */
+#define DU_ONLY(v)   (*(U32*)&(v) &= ~1)           /**< tensor flag mask for top       */
+#define POPi         (INT(POP()))                  /**< convert popped DU as an IU     */
+#define FIND(s)      (mmu.find(s, compile, ucase)) /**< find input idiom in dictionary */
 ///@}
-///
-/// Forth virtual machine class
-///
-typedef enum { VM_READY=0, VM_RUN, VM_WAIT, VM_STOP } vm_status;
 ///
 /// Forth Virtual Machine
 ///
-class ForthVM {
+class ForthVM : public VM {
 public:
-    vm_status status = VM_READY;            ///< VM status
-    DU        top    = DU0;                 ///< cached top of stack
-    Vector<DU,   T4_RS_SZ> rs;              ///< return stack
-    Vector<DU,   0>        ss;              ///< parameter stack (setup in ten4.cu)
-
     __GPU__ ForthVM(int khz, Istream *istr, Ostream *ostr, MMU *mmu);
-    __GPU__ void init();
-    __GPU__ void outer();
 
-private:
-    Istream       &fin;                     ///< VM stream input
-    Ostream       &fout;                    ///< VM stream output
-    MMU           &mmu;                     ///< memory managing unit
-    Code          *dict;                    ///< dictionary array
-
-    int   khz;                              ///< VM clock rate
-    U32   *ptop   = (U32*)&top;             ///< 32-bit mask for top
+    __GPU__ void init_f();                  ///< TODO: CC - polymorphism does not work in kernel?
+    __GPU__ void virtual init() override { init_f(); }
+    
+protected:
+    Code  *dict;                            ///< dictionary array
+    Vector<DU, T4_RS_SZ> rs;                ///< return stack
     
     bool  ucase   = true;                   ///< case insensitive
-    bool  compile = false;                  ///< compiling flag
     int   radix   = 10;                     ///< numeric radix
     IU    WP      = 0;                      ///< word and parameter pointers
     IU    IP      = 0;                      ///< instruction pointer
-
-    char  idiom[T4_STRBUF_SZ];              ///< terminal input buffer
-    int   ten_lvl = 0;                      ///< tensor input level
-    int   ten_off = 0;                      ///< tensor offset (array index)
-
+    ///
+    /// stack short hands
+    ///
     __GPU__ __INLINE__ DU POP()             { DU n=top; top=ss.pop(); return n; }
     __GPU__ __INLINE__ void PUSH(DU v)      { ss.push(top); top = v; }
     __GPU__ __INLINE__ void PUSH(Tensor &t) { ss.push(top); top = mmu.ten2du(t); }
-
-    __GPU__ int  find(const char *s);       ///< search dictionary reversely
     ///
     /// Forth inner interpreter
     ///
-    __GPU__ char *next_idiom();
-    __GPU__ char *scan(char c);
+    __GPU__ int  virtual parse(char *str) override;  ///< TODO: CC - this worked, why?
+    __GPU__ int  virtual number(char *str) override; ///< TODO: CC - this worked, why?
+
+    __GPU__ int  find(const char *s);       ///< search dictionary reversely
     __GPU__ void nest();
     ///
     /// compiler proxy funtions to reduce verbosity
@@ -71,22 +55,6 @@ private:
     __GPU__ void add_iu(IU i);              ///< append an instruction unit to parameter memory
     __GPU__ void add_du(DU d);              ///< append a data unit to pmem
     __GPU__ void add_str(const char *s);    ///< append a string to pmem
-    __GPU__ void add_tensor(DU d);          ///< append a literal into tensor storage
     __GPU__ void call(IU w);                ///< execute word by index
-    ///
-    /// tensor methods
-    ///
-    __GPU__ DU   tadd(bool sub=false);      ///< matrix-matrix addition (or subtraction)
-    __GPU__ DU   tmul();                    ///< matrix multiplication (no broadcast)
-    __GPU__ DU   tdiv();                    ///< matrix division (no broadcast)
-    __GPU__ DU   tinv();                    ///< TODO: matrix inverse (Gaussian Elim.?)
-    __GPU__ DU   ttrans();                  ///< matrix transpose
-    __GPU__ void gemm();                    ///< GEMM C' = alpha * A x B + beta * C
-    ///
-    /// output methods
-    ///
-    __GPU__ void dot(DU v);
-    __GPU__ void dot_r(int n, DU v);
-    __GPU__ void ss_dump(int n);
 };
 #endif // TEN4_SRC_EFORTH_H
