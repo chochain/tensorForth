95b5efbe043160d989caf9a0dd16baceb7538281
gnii <chochain@yahoo.com>
2022-07-11 19:59:04 -0700

    ~/src/tenvm.cu change tensor methods to void (update top by default); IS_TENSOR=>IS_TEN; use VLOG to trace; NO_OBJ(literal)

--- src/tenvm.cu
+++ src/tenvm.cu
@@ -6,27 +6,39 @@
  */
 #include "tenvm.h"
 
-__GPU__ void TensorVM::add_tensor(DU n) {
+#if T4_ENABLE_OBJ
+__GPU__ void
+TensorVM::tprint(DU d) {
+    if (IS_OBJ(d)) { fout << d; mmu.mark_free(d); }
+    else fout << " " << d;                  /// eForth has a space prefix
+}
+__GPU__ void TensorVM::add_to_tensor(DU n) {
     DU *d = (DU*)mmu.du2ten(top).data;
     d[ten_off++] = n;
 }
 ///
 /// tensor methods
 ///
-__GPU__ DU
+__GPU__ void
 TensorVM::texp() {
-    if (!IS_TENSOR(top)) return EXP(top);    /// * scaler
+    if (!IS_TEN(top)) {                      /// * scalar
+        top = EXP(top); NO_OBJ(top);         /// * mask off object-bit if any
+        return;
+    }
     Tensor &A = mmu.du2ten(top);
     Tensor &B = mmu.copy(A);
     DU *d = (DU*)B.data;
-    for (int i=0; i<B.size; i++, d++) *d = EXP(*d);
+    for (int i=0; i < B.size; i++, d++) {    ///> TODO: CDP
+        *d = EXP(*d);
+    }
     PUSH(B);
-    return top;
 }
-__GPU__ DU
+__GPU__ void
 TensorVM::tadd(bool sub) {
-    if (!IS_TENSOR(ss[-1])) return sub ? ss.pop() - top : ss.pop() + top;
-
+    if (!IS_TEN(ss[-1])) {
+        top = sub ? ss.pop() - top : ss.pop() + top;
+        return;
+    }
     Tensor &A = mmu.du2ten(ss[-1]);
     Tensor &B = mmu.du2ten(top);
     U16 h = A.H(), w = A.W();
@@ -36,7 +48,6 @@ TensorVM::tadd(bool sub) {
         PUSH(C);
     }
     else ERROR("dim?");
-    return top;
 }
 /**
   TODO: Matrix product of two Tensors.
@@ -57,63 +68,63 @@ TensorVM::tadd(bool sub) {
     must be broadcastable).  For example, if tensor1 is a (j x 1 x n x m) Tensor
     and tensor2 is a (k x m x p) Tensor, the returned tensor will be an (j x k x n x p) Tensor.
 */
-__GPU__ DU
+__GPU__ void
 TensorVM::tmul() {                                    ///< tensor multiplication
-    if (!IS_TENSOR(ss[-1])) return top * ss.pop();    /// * scaler * scaler
-    
+    if (!IS_TEN(ss[-1])) {
+        top *= ss.pop();                              /// * scaler * scaler
+        return;
+    }
     Tensor &A = mmu.du2ten(ss[-1]);
     if (!IS_OBJ(top)) {                               /// * tensor * scaler
         Tensor &C = mmu.copy(A);                      /// * hard copy A tensor
-        WARN("T%d=%p * %f => A'=%p\n", A.rank, &A, top, &C);
-        return mmu.ten2du(C.scale(top));              /// * resultant tensor on TOS
+        VLOG2("T%d=%p * %f => A'=%p\n", A.rank, &A, top, &C);
+        top = mmu.ten2du(C.scale(top));               /// * resultant tensor on TOS
+        return;
     }
     
     Tensor &B = mmu.du2ten(top);
     U16 m  = A.H(), ka = A.W(), kb = B.H(), n  = B.W();
-    WARN("A[%d,%d]=%p x B[%d,%d]=%p ", m, ka, &A, kb, n, &B);
+    VLOG2("A[%d,%d]=%p x B[%d,%d]=%p ", m, ka, &A, kb, n, &B);
     if (A.rank==1 && B.rank==1 && A.size==B.size) {   /// * array x array
         PUSH(A.dot(B));                               /// * dot product on TOS
-        WARN(" => %f\n", top);
+        VLOG2(" => %f\n", top);
     }
     else if (ka == kb) {                              /// * tensor x tensor
         Tensor &C = mmu.tensor(m, n);
         Tensor::mm(A, B, C);
         PUSH(mmu.ten2du(C));                          /// * resultant tensor on TOS
-        WARN("=> C[%d,%d]=%p\n", C.H(), C.W(), &C);
+        VLOG2("=> C[%d,%d]=%p\n", C.H(), C.W(), &C);
     }
     else ERROR("dim?");
-
-    return top;
 }
-__GPU__ DU
+__GPU__ void
 TensorVM::tdiv() {                                     ///< tensor division
-    if (!IS_TENSOR(ss[-1])) {
-        top /= ss.pop();                              /// * scaler / scaler
-        DU_ONLY(top);
-        return top;
+    if (!IS_TEN(ss[-1])) {
+        top = ss.pop() / top;                          /// * scaler / scaler
+        NO_OBJ(top);
+        return;
     }
     Tensor &A = mmu.du2ten(ss[-1]);
-    if (!IS_TENSOR(top)) {                            /// * tensor / scaler
+    if (!IS_TEN(top)) {                               /// * tensor / scaler
         Tensor &C = mmu.copy(A);                      /// * hard copy A tensor
-        WARN("A[%d,%d]=%p / %f => A'=%p\n", A.H(), A.W(), &A, top, &C);
-        return mmu.ten2du(C.scale(1.0/top));          /// * resultant tensor on TOS
+        VLOG2("A[%d,%d]=%p / %f => A'=%p\n", A.H(), A.W(), &A, top, &C);
+        top = mmu.ten2du(C.scale(1.0/top));           /// * resultant tensor on TOS
+        return;
     }
-    return top;
     /// TODO: tensor * inverse(tensor)
 }
-__GPU__ DU
-TensorVM::tinv() {                         ///< TODO: tensor inversion
-    return top;
+__GPU__ void
+TensorVM::tinv() {
+    ///< TODO: tensor inversion
 }
-__GPU__ DU
+__GPU__ void
 TensorVM::ttrans() {
     Tensor &A = mmu.du2ten(top);
     U16 h = A.H(), w = A.W();
     Tensor &B = mmu.tensor(w, h);
-    WARN("A[%d,%d]=%p => B[%d,%d]=%p", h, w, &A, B.H(), B.W(), &B);
+    VLOG2("A[%d,%d]=%p => B[%d,%d]=%p", h, w, &A, B.H(), B.W(), &B);
     Tensor::transpose(B, A);
     PUSH(B);
-    return top;
 }
 __GPU__ void
 TensorVM::gemm() {                       ///< blas GEMM
@@ -124,12 +135,14 @@ TensorVM::gemm() {                       ///< blas GEMM
     DU     a  = ss[-4];
     U16 m = A.H(), k = A.W(), n = B.W();
     if (k == B.H() && m == C.H() && n == C.W()) {
-        Tensor::gemm(A, B, C, a, b);
+        Tensor &D = mmu.copy(C);         /// * hard copy C tensor
+        Tensor::gemm(A, B, D, a, b);
+        PUSH(D);
     }
     else ERROR("dim?");
 }
 ///
-/// dictionary initializer
+/// Tensor specific dictionary constructor
 ///
 __GPU__ void
 TensorVM::init_t() {
@@ -173,26 +186,26 @@ TensorVM::init_t() {
     ///@brief - stick to PyTorch naming when possible
     ///@{
     CODE("={",                          ///< (n -- ) or ( -- )
-         ten_off = IS_TENSOR(top) ? 0 : POPi;
-         ten_lvl = IS_TENSOR(top) ? 1 : 0),
-    CODE("zeros", if (IS_TENSOR(top)) mmu.du2ten(top).fill(0)),
-    CODE("ones",  if (IS_TENSOR(top)) mmu.du2ten(top).fill(1)),
-    CODE("full",  if (IS_TENSOR(ss[-1])) { DU d = POP(); mmu.du2ten(top).fill(d); }),
-    CODE("eye",   if (IS_TENSOR(top)) mmu.du2ten(top).identity()),
+         ten_off = IS_TEN(top) ? 0 : POPi;
+         ten_lvl = IS_TEN(top) ? 1 : 0),
+    CODE("zeros", if (IS_TEN(top)) mmu.du2ten(top).fill(0)),
+    CODE("ones",  if (IS_TEN(top)) mmu.du2ten(top).fill(1)),
+    CODE("full",  if (IS_TEN(ss[-1])) { DU d = POP(); mmu.du2ten(top).fill(d); }),
+    CODE("eye",   if (IS_TEN(top)) mmu.du2ten(top).identity()),
     CODE("rand",  top = mmu.rand(top, UNIFORM)),  ///< uniform randomize a tensor or number
     CODE("randn", top = mmu.rand(top, NORMAL)),   ///< normal dist. randomize a tensor
     ///@defgrup Tensor slice and dice
     ///@{
     CODE("sum",
-        if (IS_TENSOR(top)) {
+        if (IS_TEN(top)) {
             DU d =  mmu.du2ten(top).sum();
             PUSH(d);
         }),
-    CODE("{",   if (IS_TENSOR(top) && ten_lvl > 0) ++ten_lvl),
-    CODE("}",   if (IS_TENSOR(top) && ten_lvl > 0) --ten_lvl),
+    CODE("{",   if (IS_TEN(top) && ten_lvl > 0) ++ten_lvl),
+    CODE("}",   if (IS_TEN(top) && ten_lvl > 0) --ten_lvl),
     CODE("slice",
          IU y1 = POPi; IU y0 = POPi; IU x1 = POPi; IU x0 = POPi;
-         if (IS_TENSOR(top)) {
+         if (IS_TEN(top)) {
              Tensor &t0 = mmu.du2ten(top);
              Tensor &t1 = mmu.slice(t0, x0, x1, y0, y1);
              PUSH(t1);
@@ -202,20 +215,21 @@ TensorVM::init_t() {
     ///@defgroup Tensor matrix ops
     ///@brief - stick to PyTorch naming when possible
     ///@{
-    CODE("inverse",   tinv()),           ///< (A -- A A')    matrix inversion
-    CODE("transpose", ttrans()),         ///< (A -- A At)    matrix transpose
-    CODE("matmul",    tmul()),           ///< (A B -- A B C) matrix multiplication
-    CODE("gemm",      gemm()),           ///< (a b A B C -- a b A B C') GEMM (C updated)
+    CODE("exp",       texp()),     ///< (A -- A A')    matrix exponential
+    CODE("inverse",   tinv()),     ///< (A -- A A')    matrix inversion
+    CODE("transpose", ttrans()),   ///< (A -- A At)    matrix transpose
+    CODE("matmul",    tmul()),     ///< (A B -- A B C) matrix multiplication
+    CODE("gemm",      gemm()),     ///< (a b A B C -- a b A B C') GEMM (C updated)
     ///@}
     };
     const Code old[] = {
     ///@defgroup redefined tensor ops
     ///@{
-    CODE("+",   top = tadd()),
-    CODE("*",   top = tmul()),
-    CODE("-",   top = tadd(true)),
-    CODE("/",   top = tdiv()),
-    CODE("exp", top = texp()),
+    CODE(".",   tprint(POP())),
+    CODE("+",   tadd()),
+    CODE("*",   tmul()),
+    CODE("-",   tadd(true)),
+    CODE("/",   tdiv()),
     ///@}
     CODE("boot", mmu.clear(FIND("gemm") + 1))
     };
@@ -237,18 +251,20 @@ TensorVM::number(char *str) {
         ? STRTOF(idiom, &p)
         : STRTOL(idiom, &p, radix);
     if (*p != '\0') return 0;
+    NO_OBJ(n);                           /// * mask out object bit
     if (compile) {                       /// * add literal when in compile mode
-        WARN("%f\n", n);
+        VLOG2("%f\n", n);
         add_w(DOLIT);                    ///> dovar (+parameter field)
         add_du(n);                       ///> store literal
     }
     else if (ten_lvl > 0) {              /// * append literal into tensor storage
-        WARN("T[%d]=%f\n", ten_off, n);
-        add_tensor(n);
+        VLOG2("T[%d]=%f\n", ten_off, n);
+        add_to_tensor(n);
     }
     else {                               ///> or, add value onto data stack
-        WARN("ss.push(%08x)\n", *(U32*)&n);
+        VLOG2("ss.push(%08x)\n", *(U32*)&n);
         PUSH(n);
     }
 }
+#endif  // T4_ENABLE_OBJ
 //=======================================================================================
