39c0ea5720520563ecda2cf52bc0e626455623bf
gnii <chochain@yahoo.com>
2022-07-28 10:40:44 -0700

    ~/tests/t_inverse add det; fix lu inversion

--- tests/t_inverse.cu
+++ tests/t_inverse.cu
@@ -12,14 +12,16 @@ void dump(DU A[3][3], DU I[3][3], DU P[3], int sz) {
 int  find_max(DU A[3][3], int z, int sz) {
     int u = z;
     /* finding maximum xth column element in last (sz-x) rows */
+    /*
     for (int y = z + 1; y < sz; y++) {
         if (fabs(A[y][z]) > fabs(A[u][z])) u = y;
     }
+    */
     if (fabs(A[u][z]) < DU_EPS) {
         printf("sigular !!!\n");
         return -1;
     }
-    else printf("A[%d][%d]=%f\n", u, z, A[u][z]);
+    else printf("max A[%d][%d]=%f\n", u, z, A[u][z]);
     
     return u;
 }
@@ -71,77 +73,98 @@ int gj(DU A[3][3], DU I[3][3], int sz) {
 // A^-1 = (U)^-1(L)^-1(P)^-1
 // A^-1 = (U)^-1(L)^-1(P)t
 //
-void h_tri(DU A[3][3], int z, int sz) {
+void h_elim(DU A[3][3], int z, int sz) { // outer product gauss elimination
     float ra = A[z][z];
-    if (fabs(ra) < DU_EPS) return;     // if 0 skip the row
+    if (fabs(ra) < DU_EPS) return;       // skip row that is 0
     for (int y = z + 1; y < sz; y++) {
-        float r1 = -A[y][z] / ra;
+        float r1 = A[y][z] / ra;
         for (int k = z; k < sz; k++) {
-            A[y][k] += r1 * A[z][k];
+            A[y][k] -= r1 * A[z][k];
         }
-        A[y][z] = r1;                  // L^-1 store in U to save space
+        A[y][z] = r1;                    // L store in U to save space
     }
 }
-int plu(DU A[3][3], DU P[3], int sz) {
+int lu(DU A[3][3], DU P[3], int sz) {    // LU with permutation
     DU I[3][3] = {{1,0,0},{0,1,0},{0,0,1}}; // dummy
     printf("PLU = A\n");
 	for (int z = 0; z < sz; z++) {
         printf("==== %d\n", z);
         dump(A, I, P, sz);
-        int u = find_max(A, z, sz);    // pivot to reduce rounding error
-        if (u < 0) return -1;
-		if (u != z) { 	               // swapping row which has maximum xth column element
+        int u = find_max(A, z, sz);      // pivot to reduce rounding error
+        if (u < 0) return -1;            // singular matrix
+		if (u != z) { 	                 // swapping row which has maximum xth column element
             swap_row(A, z, u, z, sz);
             DU t = P[z]; P[z] = P[u]; P[u] = t;
         }
         dump(A, I, P, sz);
-        h_tri(A, z, sz);
+        h_elim(A, z, sz);
 	}
     return 0;
 }
 ///
 /// LU inversion
 ///
-int h_lu_inv(DU A[3][3], DU I[3][3], int z, int sz) {
-    float r0 = A[z][z]; A[z][z]  = 1.0;
-    if (fabs(r0) < DU_EPS) return -1;             // singular
-    
-    for (int k = z; k < sz; k++) I[z][k] /= r0;   // current z row
-    for (int y = 0; y < z; y++) {                 // factorize rows above
-        float r1 = A[y][z]; A[y][z] = 0.0;
-        for (int k = z; k < sz; k++) {
+float h_det(DU A[3][3], int sz) {
+    float d = 1.0;
+    for (int y = 0; y < sz; y++) {
+        d *= A[y][y];
+    }
+    printf("LU det= %f\n", d);
+    return d;
+}
+void h_forward(DU A[3][3], DU I[3][3], int z, int sz) {
+    // lower triangle forward substitution
+    for (int y = z + 1; y < sz; y++) {              //
+        float r1 = A[y][z];
+        A[y][z] = 0.0; I[y][z] = -r1;               // current z column
+        for (int k = 0; k < z; k++) {
             I[y][k] -= I[z][k] * r1;
         }
     }
-    return 0;
+}
+void h_backward(DU A[3][3], DU I[3][3], int z, int sz) {
+    // upper triangle backward substitution
+    float r0 = 1.0/A[z][z];
+    A[z][z] = 1.0; I[z][z] = r0;                    // diag
+    for (int k = z + 1; k < sz; k++) {              // current z row
+        I[z][k] *= r0;
+    }
+    for (int y = 0; y < z; y++) {                   // factorize rows above
+        float r1 = A[y][z];
+        A[y][z] = 0.0; I[y][z] = -r1 * r0;          // current z column
+        for (int k = z + 1; k < sz; k++) {
+            I[y][k] -= I[z][k] * r1;
+        }
+    }
 }
 int lu_inv(DU A[3][3], DU I[3][3], DU P[3], int sz) {
-    DU X[3] = { 0, 0, 0 };
-    /// A => L^-1, I => U^-1
-    for (int z = sz-1; z >= 0; z--) {
-        printf("LU inv==== %d\n", z);
-        dump(A, I, X, sz);
-        
-        if (h_lu_inv(A, I, z, sz) < 0) {
-            printf("lu_inv sigular!!!\n");
-            return -1;
-        }
+    if (fabs(h_det(A, sz)) < DU_EPS) return -1;  // singular
+
+    /// I += L^-1
+    for (int z = 0; z < sz - 1; z++) {           // forward lower triangle
+        printf("forward ==== %d\n", z);
+        h_forward(A, I, z, sz);                       
+        dump(A, I, P, sz);
+    }
+    /// I += U^-1
+    for (int z = sz-1; z >= 0; z--) {            // backward upper triangle
+        printf("backward ==== %d\n", z);
+        h_backward(A, I, z, sz);
+        dump(A, I, P, sz);
     }
     /// A^-1 = (U^-1)(L^-1)(PI), i.e. use P to swap_rows
     return 0;
 }
 int main(int argc, char **argv) {
-    DU A[3][3] = {{1,1,2},{2,1,1},{1,2,1}};
+//    DU A[3][3] = {{1,1,2},{2,1,1},{1,2,1}};
 //    DU A[3][3] = {{2,2,5},{1,1,1},{4,6,8}};
-//    DU A[3][3] = {{6,18,-12},{2,4,-2},{3,17,10}};
+    DU A[3][3] = {{1, 2, 4},{3, 8, 14},{2, 6, 13}};
     DU I[3][3] = {{1,0,0},{0,1,0},{0,0,1}};
     DU P[3]    = { 0, 1, 2 };               // compressed sparse matrix
 
 //    gj(A, I, 3);
-    plu(A, P, 3);
+    lu(A, P, 3);
     lu_inv(A, I, P, 3);
-    printf("final=====\n");
-    dump(A, I, P, 3);
 
     return 0;
 }
