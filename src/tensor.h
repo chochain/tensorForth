2b4318e44d30958c50b2b6856c3c81530e4be64e
gnii <chochain@yahoo.com>
2022-07-30 13:43:44 -0700

    ~/src/tensor.h add tensor-scalar add/sub

--- src/tensor.h
+++ src/tensor.h
@@ -40,6 +40,7 @@ struct Tensor : public Managed {
     static __BOTH__ Tensor &grad(Tensor &A, Tensor &B, Tensor &C);
     static __BOTH__ Tensor &mm(Tensor &A, Tensor &B, Tensor &C) { return gemm(A, B, C, 1.0, 0.0); }
     static __BOTH__ Tensor &add(Tensor &A, Tensor &B, Tensor &C, bool sub=0);
+    static __BOTH__ Tensor &add(Tensor &A, DU v, Tensor &C, bool sub=0);
     static __BOTH__ Tensor &copy(Tensor &A, Tensor &C);
     static __BOTH__ Tensor &transpose(Tensor &A, Tensor &T);
     static __BOTH__ Tensor &inverse(Tensor &A, Tensor &I);  /// GaussJordan (with Pivot)
