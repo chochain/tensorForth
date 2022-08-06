3554617aa0f8bd234d36417c1ba9a87ef2f1fa7e
gnii <chochain@yahoo.com>
2022-08-05 19:32:12 -0700

    ~/src/tenvm.h override by netvm class

--- src/tenvm.h
+++ src/tenvm.h
@@ -27,12 +27,12 @@ public:
         ForthVM(khz, istr, ostr, mmu0) {
         VLOG1("\\  ::TensorVM(...) sizeof(Tensor)=%ld\n", sizeof(Tensor));
     }
-    __GPU__ void init() final { init_t(); } ///< TODO: CC - polymorphism does not work here?
-    __GPU__ void init_t();                  ///< so fake it
+    __GPU__ void virtual init() override { init_t(); }  ///< TODO: CC - polymorphism does not work here?
+    __GPU__ void init_t();                              ///< so fake it
     
 protected:
-    int   ten_lvl = 0;                      ///< tensor input level
-    int   ten_off = 0;                      ///< tensor offset (storage index)
+    int    ten_lvl = 0;                     ///< tensor input level
+    int    ten_off = 0;                     ///< tensor offset (storage index)
     ///
     /// override literal handler
     ///
