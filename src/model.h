ad946a57bd78040059d762cef78daf9dcdcb50e9
gnii <chochain@yahoo.com>
2022-08-15 14:38:28 -0700

    ~/src/model.h drop nten; make init, derivatives to private section; add forward/backprop

--- src/model.h
+++ src/model.h
@@ -22,16 +22,10 @@ class Model : public T4Base {
     
 public:
     bool    autograd = true;
-    Tensor  *nten;       ///< cached tensor pointer
     ///
     /// @name Derivertive ops
     /// @{
     static __HOST__ const char* nname(int n);  /// network layer name
-    static __GPU__ void dconv2d(Tensor &A, Tensor &B)  {}
-    static __GPU__ void drelu(Tensor &A, Tensor &B)    {}
-    static __GPU__ void dmaxpool(Tensor &A, Tensor &B) {}
-    static __GPU__ void dflatten(Tensor &A, Tensor &B) {}
-    static __GPU__ void dlinear(Tensor &A, Tensor &B)  {}
     /// @}
     __BOTH__ __INLINE__ Tensor &operator[](int i) {
         return _mmu->du2ten(data[i]);
@@ -47,47 +41,46 @@ public:
         autograd = true;
         npush(store);            // keep store as root
     }
-    __BOTH__ __INLINE__ DU npop() {
-        DU n = data[--numel];
-        nten = &_mmu->du2ten(n);
-        return n;
-    }
-    __BOTH__ __INLINE__ Model &npush(DU v) {
-        nten = &_mmu->du2ten(v);
-        data[numel++] = v;
-        return *this;
-    }
-    __BOTH__ __INLINE__ Model &npush(Tensor &t) {
-        nten = &t;
-        data[numel++] = _mmu->ten2du(t);
-        return *this;
-    }
+    __BOTH__ __INLINE__ DU npop()               { return data[--numel]; }
+    __BOTH__ __INLINE__ Model &npush(DU v)      { data[numel++] = v; return *this; }
+    __BOTH__ __INLINE__ Model &npush(Tensor &t) { return npush(_mmu->ten2du(t)); }
     __GPU__ __INLINE__ Tensor &tensor(U16 n, U16 h, U16 w, U16 c) {
         return _mmu->tensor(n, h, w, c);
     }
     __GPU__ __INLINE__ Tensor &vector(U16 n) {
         return _mmu->tensor(n);
     }
+    __GPU__ Model &add(t4_layer fn, U16 n=0, DU bias=DU0, U16 *opt=0);
+    __GPU__ Model &forward(Tensor &input);
+    __GPU__ Model &backprop(Tensor &output);
+
+private:
+    __GPU__ void _step(Tensor &in, Tensor &out);
+    
     /// @name Convolution and Linear initializer
     /// @{
-    __GPU__ Model &iconv2d(DU bias, U16 c, U16 *opt);
-    __GPU__ Model &ilinear(DU bias, U16 n);   ///< linearize (Dense) with n output
-    __GPU__ Model &iflatten();      ///< flatten (input 
+    __GPU__ void _iconv2d(Tensor &in, U16 c, DU bias, U16 *opt);
+    __GPU__ void _ilinear(Tensor &in, U16 n, DU bias);   ///< linearize (Dense) with n output
+    __GPU__ void _iflatten(Tensor &in);      ///< flatten (input 
     /// @}
     /// @name Activation ops
     /// @{
-    __GPU__ Model &irelu();         ///< Rectified Linear Unit
-    __GPU__ Model &itanh();         ///< Tanh Unit
-    __GPU__ Model &isigmoid();      ///< 1/(1+exp(-z))
-    __GPU__ Model &isoftmax();      ///< probability vector exp(x)/sum(exp(x))
+    __GPU__ void _irelu(Tensor &in);         ///< Rectified Linear Unit
+    __GPU__ void _itanh(Tensor &in);         ///< Tanh Unit
+    __GPU__ void _isigmoid(Tensor &in);      ///< 1/(1+exp(-z))
+    __GPU__ void _isoftmax(Tensor &in);      ///< probability vector exp(x)/sum(exp(x))
     /// @}
     /// @name Pooling and Dropout ops
     /// @{
-    __GPU__ Model &imaxpool(U16 n); ///< maximum pooling with nxn filter
-    __GPU__ Model &iavgpool(U16 n); ///< average pooling with nxn filter
-    __GPU__ Model &iminpool(U16 n); ///< minimum pooling with nxn filter
-    __GPU__ Model &idropout(U16 p); ///< zero out p% of channel data (add noise between data points)
+    __GPU__ void _imaxpool(Tensor &in, U16 n); ///< maximum pooling with nxn filter
+    __GPU__ void _iavgpool(Tensor &in, U16 n); ///< average pooling with nxn filter
+    __GPU__ void _iminpool(Tensor &in, U16 n); ///< minimum pooling with nxn filter
+    __GPU__ void _idropout(Tensor &in, U16 p); ///< zero out p% of channel data (add noise between data points)
     /// @}
-    __GPU__ void  step(Tensor &in, Tensor &out);
+    __GPU__ void _dconv2d(Tensor &A, Tensor &B)  {}
+    __GPU__ void _dlinear(Tensor &A, Tensor &B)  {}
+    __GPU__ void _dflatten(Tensor &A, Tensor &B) {}
+    __GPU__ void _drelu(Tensor &A, Tensor &B)    {}
+    __GPU__ void _dmaxpool(Tensor &A, Tensor &B) {}
 };
 #endif // TEN4_SRC_MODEL_H
