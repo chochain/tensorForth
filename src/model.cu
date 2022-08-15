f77cb7fbc000f05f736adfe4bbb7614af72eb7d6
gnii <chochain@yahoo.com>
2022-08-15 14:39:48 -0700

    ~/src/model.cu refactor add(layer factory); forward from netvm

--- src/model.cu
+++ src/model.cu
@@ -36,7 +36,7 @@ __KERN__ void k_conv2d(          ///< TODO: C
             int d0 = tx + (y + ty) * Z;     ///< offset to smem block
             int b0 = y * K;
             for (int x = 0; x < K; x++) {
-                sum += d[x + d0] * F[x + b0] + B[x + b0]; 
+                sum += F[x + b0] * d[x + d0] + B[x]; 
             }
         }
         if (i0 < M && j0 < N) {             /// * update C[i][j]
@@ -106,139 +106,69 @@ Model::nname(int i) {               ///< network layer name
     return name[i];
 }
 ///
-/// Convolution and Linear ops
+/// NN layer factory
 ///
 __GPU__ Model&
-Model::iconv2d(DU bias, U16 c, U16 *opt) {
-    if (NO_INIT) return *this;
-    Tensor &in = *nten; in.grad_fn = L_CONV2D;   ///> derivative function
+Model::add(t4_layer fn, U16 n, DU bias, U16 *opt) {
+    Tensor &in = _mmu->du2ten(data[numel - 1]);
+    if (!autograd || in.grad_fn != L_NONE) return *this;
+    
+    switch(fn) {
+    case L_CONV2D:  _iconv2d(in, n, bias, opt); break;
+    case L_LINEAR:  _ilinear(in, n, bias);      break;
+    case L_FLATTEN: _iflatten(in);              break;
+    case L_RELU:    _irelu(in);                 break;
+    case L_TANH:    _itanh(in);                 break;
+    case L_SIGMOID: _isigmoid(in);              break;
+    case L_SOFTMAX: _isoftmax(in);              break;
+    case L_MAXPOOL: _imaxpool(in, n);           break;
+    case L_AVGPOOL: _iavgpool(in, n);           break;
+    case L_MINPOOL: _iminpool(in, n);           break;
+    case L_DROPOUT: _idropout(in, n);           break;
+    }
+    in.grad_fn = fn;
+    return *this;
+}
 
-    U16 m = opt[0], n = opt[1];                  ///> filter sizing
-    U16 p = opt[2] ? opt[2] : int((m-1)/2);      ///> padding
-    U16 s = opt[3], d = opt[4];                  ///> stride, dilation
-
-    Tensor *f  = in.grad[0] = &tensor(1, m, n, c);                   ///> f
-    Tensor *b  = in.grad[1] = &tensor(1, m, n, 1).map(O_FILL, bias); ///> b
-    Tensor *df = in.grad[2] = &tensor(1, m, n, c).map(O_FILL, DU0);  ///> df
-    Tensor *db = in.grad[3] = &tensor(1, m, n, 1).map(O_FILL, DU0);  ///> db
-    _mmu->random(*f, NORMAL);                    /// * randomize f
-    
-    Tensor &out = tensor(                        ///> output tensor sizing
-        1,
-        in.H() + 2 * (p - int(m/2)),
-        in.W() + 2 * (p - int(n/2)),
-        c).map(O_FILL, DU0);
-    npush(out);                                  /// * stage for next stage
+__GPU__ Model&
+Model::forward(Tensor &input) {
+    Tensor &in = (*this)[1];
+    if (!in.is_same_shape(input)) {
+        ERROR("model#forward dim?\n");
+        return *this;
+    }
+    Tensor::copy(input, in);
+    /*
+    for (int i = 2; i < (model.numel - 1); i++) {
+        Tensor &out = model[i];
+        model.forward(in, out);
+        in = out;
+    }
+    */
     return *this;
 }
 __GPU__ Model&
-Model::ilinear(DU bias, U16 n) {
-    if (NO_INIT) return *this;
-    Tensor &in = *nten; in.grad_fn = L_LINEAR;   ///> derivative function
-
-    U16 m = in.H();
-    Tensor *w  = in.grad[0] = &tensor(1, n, m, 1);                   ///> w
-    Tensor *dw = in.grad[2] = &tensor(1, n, m, 1).map(O_FILL, DU0);  ///> dw
-    Tensor *b  = in.grad[1] = &vector(n).map(O_FILL, bias);          ///> b
-    Tensor *db = in.grad[3] = &vector(n).map(O_FILL, DU0);           ///> db
-    _mmu->random(*w, NORMAL);                    /// * randomize w
-    
-    Tensor &out = vector(n);                     ///> output tensor sizing
-    npush(out);                                  /// * stage for next stage
+Model::backprop(Tensor &output) {
     return *this;
 }
-__GPU__ Model&
-Model::iflatten() {
-    if (NO_INIT) return *this;
-    Tensor &in  = *nten;
-    Tensor &out = vector(in.numel);
-    in.grad_fn  = L_FLATTEN;
-    in.parm     = in.numel;
-    npush(out);
-    return *this;
-}
-///
-/// Activation ops
-///
-__GPU__ Model&
-Model::irelu() {
-    if (NO_INIT) return *this;
-    Tensor &in  = *nten;
-    Tensor &out = _mmu->copy(in); ///> output tensor sizing
-    in.grad_fn  = L_RELU;
-    npush(out);                   /// * stage for next stage
-    return *this;
-}
-__GPU__ Model&
-Model::itanh() {
-    return *this;
-}
-__GPU__ Model&
-Model::isigmoid() {
-    return *this;
-}
-__GPU__ Model&
-Model::isoftmax() {
-    if (NO_INIT) return;
-    Tensor &in  = *nten;
-    Tensor &out = _mmu->copy(in); ///> output tensor sizing
-    in.grad_fn  = L_SOFTMAX;
-    npush(out);                   /// * stage for next stage
-    return *this;
-}
-///
-/// Pooling and Dropout ops
-///
-__GPU__ Model&
-Model::imaxpool(U16 f) {
-    if (NO_INIT) return *this;
-    Tensor &in = *nten; in.grad_fn = L_MAXPOOL;
-    in.parm    = f;              /// * keep pooling width
-    
-    U16 m = int((in.H() - f) / f) + 1;
-    U16 n = int((in.W() - f) / f) + 1;
-    U16 s[4] = { 1, f, f, 1 }; memcpy(in.stride, s, sizeof(s));  // stride
-    
-    Tensor &out = tensor(1, m, n, in.C());
-    npush(out);                 /// * stage for next stage
-    return *this;
-}
-__GPU__ Model&
-Model::iavgpool(U16 n) {
-    return *this;
-}
-__GPU__ Model&
-Model::iminpool(U16 n) {
-    return *this;
-}
-__GPU__ Model&
-Model::idropout(U16 f) {
-    if (NO_INIT) return *this;
-    Tensor &in  = *nten;
-    Tensor &out = _mmu->copy(in);
-    in.grad_fn  = L_DROPOUT;
-    in.parm     = f;
-    npush(out);
-    return *this;
-}
-///
-/// private methods
+/// ========================================================================
+/// private methods 
 ///
 __GPU__ void
-Model::step(Tensor &in, Tensor &out) {
-    DU     *da  = in.data;
-    DU     *dc  = out.data;
-    int    m    = out.H();
-    int    n    = out.W();
-    int    k    = in.parm;
-    dim3   blk(WARP_SZ, WARP_SZ);
-    dim3   grd((n + WARP_SZ - 1)/WARP_SZ, (m + WARP_SZ - 1)/WARP_SZ);
+Model::_step(Tensor &in, Tensor &out) {
+    DU   *da = in.data;
+    DU   *dc = out.data;
+    int  m   = out.H();
+    int  n   = out.W();
+    int  k   = in.parm;
+    dim3 blk(WARP_SZ, WARP_SZ);
+    dim3 grd((n + WARP_SZ - 1)/WARP_SZ, (m + WARP_SZ - 1)/WARP_SZ);
 
-    auto conv3 = [da, dc, m, n, blk](DU *f, DU *b) {
+    auto conv3x3 = [da, dc, m, n, blk](DU *f, DU *b) {
         dim3 g((n+CONV_SZ_3-1)/CONV_SZ_3, (m+CONV_SZ_3-1)/CONV_SZ_3);
         k_conv2d<WARP_SZ, CONV_SZ_3, 3><<<g, blk>>>(da, f, b, dc, m, n);
     };
-    auto conv5 = [da, dc, m, n, blk](DU *f, DU *b) {
+    auto conv5x5 = [da, dc, m, n, blk](DU *f, DU *b) {
         dim3 g((n+CONV_SZ_5-1)/CONV_SZ_5, (m+CONV_SZ_5-1)/CONV_SZ_5);
         k_conv2d<WARP_SZ, CONV_SZ_5, 5><<<g, blk>>>(da, f, b, dc, m, n);
     };
@@ -247,11 +177,11 @@ Model::step(Tensor &in, Tensor &out) {
     case L_CONV2D: {
         Tensor &f = *in.grad[0];     ///< filter tensor
         Tensor &b = *in.grad[1];     ///< bias tensor
-        int k = f.H();
-        switch(k) {
-        case 3: conv3(f.data, b.data); break;
-        case 5: conv5(f.data, b.data); break;
-        default: ERROR("model#conv2d kernel size=%d not supported\n", k);
+        int ks = f.H();
+        switch(ks) {
+        case 3: conv3x3(f.data, b.data); break;
+        case 5: conv5x5(f.data, b.data); break;
+        default: ERROR("model#conv2d kernel_size=%d not supported\n", ks);
         }
     } break;
     case L_LINEAR:  {                ///< dc = W * da + B
@@ -275,6 +205,88 @@ Model::step(Tensor &in, Tensor &out) {
     case L_MINPOOL: k_pooling<<<grd, blk>>>(da, dc, m, n, k, POOL_MIN); break;
     case L_DROPOUT: break;
     }
+}
+///
+/// Convolution and Linear ops
+///
+__GPU__ void
+Model::_iconv2d(Tensor &in, U16 c, DU bias, U16 *opt) {
+    U16 m = opt[0], n = opt[1];                  ///> filter sizing
+    U16 p = opt[2] ? opt[2] : int((m-1)/2);      ///> padding
+    U16 s = opt[3], d = opt[4];                  ///> stride, dilation
+    U16 h = in.H() - 2 * (p - int(m/2));         ///> output height
+    U16 w = in.W() - 2 * (p - int(n/2));         ///> output width
+
+    Tensor *f  = in.grad[0] = &tensor(1, m, n, c);                   ///> f
+    Tensor *df = in.grad[2] = &tensor(1, m, n, c).map(O_FILL, DU0);  ///> df
+    Tensor *b  = in.grad[1] = &tensor(1, h, w, 1).map(O_FILL, bias); ///> b
+    Tensor *db = in.grad[3] = &tensor(1, h, w, 1).map(O_FILL, DU0);  ///> db
+    _mmu->random(*f, NORMAL);                    /// * randomize f
+    
+    Tensor &out= tensor(1, h, w, c).map(O_FILL, DU0);  ///> output tensor
+    npush(out);                                  /// * stage for next stage
+}
+__GPU__ void
+Model::_ilinear(Tensor &in, U16 n, DU bias) {
+    U16 m = in.H();
+    Tensor *w  = in.grad[0] = &tensor(1, n, m, 1);                   ///> w
+    Tensor *dw = in.grad[2] = &tensor(1, n, m, 1).map(O_FILL, DU0);  ///> dw
+    Tensor *b  = in.grad[1] = &vector(n).map(O_FILL, bias);          ///> b
+    Tensor *db = in.grad[3] = &vector(n).map(O_FILL, DU0);           ///> db
+    _mmu->random(*w, NORMAL);                    /// * randomize w
+    
+    Tensor &out = vector(n);                     ///> output tensor sizing
+    npush(out);                                  /// * stage for next stage
+}
+__GPU__ void
+Model::_iflatten(Tensor &in) {
+    Tensor &out = vector(in.numel);
+    in.parm     = in.numel;
+    npush(out);
+}
+///
+/// Activation ops
+///
+__GPU__ void
+Model::_irelu(Tensor &in) {
+    Tensor &out = _mmu->copy(in); ///> output tensor sizing
+    npush(out);                   /// * stage for next stage
+}
+__GPU__ void
+Model::_itanh(Tensor &in) {}
+
+__GPU__ void
+Model::_isigmoid(Tensor &in) {}
+
+__GPU__ void
+Model::_isoftmax(Tensor &in) {
+    Tensor &out = _mmu->copy(in); ///> output tensor sizing
+    npush(out);                   /// * stage for next stage
+}
+///
+/// Pooling and Dropout ops
+///
+__GPU__ void
+Model::_imaxpool(Tensor &in, U16 f) {
+    in.parm = f;                  /// * keep pooling width
+    U16 m = int((in.H() - f) / f) + 1;
+    U16 n = int((in.W() - f) / f) + 1;
+    U16 s[4] = { 1, f, f, 1 }; memcpy(in.stride, s, sizeof(s));  // stride
+    
+    Tensor &out = tensor(1, m, n, in.C());
+    npush(out);                 /// * stage for next stage
+}
+__GPU__ void
+Model::_iavgpool(Tensor &in, U16 n) {}
+
+__GPU__ void
+Model::_iminpool(Tensor &in, U16 n) {}
+
+__GPU__ void
+Model::_idropout(Tensor &in, U16 f) {
+    Tensor &out = _mmu->copy(in);
+    in.parm = f;
+    npush(out);
 }
 #endif  // T4_ENABLE_OBJ
 //==========================================================================
