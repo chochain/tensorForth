0420870be71922202d87ea4e786f82c914d8b8c6
gnii <chochain@yahoo.com>
2022-08-16 19:42:04 -0700

    ~/src/tenvm.h add xop1,xop2 dispatchers, move ops to private

--- src/tenvm.h
+++ src/tenvm.h
@@ -33,25 +33,43 @@ public:
 protected:
     int    ten_lvl = 0;                     ///< tensor input level
     int    ten_off = 0;                     ///< tensor offset (storage index)
+    
     ///
     /// override literal handler
     ///
     __GPU__ void tprint(DU d);              ///< tensor dot (print)
     __GPU__ int  number(char *str) final;   ///< TODO: CC - this worked, why?
     ///
-    /// tensor ops
+    /// short-hands for object identification
     ///
-    __GPU__ void ssop(t4_mat_op op);        ///< scalar-scalar (Forth) ops
-    __GPU__ void tsop(t4_mat_op op, t4_drop_opt x, bool swap); ///< tensor-scalar broadcast op
-    __GPU__ void tmat(t4_mat_op op, t4_drop_opt x); ///< matrix-matrix element ops (Hadamard)
-    __GPU__ void tmul(t4_drop_opt x);       ///< matrix-matrix multiplication @
-    __GPU__ void tdiv(t4_drop_opt x);       ///< matrix-matrix division (no broadcast)
-    __GPU__ void tinv();                    ///< matrix inversion (Gauss-Jordan)
-    __GPU__ void tlu();                     ///< matrix LU decomposition
-    __GPU__ void tdet();                    ///< matrix determinant (via LU)
-    __GPU__ void ttrans();                  ///< matrix transpose
-    __GPU__ void solve();                   ///< solve linear equation Ax = b
-    __GPU__ void gemm();                    ///< GEMM C' = alpha * A x B + beta * C
+    __GPU__ __INLINE__ bool is_ten() {      ///< is TOS a tensor?
+        return IS_OBJ(top) && mmu.du2ten(top).is_tensor();
+    }
+    __GPU__ __INLINE__ bool is_2ten() {     /// are both TOS and NOS tensors?
+        return is_ten() &&
+            IS_OBJ(ss[-1]) && mmu.du2ten(ss[-1]).is_tensor();
+    }
+    __GPU__ __INLINE__ bool is_3ten() {     /// all three on stack tensors
+        return is_2ten() &&
+            IS_OBJ(ss[-2]) && mmu.du2ten(ss[-2]).is_tensor();
+    }
+private:
+    ///
+    /// tensor ops based on data types
+    ///
+    __GPU__ void xop1(t4_ten_op op, DU v=DU0);                   /// 1-operand ops
+    __GPU__ void xop2(t4_ten_op op, t4_drop_opt x=KEEP);         /// 2-operand ops
+    __GPU__ void _ss_op(t4_ten_op op);                           ///< scalar-scalar (Forth) ops
+    __GPU__ void _ts_op(t4_ten_op op, t4_drop_opt x, bool swap); ///< tensor-scalar broadcast op
+    __GPU__ void _tt_op(t4_ten_op op, t4_drop_opt x);            ///< tensor-tensor ops
+    ///
+    /// tensor-tensor ops
+    ///
+    __GPU__ Tensor &_tinv(Tensor &A);            ///< matrix inversion
+    __GPU__ Tensor *_tdot(Tensor &A, Tensor &B); ///< matrix-matrix multiplication @
+    __GPU__ Tensor *_tdiv(Tensor &A, Tensor &B); ///< matrix-matrix division (no broadcast)
+    __GPU__ Tensor *_solv(Tensor &A, Tensor &B); ///< solve linear equation Ax = b
+    __GPU__ void   _gemm();                      ///< GEMM C' = alpha * A x B + beta * C
 #endif // T4_ENABLE_OBJ
 };
 #endif // TEN4_SRC_TENVM_H
