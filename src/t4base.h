90697b1fe4dc521680117b9137e099924dc4087b
gnii <chochain@yahoo.com>
2022-08-23 08:31:02 -0700

    ~/src/t4base.h prefix t4_obj with T4_*; squeeze dsize in MSB, so parm=U16

--- src/t4base.h
+++ src/t4base.h
@@ -11,9 +11,9 @@
 /// tensorForth object types
 ///
 typedef enum {
-    VIEW = 0,               ///< a view object
-    TENSOR,                 ///< tensor object
-    MODEL                   ///< NN model
+    T4_VIEW = 0,            ///< a view object
+    T4_TENSOR,              ///< tensor object
+    T4_MODEL                ///< NN model
 } t4_obj;
 ///
 /// tensorForth base object class
@@ -23,11 +23,11 @@ struct T4Base : public Managed {
     union {
         U32  attr = 0;      ///< attrbutes collective
         struct {
-            U8     dsize:5; ///< size of data element, F32 for now
-            U8     rank :3; ///< rank of tensor 2:matrix, 4:NHWC tensor
-            U8     parm;    ///< parameter storage
-            U8     refc;    ///< reference counter (reserved)
-            t4_obj ttype;   ///< 0: tensor, 1: view
+            U8    ttype: 2; ///< t4_obj, 0:view, 1=tensor, 2=model
+            U8    dsize: 3; ///< size of data element, F32=5, F64=6
+            U8    rank : 3; ///< rank of tensor 2:matrix, 4:NHWC tensor
+            U8    nref;     ///< reference counter (reserved)
+            U16   parm;     ///< extra parameter storage
         };
     };
     DU  *data;    ///< managed memory block pointer
@@ -35,19 +35,19 @@ struct T4Base : public Managed {
     /// class contructors
     ///
     __HOST__ T4Base() :
-        dsize(sizeof(DU)), numel(0), rank(0) {}
+        dsize(DSIZE), numel(0), rank(0) {}
     __HOST__ T4Base(U32 sz) :
-        dsize(sizeof(DU)), numel(sz), rank(1) {
+        dsize(DSIZE), numel(sz), rank(1) {
         cudaMallocManaged((void**)&data, (size_t)numel * dsize);
         GPU_CHK();
     }
     __HOST__ T4Base(U16 h, U16 w) :
-        dsize(sizeof(DU)), numel(h * w), rank(2) {
+        dsize(DSIZE), numel(h * w), rank(2) {
         cudaMallocManaged((void**)&data, (size_t)numel * dsize);
         GPU_CHK();
     }
     __HOST__ T4Base(U16 n, U16 h, U16 w, U16 c) :
-        dsize(sizeof(DU)), numel(n * h * w * c), rank(4) {
+        dsize(DSIZE), numel(n * h * w * c), rank(4) {
         cudaMallocManaged((void**)&data, (size_t)numel * dsize);
         GPU_CHK();
     }
@@ -55,9 +55,9 @@ struct T4Base : public Managed {
         if (!data) return;
         cudaFree((void*)data);
     }
-    __BOTH__ __INLINE__ bool is_view()   { return ttype == VIEW;   }
-    __BOTH__ __INLINE__ bool is_tensor() { return ttype <= TENSOR; }
-    __BOTH__ __INLINE__ bool is_model()  { return ttype == MODEL;  }
+    __BOTH__ __INLINE__ bool is_view()   { return ttype == T4_VIEW;   }
+    __BOTH__ __INLINE__ bool is_tensor() { return ttype <= T4_TENSOR; }
+    __BOTH__ __INLINE__ bool is_model()  { return ttype == T4_MODEL;  }
 };
 #endif // TEN4_SRC_T4BASE_H_
 
