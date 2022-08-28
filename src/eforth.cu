f8ed2d5fdb552c62c561b605286fcdda37390d37
gnii <chochain@yahoo.com>
2022-08-28 10:34:32 -0700

    ~/src/eforth.cu fix clock scalar bug

--- src/eforth.cu
+++ src/eforth.cu
@@ -296,7 +296,7 @@ ForthVM::init() {
     ///@defgroup System ops
     ///@{
     CODE("mstat", mmu.status()),
-    CODE("clock", DU t = (DU)(clock64() / khz); PUSH(t)),
+    CODE("clock", DU t = I2D(clock64()) / khz; SCALAR(t); PUSH(t)),
     CODE("delay", delay(POPi)),                  ///< TODO: change to VM_WAIT
     CODE("bye",   status = VM_STOP),
     ///@}
