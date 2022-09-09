d7fc2003939efd805fd3d284489584ab1e1693a9
gnii <chochain@yahoo.com>
2022-09-09 15:51:55 -0700

    ~/src/gui.h remove version checking

--- src/gui.h
+++ src/gui.h
@@ -6,15 +6,6 @@
  */
 #ifndef T4_GUI_H
 #define T4_GUI_H
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <iterator>
-#include <vector>
-#include <assert.h>
-
 #include <GL/gl.h>
 #include <GL/glx.h>
 #include <GL/freeglut.h>
@@ -90,84 +81,6 @@ namespace T4GUI {
     GLFN(glClientActiveTexture,     PFNGLACTIVETEXTUREPROC);
 #endif
 #undef GLFN
-
-    namespace Int {
-        static std::vector<std::string> split(const std::string &str) {
-            std::istringstream ss(str);
-            std::istream_iterator<std::string> it(ss);
-            return std::vector<std::string>(it, std::istream_iterator<std::string>());
-        }
-        /* Sort the vector passed by reference */
-        template<typename T>
-        static inline void sort(std::vector<T> &a) {
-            std::sort(a.begin(), a.end());
-        }
-        /* Compare two vectors */
-        template<typename T>
-        static int equals(std::vector<T> a, std::vector<T> b) {
-            if (a.size() != b.size()) return 0;
-            sort(a);
-            sort(b);
-            return std::equal(a.begin(), a.end(), b.begin());
-        }
-        template<typename T>
-        static std::vector<T> hit(std::vector<T> a, std::vector<T> b) {
-            sort(a);
-            sort(b);
-            std::vector<T> rc;
-            std::set_intersection(
-                a.begin(), a.end(), b.begin(), b.end(),
-                std::back_inserter<std::vector<std::string> >(rc));
-            return rc;
-        }
-        static std::vector<std::string> gl_ext() {
-            std::string ext((const char*)glGetString(GL_EXTENSIONS));
-            return split(ext);
-        }
-    }
-    static int glExtOK(const std::string &ext) {
-        std::vector<std::string> all = Int::gl_ext();
-        std::vector<std::string> req = Int::split(ext);
-        std::vector<std::string> hit = Int::hit(all, req);
-        return Int::equals(hit, req);
-    }
-    static int glVersionOK(int major, int minor) {
-        std::string       ver((const char*)glGetString(GL_VERSION));
-        std::stringstream ss(ver);
-        int  m, n;
-        char dot;
-        ss >> m >> dot >> n;
-        assert(dot == '.');
-        return m > major || (m == major && n >= minor);
-    }
-    static inline const char* glErrString(GLenum err) {
-        switch(err) {
-        case GL_NO_ERROR:         return "GL_NO_ERROR";
-        case GL_INVALID_ENUM:     return "GL_INVALID_ENUM";
-        case GL_INVALID_VALUE:    return "GL_INVALID_VAL";
-        case GL_INVALID_OPERATION:return "GL_INVALID_OP";
-        case GL_OUT_OF_MEMORY:    return "GL_OUT_OF_MEMORY";
-        case GL_STACK_UNDERFLOW:  return "GL_STACK_UNDERFLOW";
-        case GL_STACK_OVERFLOW:   return "GL_STACK_OVERFLOW";
-#ifdef GL_INVALID_FRAMEBUFFER_OPERATION
-        case GL_INVALID_FRAMEBUFFER_OPERATION: return "GL_INVALID_FBO";
-#endif
-        default: return "UNKNOWN GL opcode";
-        }
-    }
-    inline bool gl_check(const char *file, const int line) {
-        GLenum err = glGetError();
-        if (err != GL_NO_ERROR) {
-            fprintf(stderr, "GL Error in file '%s' in line %d :\n", file, line);
-            fprintf(stderr, "%s\n", glErrString(err));
-            return false;
-        }
-        return true;
-    }
-#define GL_CHK()                         \
-    if(!gl_check( __FILE__, __LINE__)) { \
-        exit(EXIT_FAILURE);              \
-    }
 } /// namespace T4GUI
 
 using namespace T4GUI;
