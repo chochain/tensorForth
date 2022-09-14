9bef80cf6729d02e15c0fcfdc9eac2546af6691f
gnii <chochain@yahoo.com>
2022-09-13 19:35:19 -0700

    ~/src/vu.cu add texture builder

--- src/vu.cu
+++ src/vu.cu
@@ -6,22 +6,42 @@
  */
 #include "vu.h"
 
-Vu::Vu(const char *name) : fname(name) {}
-Vu::~Vu() {
-    if (!h_src) return;
+int Vu::_build_texture() {
+    int c = dset.C;
+    if (c==4) {
+        h_tex = (uchar4*)dset.h_data;        ///< pass through
+        _bind_to_cuda();
+        return 0;                            ///< no extra mem allocated
+    }
+    ///
+    /// need rebuild GL texture in uchar4 format
+    /// 
+    int bsz = X * Y * sizeof(uchar4);        ///< texture block size
+
+    h_tex = (uchar4*)malloc(bsz);            ///< alloc block
+    if (!h_tex) return -1;
     
-    free(h_src);             /// * free host memory = stbi_image_free(h_src)
-    cudaFreeArray(d_ary);    /// * free device memory
-    GPU_CHK();
+    U8     *s = dset.h_data;
+    uchar4 *t = h_tex;
+    for (int i = 0; i < Y; i++) {
+        for (int j = 0; j < X; j++, t++, s+=c) {
+            t->x = *s;
+            t->y = c < 2 ? *s   : *(s+1);
+            t->z = c < 3 ? *s   : *(s+2);
+            t->w = c < 4 ? 0xff : *(s+3);
+        }
+    }
+    _bind_to_cuda();
+    return 1;                                ///< has malloc
 }
 
-Vu &Vu::setup() {
-    int pitch = sizeof(uchar4) * W;
+void Vu::_bind_to_cuda() {
+    int pitch = sizeof(uchar4) * X;
     
     cudaChannelFormatDesc fmt = cudaCreateChannelDesc<uchar4>();
-    cudaMallocArray(&d_ary, &fmt, W, H); GPU_CHK();
+    cudaMallocArray(&d_ary, &fmt, X, Y); GPU_CHK();
     cudaMemcpy2DToArray(
-        d_ary, 0, 0, h_src, pitch, pitch, H, cudaMemcpyHostToDevice);
+        d_ary, 0, 0, h_tex, pitch, pitch, Y, cudaMemcpyHostToDevice);
     GPU_CHK();
 
     cudaResourceDesc res;
@@ -39,7 +59,7 @@ Vu &Vu::setup() {
   
     cudaCreateTextureObject(&img, &res, &desc, NULL);
     GPU_CHK();
-
-    return *this;
 }
+
+
 
