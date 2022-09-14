f8058a365bc6b185c480ae3e0274b4dee0950b67
gnii <chochain@yahoo.com>
2022-09-13 19:34:02 -0700

    ~/src add dataset.h

---
+++ src/dataset.h
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @brief tensorForth - Dataset class
+ *
+ * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
+ */
+#ifndef T4_DATASET_H
+#define T4_DATASET_H
+
+typedef unsigned char U8;
+
+class Dataset {
+public:
+    const char *d_fn;              ///< data file name
+    const char *t_fn;              ///< target lable file name
+    int        N, H, W, C;         ///< dimensions and channel size
+    U8         *h_data  = NULL;    ///< source data on host
+    U8         *h_lable = NULL;    ///< label data on host
+    U8         *d_data  = NULL;    ///< source data on device
+
+    Dataset(const char *data_fn, const char *label_fn) : d_fn(data_fn), t_fn(label_fn) {}
+    ~Dataset() {
+        if (!h_data) return;
+
+        free(h_data);
+        cudaFree(d_data);
+        GPU_CHK();
+    }
+    int dsize() { return C; }
+    int len()   { return N; }
+    
+    virtual Dataset &load() { printf("load() implemented?\n"); return *this; }
+    virtual U8      *operator [](int idx) { return &h_data[idx * C]; }
+};
+#endif // T4_DATASET_H
+
