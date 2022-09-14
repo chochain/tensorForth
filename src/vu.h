c010cc08369eb22d7a48310dfe10f17adfa773d6
gnii <chochain@yahoo.com>
2022-09-14 11:43:50 -0700

    ~/src/vu.h create CuTexObj,CuGfxPbo; make build_texture virtual; member all host only

--- src/vu.h
+++ src/vu.h
@@ -10,40 +10,47 @@
 #include "ten4_types.h"
 #include "dataset.h"
 
-typedef U32 TColor;
+typedef U32                         TColor;
+typedef struct cudaGraphicsResource CuGfxPbo;
+typedef cudaTextureObject_t         CuTexObj;
 
 class Vu {
 public:
-    Dataset             &dset;
-    int                 X, Y;           ///< view port dimensions
-    uchar4              *h_tex = NULL;  ///< texture on host
-    cudaArray           *d_ary = NULL;  ///< texture on device
-    cudaTextureObject_t img;            ///< cuda Textrure object
-    struct cudaGraphicsResource *pbo;   ///< OpenGL-CUDA exchange
+    Dataset   &dset;
+    int       X, Y;           ///< view port dimensions
+    uchar4    *h_tex = NULL;  ///< texture on host
     
-    Vu(Dataset &ds, int x, int y) : dset(ds), X(x), Y(y) {
+    cudaArray *d_ary = NULL;  ///< texture buffer on device
+    CuTexObj   cu_tex;        ///< cuda Textrure object
+    CuGfxPbo   *pbo;          ///< OpenGL-CUDA exchange
+    
+    __HOST__ Vu(Dataset &ds, int x=0, int y=0) :
+        dset(ds), X(x ? x : ds.W), Y(y ? y : ds.H) {
         if (sizeof(uchar4) != 4) {
             printf("***Bad uchar4 size***\n");
             exit(EXIT_SUCCESS);
         }
-        _build_texture();
+        if (X == ds.W && Y == ds.H && ds.C == 4) {
+            h_tex = (uchar4*)dset.h_data;   /// * pass thru, no buffer needed
+        }
+        else build_texture();               /// * alloc memory for texture
         _bind_to_cuda();
     }
-    ~Vu() {
+    __HOST__ ~Vu() {
         if (!h_tex) return;
         free(h_tex);
         
         if (!d_ary) return;
-        cudaFreeArray(d_ary);           /// * free device memory
+        cudaFreeArray(d_ary);               /// * free device memory
         GPU_CHK();
-    }        
-    virtual void mouse(int button, int state, int x, int y) {}
-    virtual void keyboard(U8 k)         {}
-    virtual void display(TColor *d_dst) {}
+    }
+    __HOST__ virtual int  build_texture();  ///< build texture buffer
+    __HOST__ virtual void mouse(int button, int state, int x, int y) {}
+    __HOST__ virtual void keyboard(U8 k)         {}
+    __HOST__ virtual void display(TColor *d_dst) {}
     
 private:
-    int  _build_texture();              ///< build texture memory
-    void _bind_to_cuda();
+    __HOST__ void _bind_to_cuda();
 };
 
 extern "C" int  gui_init(int *argc, char **argv);
