5f958da621f4adcb04522c7aa103a2414fdf485a
gnii <chochain@yahoo.com>
2022-09-15 11:02:08 -0700

    ~/src/vu.h virtual init_host_tex for extended class overload

--- src/vu.h
+++ src/vu.h
@@ -17,40 +17,23 @@ typedef cudaTextureObject_t         CuTexObj;
 class Vu {
 public:
     Dataset   &dset;
-    int       X, Y;           ///< view port dimensions
-    uchar4    *h_tex = NULL;  ///< texture on host
+    int       X, Y;             ///< view port dimensions
+    uchar4    *h_tex   = NULL;  ///< texture on host
     
-    cudaArray *d_ary = NULL;  ///< texture buffer on device
-    CuGfxPbo  *pbo   = NULL;  ///< OpenGL-CUDA exchange
-    CuTexObj  cu_tex = 0;     ///< cuda Textrure object
+    cudaArray *d_ary   = NULL;  ///< CUDA texture buffer on device
+    CuGfxPbo  *pbo     = NULL;  ///< OpenGL-CUDA exchange
+    CuTexObj  cu_tex   = 0;     ///< cuda Textrure object
     
-    __HOST__ Vu(Dataset &ds, int x=0, int y=0) :
-        dset(ds), X(x ? x : ds.W), Y(y ? y : ds.H) {
-        if (sizeof(uchar4) != 4) {
-            printf("***Bad uchar4 size***\n");
-            exit(EXIT_SUCCESS);
-        }
-        if (X == ds.W && Y == ds.H && ds.C == 4) {
-            h_tex = (uchar4*)dset.h_data;   /// * pass thru, no buffer needed
-        }
-        else transform();                   /// * rebuild texture
-        _bind_to_cuda();
-    }
-    __HOST__ ~Vu() {
-        if (!h_tex) return;
-        free(h_tex);
-
-        cudaDestroyTextureObject(cu_tex);   /// * release texture object
-        cudaFreeArray(d_ary);               /// * free device memory
-        GPU_CHK();
-    }
-    __HOST__ virtual int  transform();      ///< rebuild texture buffer
+    __HOST__ Vu(Dataset &ds, int x=0, int y=0);
+    __HOST__ ~Vu();
+    __HOST__ virtual int  init_host_tex();   ///< rebuild texture buffer
+    
     __HOST__ virtual void mouse(int button, int state, int x, int y) {}
     __HOST__ virtual void keyboard(U8 k)         {}
     __HOST__ virtual void display(TColor *d_dst) {}
     
-private:
-    __HOST__ void _bind_to_cuda();
+    __HOST__ void tex_dump();
+    __HOST__ void init_cuda_tex();
 };
 
 extern "C" int  gui_init(int *argc, char **argv);
