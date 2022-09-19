7f97480acf831d65da84a786028b293d156791c1
gnii <chochain@yahoo.com>
2022-09-18 18:55:33 -0700

    ~/src/ten4.h add vmst, vmst_cnt for VM state cache

--- src/ten4.h
+++ src/ten4.h
@@ -7,17 +7,21 @@
 #include "vm.h"
 
 #define WARP(t)  ((((t) + 32)>>5) << 5)   /** calculate warp thread count */
+#define VMST_SZ  (sizeof(vm_state) * VM_MIN_COUNT)
+#define VMSS_SZ  (sizeof(DU) * T4_SS_SZ * VM_MIN_COUNT)
+
 class TensorForth {
     AIO  *aio;                            ///< async IO manager
     MMU  *mmu;                            ///< memory management unit
-    int  *busy;                           ///< for Device to Host VM status reporting
+    vm_state *vmst;                       ///< VM state on Managed memory
+    int      *vmst_cnt;                   ///< state tally
 
 public:
     TensorForth(int device=0, int verbose=0);
     ~TensorForth();
 
-    __HOST__ int   is_ready();           ///< has worker ready to take input
-    __HOST__ int   run();                ///< execute tensorForth main loop
+    __HOST__ int   vm_tally();            ///< tally fetch state of VMs
+    __HOST__ int   run();                 ///< execute tensorForth main loop
     __HOST__ void  teardown(int sig=0);
 };
 #endif // TEN4_SRC_TEN4_H_
