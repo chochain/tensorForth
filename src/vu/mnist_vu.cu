b044c3d2c3e2ccb9d09bb2d1dadfe66f132c7a6b
gnii <chochain@yahoo.com>
2022-09-23 13:10:34 -0700

    ~/src/vu/mnist.vu refactor into mnist.h

--- src/vu/mnist_vu.cu
+++ src/vu/mnist_vu.cu
@@ -4,44 +4,34 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
-#ifndef TEN4_SRC_VU_MNIST_VU_H
-#define TEN4_SRC_VU_MNIST_VU_H
-#include "vu.h"
+#include "mnist_vu.h"
 ///
 /// MNIST GUI texture builder
 ///
-class MnistVu : public Vu {
-    int NX, NY;
-public:
-    MnistVu(Dataset &ds, int nx=40, int ny=30) :
-        Vu(ds, ds.W * nx, ds.H * ny), NX(nx), NY(ny) {}
-    
-    virtual int init_host_texture() {
-        auto fit = [this](int z0, unsigned char *src) {
-            uchar4 *t = &h_tex[z0];
-            for (int i = dset.H - 1; i >= 0; i--) {      // y top-down flip
-                for (int j = 0; j < dset.W; j++, src++) {
-                    int z = j + i * dset.W * NX;
-                    t[z].x = t[z].y = t[z].z = *src;
-                    t[z].w = 0xff;
-                }
-            }
-        };
-        for (int y = 0, y1 = NY-1; y < NY; y++, y1--) { // y1 top-down flip
-            printf("\n");
-            for (int x = 0; x < NX; x++) {
-                int z   = x + y * NX;
-                U8 *src = dset[z];
-                int z0  = (x + y1 * dset.H * NX) * dset.W;
-                fit(z0, src);
-                printf("%1d", dset.h_label[z]);
+int MnistVu::init_host_texture() {
+    auto fit = [this](int z0, unsigned char *src) {
+        uchar4 *t = &h_tex[z0];
+        for (int i = dset.H - 1; i >= 0; i--) {      // y top-down flip
+            for (int j = 0; j < dset.W; j++, src++) {
+                int z = j + i * dset.W * NX;
+                t[z].x = t[z].y = t[z].z = *src;
+                t[z].w = 0xff;
             }
         }
+    };
+    for (int y = 0, y1 = NY-1; y < NY; y++, y1--) { // y1 top-down flip
         printf("\n");
-        return 0;
+        for (int x = 0; x < NX; x++) {
+            int z   = x + y * NX;
+            U8 *src = dset[z];
+            int z0  = (x + y1 * dset.H * NX) * dset.W;
+            fit(z0, src);
+            printf("%1d", dset.h_label[z]);
+        }
     }
-};
-#endif  // TEN4_SRC_VU_MNIST_VU_H
+    printf("\n");
+    return 0;
+}
 
 
 
