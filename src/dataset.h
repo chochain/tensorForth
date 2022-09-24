838ddc5e32a42e11662dcdd1c105239f34a7a1ba
gnii <chochain@yahoo.com>
2022-09-23 19:24:13 -0700

    ~/src/dataset.h data, label use host/device common pointers

--- src/dataset.h
+++ src/dataset.h
@@ -7,31 +7,37 @@
 #ifndef T4_DATASET_H
 #define T4_DATASET_H
 
+#define DS_ALLOC(p, sz) \
+    if (cudaMallocManaged(p, sz) != cudaSuccess) { \
+        fprintf(stderr, "ERROR: Dataset malloc %d\n", (int)(sz)); \
+        exit(-1); \
+    }
 typedef uint8_t U8;
 
 class Dataset {
 public:
-    const char *d_fn;              ///< data file name
-    const char *t_fn;              ///< target lable file name
+    const char *d_fn;         ///< data file name
+    const char *t_fn;         ///< target lable file name
     
-    int        N, H, W, C;         ///< dimensions and channel size
+    int   N, H, W, C;         ///< dimensions and channel size
+    int   batch_sz;
     
-    U8         *h_data  = NULL;    ///< source data on host
-    U8         *h_label = NULL;    ///< label data on host
-    U8         *d_data  = NULL;    ///< source data on device
+    U8    *data  = NULL;      ///< source data on host
+    U8    *label = NULL;      ///< label data on host
 
-    Dataset(const char *data_fn, const char *label_fn) : d_fn(data_fn), t_fn(label_fn) {}
+    Dataset(const char *data_fn, const char *label_fn, int bsz=0)
+        : d_fn(data_fn), t_fn(label_fn), batch_sz(bsz) {}
     ~Dataset() {
-        if (!h_data) return;
+        if (!data) return;
 
-        free(h_data);
-        cudaFree(d_data);
+        cudaFree(data);
+        cudaFree(label);
     }
     int dsize() { return H * W * C; }
     int len()   { return N; }
     
     virtual Dataset *load() { printf("load() implemented?\n"); return this; }
-    virtual U8      *operator [](int idx) { return &h_data[idx * dsize()]; }
+    virtual U8 *operator [](int idx) { return &data[idx * dsize()]; }
 };
 #endif // T4_DATASET_H
 
