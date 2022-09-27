771b92863bc2dcc8f078292f3365e781d30c5a85
gnii <chochain@yahoo.com>
2022-09-26 19:16:15 -0700

    ~/src/dataset.h add batch control

--- src/dataset.h
+++ src/dataset.h
@@ -12,24 +12,28 @@
         fprintf(stderr, "ERROR: Dataset malloc %d\n", (int)(sz)); \
         exit(-1); \
     }
+#define IO_ERROR(fn) \
+    fprintf(stderr, "Dataset: fail to open file %s\n", fn);
+
 typedef uint8_t U8;
 
-class Dataset {
-public:
-    const char *d_fn;         ///< data file name
-    const char *t_fn;         ///< target lable file name
+struct Dataset {
+    const char *ds_name;      ///< data source name
+    const char *tg_name;      ///< target label name
     
-    int   N, H, W, C;         ///< dimensions and channel size
-    int   batch_sz;
+    int   N, H, W, C;         ///< set dimensions and channel size
+    int   batch_sz = 0;       ///< batch size (unit of data)
+    int   batch_id = 0;       ///< current batch id
     
     U8    *data  = NULL;      ///< source data on host
     U8    *label = NULL;      ///< label data on host
 
-    Dataset(const char *data_fn, const char *label_fn, int bsz=0)
-        : d_fn(data_fn), t_fn(label_fn), batch_sz(bsz) {}
+    Dataset(const char *data_name, const char *label_name, int batch=0)
+        : ds_name(data_name), tg_name(label_name), batch_sz(batch) {}
+    
     ~Dataset() {
         if (!data) return;
-
+        
         cudaPointerAttributes attr;
         cudaPointerGetAttributes(&attr, data);
         if (attr.devicePointer != NULL) {
@@ -43,11 +47,23 @@ public:
             if (label) free(label);
         }
     }
+    Dataset *set_batch(int bsz, int idx=0) {
+        batch_sz = bsz;
+        batch_id = idx;
+        return this;
+    }
     int dsize() { return H * W * C; }
     int len()   { return N; }
     
-    virtual Dataset *load() { printf("load() implemented?\n"); return this; }
-    virtual U8 *operator [](int idx) { return &data[idx * dsize()]; }
+    virtual Dataset *load() {
+        printf("load() implemented?\n");
+        return this;
+    }
+    virtual Dataset *get_batch(U8 *dst) {
+        printf("batch(U8*) implemented?\n");
+        return this;
+    }
+    virtual U8 *operator [](int idx){ return &data[idx * dsize()]; }
 };
 #endif // T4_DATASET_H
 
