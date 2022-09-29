8adebdc4fd415669027cad39327dd0bec334a202
gnii <chochain@yahoo.com>
2022-09-29 09:12:05 -0700

    ~/src/vu,nn update Makefiles; update .gitignore

--- src/vu/vu.h
+++ src/vu/vu.h
@@ -8,7 +8,7 @@
 #define T4_VU_H
 #include <GL/gl.h>
 #include "../ten4_types.h"
-#include "../ndata.h"
+#include "../ldr/ndata.h"
 
 typedef U32                         TColor;
 typedef struct cudaGraphicsResource CuGfxPbo;
