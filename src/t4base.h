cf441415d9937037a7061f6135c4964510fd0010
gnii <chochain@yahoo.com>
2022-09-28 19:52:50 -0700

    ~/src/t4base.h add dataset; fix dsize multiplier

--- src/t4base.h
+++ src/t4base.h
@@ -24,7 +24,7 @@ struct T4Base : public Managed {
     union {
         U32  attr = 0;      ///< attrbutes collective
         struct {
-            U8    ttype: 2; ///< t4_obj, 0:view, 1=tensor, 2=model
+            U8    ttype: 2; ///< t4_obj, 0:view, 1=tensor, 2=model, 3=dataset
             U8    dsize: 3; ///< size of data element, F32=5, F64=6
             U8    rank : 3; ///< rank of tensor 2:matrix, 4:NHWC tensor
             U8    nref;     ///< reference counter (reserved)
@@ -39,15 +39,15 @@ struct T4Base : public Managed {
         dsize(DSIZE), numel(0), rank(0) {}
     __HOST__ T4Base(U32 sz) :
         dsize(DSIZE), numel(sz), rank(1) {
-        MM_ALLOC((void**)&data, (size_t)numel * dsize);
+        MM_ALLOC((void**)&data, (size_t)numel * sizeof(DU));
     }
     __HOST__ T4Base(U16 h, U16 w) :
         dsize(DSIZE), numel(h * w), rank(2) {
-        MM_ALLOC((void**)&data, (size_t)numel * dsize);
+        MM_ALLOC((void**)&data, (size_t)numel * sizeof(DU));
     }
     __HOST__ T4Base(U16 n, U16 h, U16 w, U16 c) :
         dsize(DSIZE), numel(n * h * w * c), rank(4) {
-        MM_ALLOC((void**)&data, (size_t)numel * dsize);
+        MM_ALLOC((void**)&data, (size_t)numel * sizeof(DU));
     }
     __HOST__ ~T4Base() {
         if (!data) return;
