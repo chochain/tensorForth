0cf0abfaf8621b98e9a7e77d19dab0ffcdfd794c
gnii <chochain@yahoo.com>
2022-10-05 10:38:38 -0700

    ~/src/mmu/model.h matrix => tensor(n,h,1,1)

--- src/mmu/model.h
+++ src/mmu/model.h
@@ -56,7 +56,7 @@ public:
         return _mmu->tensor(sz);
     }
     __GPU__ __INLINE__ Tensor &tensor(U16 n, U16 h) {
-        return _mmu->tensor(n, h);
+        return _mmu->tensor(n, h, 1, 1);
     }
     __GPU__ __INLINE__ Tensor &tensor(U16 n, U16 h, U16 w, U16 c) {
         return _mmu->tensor(n, h, w, c);
