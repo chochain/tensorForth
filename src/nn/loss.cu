8bba6221bb839f66ebcb5c8bb61f5a8d839f5046
gnii <chochain@yahoo.com>
2022-10-05 18:59:45 -0700

    ~/src/nn/loss.cu mv onehot here; _dump,_view handle batch and small set

--- src/nn/loss.cu
+++ src/nn/loss.cu
@@ -5,8 +5,34 @@
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
 #include "model.h"
+#include "dataset.h"
 
 #if T4_ENABLE_OBJ
+__GPU__ Tensor&
+Model::onehot() {
+    auto show = [](DU *h, int n, int sz) {
+        printf("onehot[%d]=", n);
+        for (int i = 0; i < sz; i++) {
+            printf("%2.0f", h[i]);
+        }
+        printf("\n");
+    };
+    Tensor &out = (*this)[-1];                         ///< model output
+    int    N    = out.N(), hwc = out.HWC();            ///< sample size
+    Tensor &hot = _t4(N, hwc).fill(DU0);               ///< one-hot vector
+    if (!_dset) {
+        ERROR("Model#loss dataset not set yet?\n");
+        return hot;
+    }
+    for (int n = 0; n < N; n++) {                      /// * loop through batch
+        DU *h = hot.slice(n);                          ///< 
+        U32 i = INT(_dset->label[n]);
+        h[i < hwc ? i : 0] = DU1;
+        show(h, n, hwc);
+    }
+    return hot;
+}
+
 __GPU__ DU
 Model::loss(t4_loss op) {
     return loss(op, *_hot);                     /// * use default one-hot vector
@@ -29,16 +55,16 @@ Model::loss(t4_loss op, Tensor &hot) {          ///< loss against one-hot
 ///
 __GPU__ void
 Model::debug(Tensor &t, DU scale) {
-    const int H  = t.H(), W = t.W(), C = t.C();
-    const int sq = (int)sqrt(0.5f + H);
+    const int N  = t.N(), H = t.H(), W = t.W(), C = t.C();
+    const int sz = H * W;
     
-    for (int n = 0; n < t.N(); n++) {
+    for (int n = 0; n < N; n++) {
         DU *d = t.slice(n);
-        if (W > 1) _view(d, H, W, C);
-        else {
-            if (sq > 6) _view(d, sq, sq, C, scale);
-            _dump(d, W, H, C);
+        if (sz < 100) {
+            printf("\nn=%d\n", n);
+            _dump(d, H, W, C);
         }
+        if (sz > 36) _view(d, H, W, C, scale);
     }
 }
 
@@ -46,15 +72,23 @@ __GPU__ void
 Model::_view(DU *v, int H, int W, int C, DU scale) {
 //  static const char *map = " .'`^\",:;Il!i><~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";   // 69 shades
     static const char *map = " .:-=+*#%@X";
-    for (int i = 0; i < H; i++) {
+    const int sz = H * W, sq = (int)sqrt(sz);
+    const int sh = (sz/sq) + ((sz - sq*sq) > 0 ? 1 : 0);
+    const int h  = W > 1 ? H : (sz < 36 ? 1 : sh);
+    const int w  = W > 1 ? W : (sz < 36 ? H : sq);
+    for (int i = 0; i < h; i++) {
         printf("\n");
         for (int k = 0; k < C; k++) {
-            for (int j = 0; j < W; j++) {
-                DU x0 = (v[k + ((j>0 ? j-1 : j) + i * W) * C]) * scale;
-                DU x1 = (x0 + v[k + (j + i * W) * C] * scale) * 0.5;
-                char c0 = map[x0 < 10.0f ? (x0 < DU0 ? 10 : (int)x0) : 9];
-                char c1 = map[x1 < 10.0f ? (x1 < DU0 ? 10 : (int)x1) : 9];
-                printf("%c%c", c0, c1);                // double width
+            for (int j = 0; j < w; j++) {
+                int n = j + i * w;
+                if (n < sz) { 
+                    DU x0 = (v[k + (j>0 ? n - 1 : n) * C]) * scale;
+                    DU x1 = (x0 + v[k + n * C] * scale) * 0.5;
+                    char c0 = map[x0 < 10.0f ? (x0 < DU0 ? 10 : (int)x0) : 9];
+                    char c1 = map[x1 < 10.0f ? (x1 < DU0 ? 10 : (int)x1) : 9];
+                    printf("%c%c", c0, c1);                           // double width
+                }
+                else printf("  ");
             }
             printf("|");
         }
@@ -63,17 +97,21 @@ Model::_view(DU *v, int H, int W, int C, DU scale) {
 }
 __GPU__ void
 Model::_dump(DU *v, int H, int W, int C) {
-    for (int k = 0; k < C; k++) {
-        printf("\nC=%d ---", k);
-        for (int i = 0; i < H; i++) {
-            printf("\n");
-            for (int j = 0; j < W; j++) {
-                DU x = v[k + (j + i * W) * C];
-                printf("%5.2f", x);
+    const int sz = H * W, sq = (int)sqrt(sz);
+    const int sh = (sz/sq) + ((sz - sq*sq) > 0 ? 1 : 0);
+    const int h  = W > 1 ? H : (sz < 36 ? 1 : sh);
+    const int w  = W > 1 ? W : (sz < 36 ? H : sq);
+    for (int i = 0; i < h; i++) {
+        for (int k = 0; k < C; k++) {
+            for (int j = 0; j < w; j++) {
+                int n = j + i * w;
+                if (n < sz) printf("%5.2f", v[k + n * C]);
+                else        printf(" ....");
             }
+            printf("|");
         }
+        printf("\n");
     }
-    printf("\n");
 }
 __GPU__ void
 Model::_dump_dbdf(Tensor &db, Tensor &df) {
