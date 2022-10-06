a36f05afcd12eae6de0cd806041b29511490f330
gnii <chochain@yahoo.com>
2022-10-05 18:58:51 -0700

    ~/src/mmu/model.h rename vector,tensor to _t4,_vec; add _fsoftmax

--- src/mmu/model.h
+++ src/mmu/model.h
@@ -25,64 +25,67 @@ public:
     ///
     /// @name Derivertive ops
     /// @{
-    static __HOST__ const char* nname(int n);    /// network layer name
-    static __GPU__  const char* d_nname(int n);
+    static __HOST__ const char* nname(int n);    ///< network layer name on host
+    static __GPU__  const char* d_nname(int n);  ///< network layer name on device
     /// @}
+    /// @name layer access methods
+    /// @{
     __BOTH__ __INLINE__ Tensor &operator[](int i) {
         return (Tensor&)_mmu->du2obj(data[(i < 0) ? numel + i : i]);
     }
     __BOTH__ __INLINE__ int  slots() { return _store->numel; }
     __GPU__  __INLINE__ void reset(MMU *mmu, Tensor &store) {
-        init(0, T4_MODEL, 0);    // T4Base attributes
+        init(0, T4_MODEL, 0);                   /// * T4Base attributes
         _mmu   = mmu;
         _store = &store;
-        data   = store.data;     // cached entries
+        data   = store.data;                    /// * cached entries
         autograd = true;
-        npush(store);            // keep store as root
+        npush(store);                           /// * keep store as root
     }
     __GPU__ __INLINE__ Model &npush(DU v) {
         data[numel++] = v;
         U32 tsz = _store->numel;
         if (tsz <= numel) {
             _mmu->resize(*_store, tsz + T4_NET_SZ);
-            data = _store->data; // reset storage cached pointer
+            data = _store->data;                /// * reset storage cached pointer
         }
         return *this;
     }
     __GPU__ __INLINE__ Model  &npush(Tensor &t) { return npush(_mmu->obj2du(t)); }
     __GPU__ __INLINE__ DU     npop() { return data[--numel]; }
     __GPU__ __INLINE__ Tensor &output() { return (*this)[numel-1]; }
-    __GPU__ __INLINE__ Tensor &vector(U16 sz) {
-        return _mmu->tensor(sz);
-    }
-    __GPU__ __INLINE__ Tensor &tensor(U16 n, U16 h) {
-        return _mmu->tensor(n, h, 1, 1);
-    }
-    __GPU__ __INLINE__ Tensor &tensor(U16 n, U16 h, U16 w, U16 c) {
-        return _mmu->tensor(n, h, w, c);
-    }
-    __GPU__ __INLINE__ Tensor &tensor(U16 c1, U16 n, U16 h, U16 w, U16 c) {
-        return _mmu->tensor(c1, n, h, w, c);
-    }
-    
+    /// @}
+    /// @name main NN methods
+    /// @{
     __GPU__ Model  &add(t4_layer fn, U16 n=0, DU bias=DU0, U16 *opt=0);
-    __GPU__ Tensor &onehot();                           ///< calculate one-hot vector
     __GPU__ Model  &forward(Tensor &input);             ///< network feed forward
     __GPU__ Model  &backprop();                         ///< back propegation
     __GPU__ Model  &backprop(Tensor &hot);              ///< back propegation
+    /// @}
+    /// @name loss functions and gradiant decents
+    /// @{
+    __GPU__ Tensor &onehot();                           ///< calculate one-hot vector
     __GPU__ DU     loss(t4_loss op);                    ///< calc loss with cached one-hot vector
     __GPU__ DU     loss(t4_loss op, Tensor &hot);       ///< calc loss from one-hhot vector
     __GPU__ Model  &sgd();                              ///< stochastic gradiant decent
     __GPU__ Model  &adam();                             ///< Adam gradiant decent
-    ///
-    /// debug dump
-    ///
+    /// @}
+
+private:
+    /// @name internal tensor constructors
+    /// @{
+    __GPU__ __INLINE__ Tensor &_vec(U16 sz)                            { return _mmu->tensor(sz); }
+    __GPU__ __INLINE__ Tensor &_t4(U16 n, U16 h)                       { return _mmu->tensor(n, h, 1, 1); }
+    __GPU__ __INLINE__ Tensor &_t4(U16 n, U16 h, U16 w, U16 c)         { return _mmu->tensor(n, h, w, c); }
+    __GPU__ __INLINE__ Tensor &_t5(U16 c1, U16 n, U16 h, U16 w, U16 c) { return _mmu->tensor(c1, n, h, w, c); }
+    /// @}
+    /// @name debug functions
+    /// @{
     __GPU__ void   debug(Tensor &t, DU scale=10.0f);
     __GPU__ void   _view(DU *v, int H, int W, int C, DU scale=10.0f);
     __GPU__ void   _dump(DU *v, int H, int W, int C);
     __GPU__ void   _dump_dbdf(Tensor &df, Tensor &db);
-
-private:
+    /// @}
     /// @name Convolution and Linear initializer
     /// @{
     __GPU__ void   _iconv(Tensor &in, U16 c, DU bias, U16 *opt);
@@ -104,7 +107,10 @@ private:
     __GPU__ void   _fstep(Tensor &in, Tensor &out);
     __GPU__ int    _fconv(Tensor &in, Tensor &out);
     __GPU__ int    _flinear(Tensor &in, Tensor &out);
+    __GPU__ int    _ffilter(Tensor &in, Tensor &m, Tensor &out);
     __GPU__ int    _fpool(Tensor &in, Tensor &out, t4_layer fn);
+    __GPU__ int    _fsoftmax(Tensor &in, Tensor &out);
+    __GPU__ int    _flogsoftmax(Tensor &in, Tensor &out);
     /// @}
     /// @name backward ops
     /// @{
