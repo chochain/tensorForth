e5eea3559f63c8490b664fb9b48c46a84118ca77
gnii <chochain@yahoo.com>
2022-11-08 18:38:17 -0800

    ~/src/io/aio.cu add _save implementation (for model persistence

--- src/io/aio.cu
+++ src/io/aio.cu
@@ -20,56 +20,64 @@ using namespace std;
 #define NEXTNODE(n) ((obuf_node*)((char*)&node->data[0] + node->sz))
 
 __HOST__ int
-AIO::readline() {
+AIO::readline(std::istream &fin) {
     _istr->clear();
     char *tib = _istr->rdbuf();
-    cin.getline(tib, T4_IBUF_SZ, '\n');
+    fin.getline(tib, T4_IBUF_SZ, '\n');
     return strlen(tib);
 }
 
 __HOST__ obuf_node*
-AIO::process_node(obuf_node *node) {
+AIO::process_node(std::ostream &fout, obuf_node *node) {
     cudaDeviceSynchronize();        /// * make sure data is completely written
     
     char *v = (char*)node->data;    ///< fetch payload in buffered print node
     switch (node->gt) {
-    case GT_INT:   cout << (*(I32*)v); break;
-    case GT_FLOAT: cout << (*(F32*)v); break;
-    case GT_STR:   cout << v;          break;
+    case GT_INT:   fout << (*(I32*)v); break;
+    case GT_FLOAT: fout << (*(F32*)v); break;
+    case GT_STR:   fout << v;          break;
     case GT_FMT:   {
         obuf_fmt *f = (obuf_fmt*)v;
         //printf("FMT: b=%d, w=%d, p=%d, f='%c'\n", f->base, f->width, f->prec, f->fill);
-        cout << std::setbase(_radix = f->base)
+        fout << std::setbase(_radix = f->base)
              << std::setw(f->width)
              << std::setprecision(f->prec ? f->prec : -1)
              << std::setfill((char)f->fill);
     } break;
-    case GT_OBJ: _print_obj(*(DU*)v); break;
+    case GT_OBJ: _print_obj(fout, *(DU*)v); break;
     case GT_OPX: {
         _opx *o = (_opx*)v;
         // printf("OP=%d a=%d, n=0x%08x=%f\n", o->op, o->a, DU2X(o->n), o->n);
         switch (o->op) {
-        case OP_WORDS: _mmu->words(cout);                               break;
-        case OP_SEE:   _mmu->see(cout, (IU)o->a);                       break;
-        case OP_DUMP:  _mmu->mem_dump(cout, (IU)o->a, (IU)o->n);        break;
-        case OP_SS:    _mmu->ss_dump(cout, (IU)node->id, o->a, _radix); break;
+        case OP_WORDS: _mmu->words(fout);                               break;
+        case OP_SEE:   _mmu->see(fout, (IU)o->a);                       break;
+        case OP_DUMP:  _mmu->mem_dump(fout, (IU)o->a, (IU)o->n);        break;
+        case OP_SS:    _mmu->ss_dump(fout, (IU)node->id, o->a, _radix); break;
         case OP_DATA:
             node = NEXTNODE(node);                          ///< get dataset repo name
             _fetch(o->n, (bool)o->a, (char*)node->data);    /// * fetch first batch
             break;
         case OP_FETCH: _fetch(o->n, (bool)o->a); break;     /// * fetch/rewind dataset batch
+        case OP_SAVE:
+            node = NEXTNODE(node);                          ///< get dataset repo name
+            _save(o->n, (bool)o->a, (char*)node->data);
+            break;
+        case OP_LOAD:
+            node = NEXTNODE(node);
+            _load(o->n, (bool)o->a, (char*)node->data);
+            break;
         }
     } break;
-    default: cout << "print type not supported: " << (int)node->gt; break;
+    default: fout << "print type not supported: " << (int)node->gt; break;
     }
     return NEXTNODE(node);
 }
 
 __HOST__ void
-AIO::flush() {
+AIO::flush(std::ostream &fout) {
     obuf_node *node = (obuf_node*)_ostr->rdbuf();
     while (node->gt != GT_EMPTY) {          // 0
-        node = process_node(node);
+        node = process_node(fout, node);
     }
     _ostr->clear();
 }
@@ -78,22 +86,22 @@ AIO::flush() {
 ///
 #if T4_ENABLE_OBJ
 __HOST__ void
-AIO::_print_obj(DU v) {
+AIO::_print_obj(std::ostream &fout, DU v) {
     T4Base &b = _mmu->du2obj(v);
     switch (b.ttype) {
     case T4_VIEW:
     case T4_TENSOR:
-    case T4_DATASET: _print_tensor(v); break;
-    case T4_MODEL:   _print_model(v);  break;
+    case T4_DATASET: _print_tensor(fout, v); break;
+    case T4_MODEL:   _print_model(fout, v);  break;
     }
 }
 __HOST__ void
-AIO::_print_vec(DU *d, int mj, int rj, int c) {
-    cout << setprecision(_prec) << "{";                 /// set precision
+AIO::_print_vec(std::ostream &fout, DU *d, int mj, int rj, int c) {
+    fout << setprecision(_prec) << "{";                 /// set precision
     for (int j=0; j<rj; j++) {
         DU *dx = &d[j * c];
         for (int k=0; k < c; k++) {
-            cout << (k>0 ? "_" : " ") << *dx++;
+            fout << (k>0 ? "_" : " ") << *dx++;
         }
     }
     int x = mj - rj;
@@ -101,103 +109,103 @@ AIO::_print_vec(DU *d, int mj, int rj, int c) {
     for (int j=(x > rj ? x : rj); j<mj; j++) {
         DU *dx = &d[j * c];
         for (int k=0; k < c; k++) {
-            cout << (k>0 ? "_" : " ") << *dx++;
+            fout << (k>0 ? "_" : " ") << *dx++;
         }
     }
-    cout << " }";
+    fout << " }";
 }
 __HOST__ void
-AIO::_print_mat(DU *d, int mi, int mj, int ri, int rj, int c) {
-    cout.flags(ios::showpos | ios::right | ios::fixed); /// enforce +- sign
+AIO::_print_mat(std::ostream &fout, DU *d, int mi, int mj, int ri, int rj, int c) {
+    fout.flags(ios::showpos | ios::right | ios::fixed); /// enforce +- sign
     bool full = (mi * mj) <= _thres;
     int  x    = full ? mj : rj;
     DU   *d0  = d;
     for (int i=0, i1=1; i<ri; i++, i1++, d0+=(mj * c)) {
-        _print_vec(d0, mj, x, c);
-        cout << (i1==mi ? "" : "\n\t");
+        _print_vec(fout, d0, mj, x, c);
+        fout << (i1==mi ? "" : "\n\t");
     }
     int y = full ? ri : mi - ri;
-    if (y > ri) cout << "...\n\t";
+    if (y > ri) fout << "...\n\t";
     else y = ri;
     DU *d1 = (d + y * mj * c);
     for (int i=y, i1=i+1; i<mi; i++, i1++, d1+=(mj * c)) {
-        _print_vec(d1, mj, x, c);
-        cout << (i1==mi ? "" : "\n\t");
+        _print_vec(fout, d1, mj, x, c);
+        fout << (i1==mi ? "" : "\n\t");
     }
 }
 __HOST__ void
-AIO::_print_tensor(DU v) {
-    auto   range = [this](int n) { return (n < _edge) ? n : _edge; };
+AIO::_print_tensor(std::ostream &fout, DU v) {
+    auto range = [this](int n) { return (n < _edge) ? n : _edge; };
 
     Tensor &t = (Tensor&)_mmu->du2obj(v);
     DU     *d = t.data;                     /// * short hand
     WARN("aio#print_tensor::T[%x]=%p data=%p\n", DU2X(v), &t, d);
 
-    ios::fmtflags fmt0 = cout.flags();
-    cout << setprecision(-1);               /// * standard format
+    ios::fmtflags fmt0 = fout.flags();
+    fout << setprecision(-1);               /// * standard format
     switch (t.rank) {
     case 1: {
         cout << "vector[" << t.numel << "] = ";
         int ri = (t.numel < _thres) ? t.numel : range(t.numel);
-        _print_vec(d, t.numel, ri, 1);
+        _print_vec(fout, d, t.numel, ri, 1);
     } break;
     case 2: {
         int mi = t.H(), mj = t.W(), ri = range(mi),  rj = range(mj);
-        cout << "matrix[" << mi << "," << mj << "] = {\n\t";
-        _print_mat(d, mi, mj, ri, rj, 1);
+        fout << "matrix[" << mi << "," << mj << "] = {\n\t";
+        _print_mat(fout, d, mi, mj, ri, rj, 1);
         cout << " }";
     } break;
     case 4: {
         int n  = t.N(), mi = t.H(), mj = t.W(), mc = t.C();
         int ri = range(mi), rj = range(mj);
         int pg = mi * mj * mc;
-        cout << "tensor["
+        fout << "tensor["
              << n << "," << mi << "," << mj << "," << mc
              << "] = {\n\t";
         for (int i = 0; i < n; i++, d += pg) {
-            if (mj==1) _print_mat(d, mj, mi, rj, ri, mc);
-            else       _print_mat(d, mi, mj, ri, rj, mc);
-            cout << ((i+1) < n ? "\n\t" : "");
+            if (mj==1) _print_mat(fout, d, mj, mi, rj, ri, mc);
+            else       _print_mat(fout, d, mi, mj, ri, rj, mc);
+            fout << ((i+1) < n ? "\n\t" : "");
         }
-        cout << " }";
+        fout << " }";
     } break;
     case 5: {
-        cout << "tensor[" << t.parm << "]["
+        fout << "tensor[" << t.parm << "]["
              << t.N() << "," << t.H() << "," << t.W() << "," << t.C()
              << "] = {...}";
     } break;        
-    default: cout << "tensor rank=" << t.rank << " not supported";
+    default: fout << "tensor rank=" << t.rank << " not supported";
     }
-    cout << "\n";
-    cout.flags(fmt0);
+    fout << "\n";
+    fout.flags(fmt0);
 }
 __HOST__ void
-AIO::_print_model(DU v) {
-    auto tinfo = [this](Tensor &t, int i, int fn) { ///> layer info
-        cout << "[" << std::setw(3) << i << "] "
+AIO::_print_model(std::ostream &fout, DU v) {
+    auto tinfo = [this, &fout](Tensor &t, int i, int fn) { ///> layer info
+        fout << "[" << std::setw(3) << i << "] "
              << Model::nname(fn) << ":";
-        _mmu->to_s(cout, t);
+        _mmu->to_s(fout, t);
         int sz = 0;
         for (int n = 0; n < 4; n++) {
             sz += t.grad[n] ? t.grad[n]->numel : 0;
         }
-        cout << ", #param=" << sz;
+        fout << ", #param=" << sz;
     };
-    auto finfo = [this](Tensor **g) {
+    auto finfo = [this, &fout](Tensor **g) {
         for (int i=0; g[i] && i < 2; i++) {
-            cout << " "; _mmu->to_s(cout, *g[i]);
+            fout << " "; _mmu->to_s(fout, *g[i]);
         }
     };
     Model &m = (Model&)_mmu->du2obj(v);
     int   sz = m.numel;
     if (!m.is_model()) return;
     
-    cout << "NN model[" << sz-1 << "/" << m.slots() << "]" << endl;
+    fout << "NN model[" << sz-1 << "/" << m.slots() << "]" << endl;
     for (int i = 1; i < sz; i++) {  /// skip root[0]
         Tensor &t = m[i];
         tinfo(t, i, (i==(sz-1)) ? 0 : t.grad_fn);
         if (_trace && t.grad_fn != L_NONE) finfo(t.grad);
-        cout << endl;
+        fout << endl;
     }
 }
 ///
@@ -260,6 +268,84 @@ AIO::_fetch(DU top, bool more, char *ds_name) {
     if (!cp->eof) ds.load_batch(cp->data, cp->label);
     TRACE1("batch[%d] %d record(s) loaded\n", ds.batch_id - 1, batch_sz);
     
+    return 0;
+}
+
+#include <fstream>
+__HOST__ int
+AIO::_save(DU top, U16 vid, char* fname) {
+    printf("%d|AIO::save model to '%s' ", vid, fname);
+    Model &m = (Model&)_mmu->du2obj(top);
+    ofstream of(fname, ios_base::binary);     ///< open an output file
+    if (!of.is_open()) {
+        ERROR("=> failed to open for output\n");
+        return 1;
+    }
+    of << "\\ tensorForth model\n\\ version v3.0\n";
+    _save_model(of, m);
+    _save_param(of, m);
+    of << "\n---" << endl;
+    of.close();
+    cout << "=> completed" << endl;
+    return 0;
+}
+
+__HOST__ int
+AIO::_load(DU top, U16 vid, char* fname) {
+    Model &m = (Model&)_mmu->du2obj(top);
+    printf("%d|AIO::load model from '%s'\n", vid, fname);
+    _istr->clear();
+    char *tib = _istr->rdbuf();
+    strcpy(tib, "123 . words bye");
+    return 0;
+}
+
+__HOST__ int
+AIO::_save_model(std::ostream &fout, Model &m) {
+    for (U16 i = 1; i < m.numel - 1; i++) {
+        Tensor &in = m[i], &out = m[i+1];
+        t4_layer fn = in.grad_fn;              ///< layer function
+        DU       p  = 0.001 * in.parm;         ///< layer parameter
+        switch(fn) {
+        case L_CONV:   fout << p << " " << out.C() << " "; break;
+        case L_LINEAR: fout << p << " " << out.H() << " "; break;
+        case L_MAXPOOL:
+        case L_AVGPOOL:
+        case L_MINPOOL: fout << in.parm << " ";            break;
+        case L_DROPOUT: fout << p << " ";                  break;
+        default: break;
+        }
+        const char *nm = Model::nname(fn);
+        fout << nm << endl;
+        
+        printf("\n%2d> %s [%d,%d,%d,%d]\tp=%-2d => out[%d,%d,%d,%d]",
+            i, nm, in.N(), in.H(), in.W(), in.C(), in.parm,
+            out.N(), out.H(), out.W(), out.C());
+        
+    }
+    return 0;
+}
+
+__HOST__ int
+AIO::_save_param(std::ostream &fout, Model &m) {
+    auto _dump = [&fout](const char pn, const char *nm, Tensor &t) {
+        fout << "\n--- " << pn << "." << nm << endl;
+        fout.write((char*)t.data, t.numel * sizeof(DU));
+    };
+    for (U16 i = 1; i < m.numel - 1; i++) {
+        Tensor   &in = m[i];                    ///< nth model layer
+        t4_layer fn  = in.grad_fn;              ///< layer function
+        const char *nm = Model::nname(fn);      ///< layer name
+        switch(fn) {
+        case L_CONV:
+        case L_LINEAR:
+            _dump('w', nm, *in.grad[0]);
+            _dump('b', nm, *in.grad[1]);  break;
+        case L_DROPOUT:
+            _dump('m', nm, *in.grad[0]);  break;
+        default: break;
+        }
+    }
     return 0;
 }
 #endif // T4_ENABLE_OBJ
