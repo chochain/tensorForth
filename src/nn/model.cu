9478eaa048e102225349f3843baf2e247b8509a8
gnii <chochain@yahoo.com>
2023-05-28 19:28:47 -0700

    ~/src/nn/model.cu batchnorm allocate x_hat

--- src/nn/model.cu
+++ src/nn/model.cu
@@ -191,11 +191,11 @@ __GPU__ void
 Model::_ibatchnorm(Tensor &in) {
     const int C1 = in.C();                       /// C0==C1
     Tensor &out = _mmu->copy(in);                /// * retain dimensions
-    
-    in.grad[0] = &_vec(C1).map(O_FILL, DU1);     ///> gamma (scale)
-    in.grad[1] = &_vec(C1).map(O_FILL, DU0);     ///> beta (shift)
-    in.grad[2] = &_vec(C1);                      ///> batch mean
-    in.grad[3] = &_vec(C1);                      ///> batch stdvar
+
+    in.grad[0] = &_mmu->copy(in);                ///> gamma * x_hat
+    in.grad[1] = &_vec(C1).map(O_FILL, DU1);     ///> gamma (scale)
+    in.grad[2] = &_vec(C1).map(O_FILL, DU0);     ///> beta (shift)
+    in.grad[3] = &_vec(C1*2);                    ///> batch mean, stdvar
 
     in.parm = INT(1000.0 * 0.1);                 ///> EMA momentum
     
