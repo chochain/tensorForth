129e33861cedb237f045e404327418e678f1d17a
gnii <chochain@yahoo.com>
2023-06-05 11:23:57 -0700

    ~/src/nn/gradiant.cu implement grad_alloc; update sgd, adam

--- src/nn/gradiant.cu
+++ src/nn/gradiant.cu
@@ -9,9 +9,8 @@
 
 #if T4_ENABLE_OBJ
 __KERN__ void k_sgd(
-    DU *G, DU *DG,                                  ///< func, input, output tensors
-    DU lr, DU a, bool zero,
-    int HW
+    DU *G, DU *DG, DU *M,        ///< w, dw, and momemtum tensors
+    DU lr, DU b, int HW          ///< learn rate, beta(momemtum), and dw zero flag
     ) {
     const int i  = threadIdx.x + blockIdx.x * blockDim.x;  ///< element index
     const int c  = blockIdx.y, C = gridDim.y;              ///< channel deep
@@ -19,42 +18,80 @@ __KERN__ void k_sgd(
     const int k  = c + i * C + ns;                         ///< output tensor index
     
     if (k < HW) {
-        G[k] = (a < DU_EPS)
-            ? G[k] - lr * DG[k]
-            : a * G[k] + (1 - a) * DG[k];     /// * with momentum (exp moving avg)
-        if (zero) DG[k] = DU0;
+        if (b < DU_EPS) G[k] -= lr * DG[k];
+        else {
+            M[k]  = b * M[k] + (1.0 - b) * DG[k];
+            G[k] -= lr * M[k];
+        }
+        DG[k] = DU0;                                       /// * zero after batch
     }
 }
 
 __KERN__ void k_adam(
-    DU *G, DU *DG, DU *M, DU *V,     ///< func, input, output tensors
-    DU lr, DU b1, DU b2, bool zero,  ///< lr = eta * sqrt(1 - b2^t) / (1 - b1^t) / N
-    int HW
+    DU *G, DU *DG, DU *M, DU *V, ///< w, dw, and momemtum tensors
+    DU lr, DU b1, DU b2, int HW  ///< learn rate, beta(momemtum), and dw zero flag
     ) {
     const int i  = threadIdx.x + blockIdx.x * blockDim.x;  ///< element index
     const int c  = blockIdx.y, C = gridDim.y;              ///< channel deep
     const int ns = blockIdx.z * HW * C;                    ///< batch slice index
     const int k  = c + i * C + ns;                         ///< output tensor index
-
-    if (i < HW) {
-        DU dg = DG[k];
-        DU mk = M[k] = b1 * M[k] + (1 - b1) * dg;
-        DU vk = V[k] = b2 * V[k] + (1 - b2) * dg * dg;
+    
+    if (k < HW) {
+        DU dg = DG[k] / gridDim.z;
+        DU mk = M[k] = b1 * M[k] + (1.0 - b1) * dg;
+        DU vk = V[k] = b2 * V[k] + (1.0 - b2) * dg * dg;
         G[k] -= lr * M[k] / (SQRT(V[k]) + DU_EPS);
-        
-        if (zero) DG[k] = DU0;
+        DG[k] = DU0;
+    }
+}
+
+///
+///> grad_alloc
+///  @brief - allocate Momentum and Velocity tensors
+///
+__GPU__ Model&
+Model::grad_alloc(Tensor &in, bool do_w, bool do_b) {
+    Tensor *w = in.grad[0], *dw = in.grad[2];
+    Tensor *b = in.grad[1], *db = in.grad[3];
+    
+    switch (_opti) {
+    case OPTI_SGD:
+        /// * fake tensor references (not used)
+        if (do_w && !in.mtum[0]) { in.mtum[0] = in.mtum[2] = dw; }
+        if (do_b && !in.mtum[1]) { in.mtum[1] = in.mtum[3] = db; }
+        break;
+    case OPTI_SGDM:
+        if (do_w && !in.mtum[0]) {
+            in.mtum[0] = &_mmu->copy(*w);     ///< m of w
+            in.mtum[2] = dw;                  ///< dummy
+        }
+        if (do_b && !in.mtum[1]) {
+            in.mtum[1] = &_mmu->copy(*b);     ///< m of b
+            in.mtum[3] = db;                  ///< dummy
+        }
+        break;
+    case OPTI_ADAM:
+        if (do_w && !in.mtum[0]) {
+            in.mtum[0] = &_mmu->copy(*w);     ///< m of w
+            in.mtum[2] = &_mmu->copy(*dw);    ///< v of w
+        }
+        if (do_b && !in.mtum[1]) {
+            in.mtum[1] = &_mmu->copy(*b);     ///< m of b
+            in.mtum[3] = &_mmu->copy(*db);    ///< v of b
+        }
+        break;
     }
 }
 ///
 ///> grandiant descent iterator
 ///
 __GPU__ Model&
-Model::gradiant(const char *nm, GdFunc fn, t4_optimizer opti) {
+Model::gradiant(const char *nm, GdFunc fn) {
     auto step = [this, fn](const char n,
             Tensor &g, Tensor &dg, Tensor &m, Tensor &v) {
             TRACE1("\n    %c[%d,%d,%d,%d] Σ=%6.3f - %6.3f",
                    n, g.N(), g.H(), g.W(), g.C(), g.sum(), dg.sum());
-            fn(_gparm, _gzero, g, dg, m, v);
+            fn(_gparm, g, dg, m, v);
             TRACE1(" => %cΣ=%6.3f", n, g.sum());
     };
     Tensor &n1 = (*this)[1];                       ///< reference model input layer
@@ -73,25 +110,13 @@ Model::gradiant(const char *nm, GdFunc fn, t4_optimizer opti) {
         ///
         /// * initialize adam m and v tensors for each layer if needed
         ///
-        switch (opti) {
-        case OPTI_SGD:
-            if (dw && !in.adam[0]) in.adam[0] = w; in.adam[1] = dw;
-            if (dw && !in.adam[2]) in.adam[2] = b; in.adam[1] = db;
-            break;
-        case OPTI_ADAM:
-            if (dw && !in.adam[0]) {
-                in.adam[0] = &_mmu->copy(*w).fill(DU0);     ///< m of w
-                in.adam[1] = &_mmu->copy(*dw).fill(DU0);    ///< v of w
-            }
-            if (db && !in.adam[2]) {
-                in.adam[2] = &_mmu->copy(*b).fill(DU0);     ///< m of b
-                in.adam[3] = &_mmu->copy(*db).fill(DU0);    ///< v of b
-            }
-        }
-        if (dw && dw->is_same_shape(*w))
-            step('w', *w, *dw, *in.adam[0], *in.adam[1]);
-        if (db && db->is_same_shape(*b))
-            step('b', *b, *db, *in.adam[2], *in.adam[3]);
+        bool w_ok = dw && dw->is_same_shape(*w);
+        bool b_ok = db && db->is_same_shape(*b);
+        
+        grad_alloc(in, w_ok, b_ok);
+        
+        if (w_ok) step('w', *w, *dw, *in.mtum[0], *in.mtum[2]);
+        if (b_ok) step('b', *b, *db, *in.mtum[1], *in.mtum[3]);
     }
     TRACE1("\nModel#%s %5.2f ms\n", nm, _mmu->ms() - t0);
     return *this;
@@ -102,43 +127,43 @@ Model::gradiant(const char *nm, GdFunc fn, t4_optimizer opti) {
 ///       because filters are fixed size
 ///
 __GPU__ Model&
-Model::sgd(DU lr, DU a, bool zero) {               /// a=momentum
-    auto update = [](DU *parm, bool zero,
-                     Tensor &g, Tensor &dg, Tensor &m, Tensor &v) {
+Model::sgd(DU lr, DU b) {                          /// a=momentum
+    auto update = [](DU *parm, Tensor &g, Tensor &dg, Tensor &m, Tensor &v) {
         const int HW = g.H() * g.W();
         const dim3 blk(T4_WARP_SQ, 1, 1);          ///< default blocks
         const dim3 grd((HW + blk.x - 1)/blk.x, g.C(), g.N());
         
-        k_sgd<<<grd,blk>>>(g.data, dg.data, parm[0], parm[1], zero, HW);
+        k_sgd<<<grd,blk>>>(
+            g.data, dg.data, m.data, parm[0], parm[1], HW);
     };
-    _gparm[0] = lr / batch_size();                 /// eta / batch_size
-    _gparm[1] = a;
+    _gparm[0] = lr / batch_size();                 ///> eta / batch_size
+    _gparm[1] = _iter > 1 ? b : DU0;               ///> beta
     _gparm[2] = DU0;
-    _gzero    = zero;
-    gradiant("sgd", update, OPTI_SGD);
+    _opti     = b > DU_EPS ? OPTI_SGDM : OPTI_SGD;
+    
+    gradiant("sgd", update);
+    
     return *this;
 }
 
 __GPU__ Model&
-Model::adam(DU lr, DU b1, DU b2, bool zero) {
-    static int t = 1;
-    auto update = [](DU *parm, bool zero,
-                     Tensor &g, Tensor &dg, Tensor &m, Tensor &v) {
+Model::adam(DU lr, DU b1, DU b2) {
+    auto update = [](DU *parm, Tensor &g, Tensor &dg, Tensor &m, Tensor &v) {
         const int HW = g.H() * g.W();
-        const dim3 blk(T4_WARP_SQ, 1, 1);               ///< default blocks
+        const dim3 blk(T4_WARP_SQ, 1, 1);          ///< default blocks
         const dim3 grd((HW + blk.x - 1)/blk.x, g.C(), g.N());
 
-        k_adam<<<grd,blk>>>(g.data, dg.data, m.data, v.data,
-                            parm[0], parm[1], parm[2], zero, HW);
+        k_adam<<<grd,blk>>>(
+            g.data, dg.data, m.data, v.data,
+            parm[0], parm[1], parm[2], HW);
     };
-    _gparm[0] = lr * SQRT(1 - POW(b2, t)) / (1 - POW(b1, t)) / batch_size();
-    _gparm[1] = b1;
-    _gparm[2] = b2;
-    _gzero    = zero;
-    gradiant("adam", update, OPTI_ADAM);
+    _gparm[0] = lr * SQRT(1 - POW(b2, _iter)) / (1 - POW(b1, _iter));
+    _gparm[1] = _iter > 1 ? b1 : DU0;
+    _gparm[2] = _iter > 1 ? b2 : DU0;
+    _opti     = OPTI_ADAM;
     
-    t++;
-    
+    gradiant("adam", update);
+
     return *this;
 }
 #endif  // T4_ENABLE_OBJ
