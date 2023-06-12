a44b65c945bc6d07f9a9240666bb7b4769876796
gnii <chochain@yahoo.com>
2023-06-12 10:35:12 -0700

    ~/tests/t_mnist.cu update Ndata=>Corpus, #load=>#fetch

--- tests/t_mnist.cu
+++ tests/t_mnist.cu
@@ -4,20 +4,20 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
-#include "../src/vu/mnist_vu.h"
-#include "../src/ldr/loader.h"
+#include "loader.h"
+#include "mnist_vu.h"
 
 int main(int argc, char **argv) {
     setenv("DISPLAY", ":0", 0);
     cudaSetDevice(0);
 
-    Loader::init();
+    Loader::init(1);
     if (gui_init(&argc, argv)) return -1;
     
-    Ndata *nd = Loader::get(0, "mnist_test");
+    Corpus *nd = Loader::get(0, "mnist_test");
     if (!nd) return 0;
 
-    MnistVu *vu = new MnistVu(*nd->load(1200));
+    MnistVu *vu = new MnistVu(*nd->fetch(0, 1200));
     gui_add(vu);
         
     return gui_loop();
