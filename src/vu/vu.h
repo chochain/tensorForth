4484e9b44e8758917210e2fc0b10740155a0813d
gnii <chochain@yahoo.com>
2023-07-11 18:53:37 -0700

    ~/src/nn/backprop simplify sigmoid backprop (p - y)

--- src/vu/vu.h
+++ src/vu/vu.h
@@ -8,7 +8,7 @@
 #define T4_VU_H
 #include <GL/gl.h>
 #include "ten4_types.h"
-#include "corpus.h"              // in ../nn
+#include "corpus.h"              // in ../ldr
 
 typedef U32                         TColor;
 typedef struct cudaGraphicsResource CuGfxPbo;
