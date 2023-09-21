64b81aa61892b47d4ff23cb922c076ae482c4a1f
gnii <chochain@yahoo.com>
2023-09-21 07:41:05 -0700

    ~/src/aio.h add tensor persistence methods

--- src/aio.h
+++ src/aio.h
@@ -1,15 +1,17 @@
 /**
  * @file
- * @brief tensorForth Asyn IO module
+ * @brief AIO class - asyn IO module implementation
  *
  * <pre>Copyright (C) 2021 GreenII, this file is distributed under BSD 3-Clause License.</pre>
  *
  */
-#ifndef TEN4_SRC_AIO_H_
-#define TEN4_SRC_AIO_H_
+#ifndef TEN4_SRC_AIO_H
+#define TEN4_SRC_AIO_H
 #include "istream.h"
 #include "ostream.h"
 #include "mmu.h"
+
+#define IO_TRACE(...)      { if (_mmu->trace()) INFO(__VA_ARGS__); }
 
 class AIO : public Managed {
 public:
@@ -17,22 +19,39 @@ public:
     Ostream *_ostr;         ///< managed output stream
     MMU     *_mmu;          ///< memory managing unit
     int     _radix = 10;    ///< output stream radix
-    int     _thres = 100;   ///< print number of element threshold
+    int     _thres = 10;    ///< max cell count for each dimension
     int     _edge  = 3;     ///< number of tensor edge items
     int     _prec  = 4;     ///< shown floating point precision
-    int     _trace = 0;     ///< debug tracing control
 
-    AIO(MMU *mmu, int verbose=0) :
-        _istr(new Istream()), _ostr(new Ostream()), _mmu(mmu), _trace(verbose) {}
+    AIO(MMU *mmu) :
+        _istr(new Istream()), _ostr(new Ostream()), _mmu(mmu) {}
 
-    __HOST__ Istream *istream() { return _istr; }
-    __HOST__ Ostream *ostream() { return _ostr; }
+    __HOST__ Istream   *istream() { return _istr; }
+    __HOST__ Ostream   *ostream() { return _ostr; }
+    __HOST__ int       readline(std::istream &fin);
+    __HOST__ obuf_node *process_node(std::ostream &fout, obuf_node *node);
+    __HOST__ void      flush(std::ostream &fout);
 
-    __HOST__ int  readline();
-    __HOST__ void print_vec(DU *d, int mi, int ri);
-    __HOST__ void print_mat(DU *d, int mi, int mj, int ri, int rj);
-    __HOST__ void print_obj(DU v);
-    __HOST__ void print_node(obuf_node *node);
-    __HOST__ void flush();
+private:
+#if T4_ENABLE_OBJ // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    ///
+    /// Tensor print methods
+    ///
+    __HOST__ void _print_obj(std::ostream &fout, DU v);
+    __HOST__ void _print_vec(std::ostream &fout, DU *vd, int W, int C);
+    __HOST__ void _print_mat(std::ostream &fout, DU *md, U16 *shape);
+    __HOST__ void _print_tensor(std::ostream &fout, Tensor &t);
+    ///
+    /// Tensor persistence (i.e. serialization) methods
+    ///
+    __HOST__ int  _tsave(DU top, U16 mode, char *fname);
+    __HOST__ int  _tsave_txt(std::ostream &fout, Tensor &t);
+    __HOST__ int  _tsave_raw(std::ostream &fout, Tensor &t);
+    __HOST__ int  _tsave_npy(std::ostream &fout, Tensor &t);
+#else  // T4_ENABLE_OBJ
+    __HOST__ void _print_obj(std::ostream &fout, DU v) {}        ///< stub
+
+#endif // T4_ENABLE_OBJ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 };
-#endif // TEN4_SRC_AIO_H_
+
+#endif // TEN4_SRC_AIO_H
