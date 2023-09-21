5d363543fda9760dfbc20c80287c97ea8fb755aa
gnii <chochain@yahoo.com>
2023-09-21 08:12:55 -0700

    ~/src/tenvm.h add tensor TOS macros; dispatch ss,st,ts,tt tensor ops

--- src/tenvm.h
+++ src/tenvm.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief tensorForth - TensorVM, extended ForthVM classes, to handle tensor ops
+ * @brief TensorVM class , extended ForthVM classes, tensor handler interface
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
@@ -8,53 +8,63 @@
 #define TEN4_SRC_TENVM_H
 #include "eforth.h"                         /// extending ForthVM
 
-#define NO_OBJ(v) (*(U8*)&(v) &= ~T4_OBJ_FLAG)  /**< tensor flag mask for top       */
-#define EXP(d)    (expf(d))                     /**< exponential(float)             */
+#if T4_ENABLE_OBJ
+
+#define TTOS      ((Tensor&)mmu.du2obj(top))        /**< tensor on TOS      */
+#define TNOS      ((Tensor&)mmu.du2obj(ss[-1]))     /**< tensor on NOS      */
+#define TOS1T     (IS_OBJ(top) && TTOS.is_tensor())
+#define TOS2T     (ss.idx > 0 && TOS1T && IS_OBJ(ss[-1]) && TNOS.is_tensor())
+#define TOS3T     (ss.idx > 1 && TOS2T && IS_OBJ(ss[-2]) && mmu.du2obj(ss[-2]).is_tensor())
+
 typedef enum {
     KEEP = false,
     DROP = true
-} t_drop;
+} t4_drop_opt;
 
 class TensorVM : public ForthVM {
 public:
-#if   !T4_ENABLE_OBJ
-    __GPU__ TensorVM(int khz, Istream *istr, Ostream *ostr, MMU *mmu0) :
-        ForthVM(khz, istr, ostr, mmu0) {}
-    __GPU__ void init_t() { ForthVM::init_f(); }
-    
-#else // T4_ENABLE_OBJ
-    __GPU__ TensorVM(int khz, Istream *istr, Ostream *ostr, MMU *mmu0) :
-        ForthVM(khz, istr, ostr, mmu0) {
-        VLOG1("\\  ::TensorVM(...) sizeof(Tensor)=%ld\n", sizeof(Tensor));
+    __GPU__ TensorVM(int id, Istream *istr, Ostream *ostr, MMU *mmu0)
+        : ForthVM(id, istr, ostr, mmu0) {
+        VLOG1("\\  ::TensorVM[%d](...) sizeof(Tensor)=%ld\n", id, sizeof(Tensor));
     }
-    __GPU__ void init() final { init_t(); } ///< TODO: CC - polymorphism does not work here?
-    __GPU__ void init_t();                  ///< so fake it
+    __GPU__ virtual void init() final;      ///< override ForthVM.init()
     
 protected:
-    int   ten_lvl = 0;                      ///< tensor input level
-    int   ten_off = 0;                      ///< tensor offset (storage index)
+    int    ten_lvl = 0;                     ///< tensor input level
+    int    ten_off = 0;                     ///< tensor offset (storage index)
     ///
     /// override literal handler
     ///
-    __GPU__ void tprint(DU d);              ///< tensor dot (print)
-    __GPU__ int  number(char *str) final;   ///< TODO: CC - this worked, why?
+    __GPU__ virtual int  number(char *str); ///< TODO: CC - worked without 'final', why?
     ///
-    /// mmu proxy functions
+    /// tensor ops based on number of operands
     ///
-    __GPU__ void add_to_tensor(DU n);       ///< add tensor to parameter field
+    __GPU__ void xop1(t4_ten_op op, DU v=DU0);            /// 1-operand ops in-place
+    __GPU__ void xop1x(t4_ten_op op);                     /// 1-operand ops with new tensor
+    __GPU__ void xop2(t4_ten_op op, t4_drop_opt x=KEEP);  /// 2-operand ops
+    
+private:
     ///
-    /// tensor ops
+    /// tensor ops based on data types
     ///
-    __GPU__ void texp();                    ///< element-wise all tensor elements
-    __GPU__ void tmat(mat_op op, t_drop x); ///< matrix-matrix element ops (Hadamard)
-    __GPU__ void tmul(t_drop x);            ///< matrix-matrix multiplication @
-    __GPU__ void tdiv(t_drop x);            ///< matrix-matrix division (no broadcast)
-    __GPU__ void tinv();                    ///< matrix inversion (Gauss-Jordan)
-    __GPU__ void tlu();                     ///< matrix LU decomposition
-    __GPU__ void tdet();                    ///< matrix determinant (via LU)
-    __GPU__ void ttrans();                  ///< matrix transpose
-    __GPU__ void solve();                   ///< solve linear equation Ax = b
-    __GPU__ void gemm();                    ///< GEMM C' = alpha * A x B + beta * C
-#endif // T4_ENABLE_OBJ
+    __GPU__ void _ss_op(t4_ten_op op);                    ///< scalar-scalar (eForth) ops
+    __GPU__ Tensor &_st_op(t4_ten_op op, t4_drop_opt x);  ///< scalar tensor op (broadcast)
+    __GPU__ Tensor &_ts_op(t4_ten_op op, t4_drop_opt x);  ///< tensor scalar op (broadcast)
+    __GPU__ Tensor &_tt_op(t4_ten_op op);                 ///< tensor tensor op
+    ///
+    /// tensor-tensor ops
+    ///
+    __GPU__ Tensor &_tinv(Tensor &A);                     ///< matrix inversion
+    __GPU__ Tensor &_tdot(Tensor &A, Tensor &B);          ///< matrix-matrix multiplication @
+    __GPU__ Tensor &_tdiv(Tensor &A, Tensor &B);          ///< matrix-matrix division (no broadcast)
+    __GPU__ Tensor &_solv(Tensor &A, Tensor &B);          ///< solve linear equation Ax = b
+    __GPU__ void   _gemm();                               ///< GEMM C' = alpha * A x B + beta * C
+    ///
+    /// tensor IO
+    ///
+    __GPU__ void   _pickle(bool save);                    ///< save/load a tensor to/from a file
+
 };
+
+#endif // T4_ENABLE_OBJ
 #endif // TEN4_SRC_TENVM_H
