9ee4dee27be3e08043e56a8b428e5e7105b4e83c
gnii <chochain@yahoo.com>
2023-09-21 08:21:58 -0700

    ~/src/vm.h as the VM base-class; add virtual pre/post/resume

--- src/vm.h
+++ src/vm.h
@@ -1,42 +1,42 @@
 /**
  * @file
- * @brief tensorForth - eForth VM virtual classes
+ * @brief VM class - eForth VM virtual class interface
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
 #ifndef TEN4_SRC_VM_H
 #define TEN4_SRC_VM_H
-#include "aio.h"            // async IO (includes Istream, Ostream)
+#include "aio.h"            // async IO (includes Istream, Ostream), in ../io
 ///
 ///@name Cross platform support
 ///@{
 #define ENDL         '\n'
-#define delay(ticks) { clock_t t = clock()+ticks; while (clock()<t) yield(); }
+#define delay(ticks) { U64 t = clock64() + (ticks * mmu.khz()); while ((U64)clock64()<t) yield(); }
 #define yield()                        /**< TODO: multi-VM  */
 ///@}
-#define VLOG1(...) { if (mmu.trace() > 0) INFO(__VA_ARGS__); }
-#define VLOG2(...) { if (mmu.trace() > 1) INFO(__VA_ARGS__); }
+#define VLOG1(...)         if (mmu.trace() > 0) INFO(__VA_ARGS__);
+#define VLOG2(...)         if (mmu.trace() > 1) INFO(__VA_ARGS__);
 ///
 /// virtual machine base class
 ///
-typedef enum { VM_READY=0, VM_RUN, VM_WAIT, VM_STOP } vm_status;
+typedef enum { VM_READY=0, VM_RUN, VM_WAIT, VM_STOP } vm_state;
 class VM {
 public:
-    vm_status status = VM_READY;       ///< VM status
+    int       vid    = 0;              ///< VM id
+    vm_state  state  = VM_READY;       ///< VM state
     DU        top    = DU0;            ///< cached top of stack
     Vector<DU, 0> ss;                  ///< parameter stack (setup in ten4.cu)
 
-    __GPU__ VM(int khz, Istream *istr, Ostream *ostr, MMU *mmu);
+    __GPU__ VM(int id, Istream *istr, Ostream *ostr, MMU *mmu);
 
-    __GPU__ void virtual init() { status = VM_RUN; }
-    __GPU__ void virtual outer();
+    __GPU__ virtual void init() { VLOG1("VM::init ok\n"); }
+    __GPU__ virtual void outer();
 
 protected:
     Istream  &fin;                     ///< VM stream input
     Ostream  &fout;                    ///< VM stream output
     MMU      &mmu;                     ///< memory managing unit
 
-    int   khz;                         ///< VM clock rate
     U32   *ptop   = (U32*)&top;        ///< 32-bit mask for top
     bool  compile = false;             ///< compiling flag
 
@@ -44,8 +44,11 @@ protected:
     ///
     /// inner interpreter handlers
     ///
-    __GPU__ int  virtual parse(char *str)  { return 0; }
-    __GPU__ int  virtual number(char *str) { return 0; }
+    __GPU__ virtual int resume()          { return 0; }
+    __GPU__ virtual int pre(char *str)    { return 0; }
+    __GPU__ virtual int parse(char *str)  { return 0; }
+    __GPU__ virtual int number(char *str) { return 0; }
+    __GPU__ virtual int post()            { return 0; }
     ///
     /// input stream handler
     ///
@@ -56,9 +59,9 @@ protected:
     ///
     __GPU__ void dot(DU v)          { fout << " " << v; }
     __GPU__ void dot_r(int n, DU v) { fout << setw(n) << v; }
-    __GPU__ void ss_dump(int n)     {
+    __GPU__ void ss_dump(int n=0)   {
         ss[T4_SS_SZ-1] = top;        /// * put top at the tail of ss (for host display)
-        fout << opx(OP_SS, n);
+        fout << opx(OP_SS, n ? n : ss.idx);
     }
 };
 #endif // TEN4_SRC_VM_H
