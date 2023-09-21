f4a4d5a0719c46a3b34a57f73224ec6c48025214
gnii <chochain@yahoo.com>
2023-09-21 08:22:46 -0700

    ~/src/vm.cu outer() handles WAIT/RESUME

--- src/vm.cu
+++ src/vm.cu
@@ -1,16 +1,16 @@
 /** -*- c++ -*-
- * @File
- * @brief - eForth Vritual Machine implementation
+ * @file
+ * @brief VM class - eForth Vritual Machine implementation
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
 #include "vm.h"
 
 __GPU__
-VM::VM(int khz, Istream *istr, Ostream *ostr, MMU *mmu0)
-    : khz(khz), fin(*istr), fout(*ostr), mmu(*mmu0) {
-    VLOG1("\\  VM[%d](mem=%p, vmss=%p)\n",
-          blockIdx.x, mmu.pmem(0), mmu.vmss(blockIdx.x));
+VM::VM(int id, Istream *istr, Ostream *ostr, MMU *mmu0)
+    : vid(id), fin(*istr), fout(*ostr), mmu(*mmu0) {
+    ss.init(mmu.vmss(vid), T4_SS_SZ);  /// * point data stack to managed memory block
+    VLOG1("\\  VM[%d](mem=%p, vmss=%p)\n", vid, mmu.pmem(0), ss.v);
 }
 ///
 /// ForthVM Outer interpreter
@@ -26,14 +26,20 @@ VM::VM(int khz, Istream *istr, Ostream *ostr, MMU *mmu0)
 ///
 __GPU__ void
 VM::outer() {
-    VLOG1("%c< %s\n", compile ? ':' : '<', fin.rdbuf()); /// * display input buffer
-    while (fin >> idiom) {                   /// * loop throught tib
-        VLOG2("%d>> %-10s => ", blockIdx.x, idiom);
+    VLOG1("%d%c %s\n", vid, compile ? ':' : '{', fin.rdbuf()); /// * display input buffer
+    if (state == VM_RUN) resume();                 /// * resume from suspended VM
+    while (state == VM_READY && fin >> idiom) {    /// * loop throught tib
+        if (pre(idiom)) continue;                  /// * pre process
+        VLOG2("%d| >> %-10s => ", vid, idiom);
         if (!parse(idiom) && !number(idiom)) {
-            fout << idiom << "? " << ENDL;   /// * display error prompt
-            compile = false;                 /// * reset to interpreter mode
+            fout << idiom << "? " << ENDL;         /// * display error prompt
+            compile = false;                       /// * reset to interpreter mode
         }
+        if (post()) break;                         /// * post process
     }
-    if (!compile) ss_dump(ss.idx);
+    switch (state) {
+    case VM_WAIT: VLOG1("%d} VM[%d] wait\n", vid, vid); break;
+    case VM_READY: if (!compile) ss_dump();             break;
+    }
 }
 //=======================================================================================
