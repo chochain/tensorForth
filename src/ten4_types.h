801b77b950c18811fcb16b0f8a0e2cabd067a537
gnii <chochain@yahoo.com>
2023-09-21 14:57:01 -0700

    ~/src/ten4_types.h math function macros => mmu/util.h

--- src/ten4_types.h
+++ src/ten4_types.h
@@ -107,28 +107,8 @@ typedef F64         DU2;                    /**< double preciesion data */
 #define DU0         ((DU)0.0)               /**< default data value 0   */
 #define DU1         ((DU)1.0)               /**< default data value 1   */
 #define DU_EPS      ((DU)1.0e-6)            /**< floating point epsilon */
-///
-/// cross platform floating-point ALU support (see nvcc -use_fast_math flag)
-///
 #define ZERO(d)     (ABS(d) < DU_EPS)       /**< zero check             */
 #define BOOL(d)     (ZERO(d) ? DU0 : -DU1)  /**< default boolean        */
-#define ABS(d)      ((DU)fabsf(d))          /**< absolute value         */
-#define EXP(d)      ((DU)expf(d))           /**< exponential(float)     */
-#define LOG(d)      ((DU)log10f(d))         /**< log10                  */
-#define LN(d)       ((DU)logf(d))           /**< natural logrithm       */
-#define POW(d,e)    ((DU)powf(d,e))         /**< power d^(e)            */
-#define SQRT(d)     ((DU)sqrtf(d))          /**< square root            */
-#define RCP(x)      ((DU)(DU1/(x)))         /**< reciprocol 1/x         */
-#define ADD(x,y)    ((DU)(x)+(y))           /**< addition               */
-#define SUB(x,y)    ((DU)(x)-(y))           /**< addition               */
-#define MUL(x,y)    ((DU)(x)*(y))           /**< multiplication         */
-#define DIV(x,y)    ((DU)(x)/(y))           /**< division               */
-#define TANH(d)     ((DU)tanhf(d))          /**< tanh(float)            */
-#define SIGMOID(d)  (RCP(DU1+EXP(-(d))))    /**< sigmoid(float)         */
-#define MOD(t,n)    ((DU)fmodf(t, n))       /**< fmod two floats        */
-#define MAX(x,y)    ((DU)fmaxf(x,y))        /**< maximum of the two     */
-#define MIN(x,y)    ((DU)fminf(x,y))        /**< minimum of the two     */
-#define NORM(n,p)   ((DU)normf(n,p))        /**< normal of n floats     */
 ///
 /// data conversion macros
 ///
