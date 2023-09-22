a4cb8eb68d1c19011a85f137f5c66ccef4a10f49
gnii <chochain@yahoo.com>
2023-09-22 13:23:42 -0700

    ~/src/ten4_config.h ENABLE_NN=1

--- src/ten4_config.h
+++ src/ten4_config.h
@@ -25,7 +25,7 @@
 ///@name CUDA cooperative dynamic parallelism support
 ///@{
 #define T4_ENABLE_OBJ       1        /**< enable tensor/matrix  */
-#define T4_ENABLE_NN        0        /**< enable neural network */
+#define T4_ENABLE_NN        1        /**< enable neural network */
 #define T4_ENABLE_CDP       0
 #define T4_USE_STRBUF       0
 #define T4_PER_THREAD_STACK 8*1024   /**< init() stack overflow */
