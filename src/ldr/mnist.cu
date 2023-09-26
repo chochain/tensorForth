ac08895a23323b69bbb6c6639eeac66b369479fa
gnii <chochain@yahoo.com>
2023-09-26 13:47:38 -0700

    ~/src/ldr/mnist.cu refactor _setup, fetch => init, fetch (simpify init logic)

--- src/ldr/mnist.cu
+++ src/ldr/mnist.cu
@@ -8,30 +8,66 @@
 
 #if (T4_ENABLE_OBJ && T4_ENABLE_NN)
 
-#define LOG_COUNT 1000
+#define LOG_COUNT 1  //000
 #define MAX_BATCH 0
 
-Corpus *Mnist::fetch(int batch_sz, int batch_id) {
+Corpus *Mnist::init() {
+    auto _u32 = [this](std::ifstream &fs) {
+        U32 v = 0;
+        char x;
+        for (int i = 0; i < 4; i++) {
+            fs.read(&x, 1);
+            v <<= 8;
+            v += (U32)*(U8*)&x;
+        }
+        return v;
+    };
+    if (_open()) return NULL;
+
+    U32 X0, X1, N1=0;
+    if (t_in) {
+        X1 = _u32(t_in);    ///< label magic number 0x0801
+        N1 = _u32(t_in);
+        DS_LOG1("\n\tMNIST label: magic=%08x => [%d]", X1, N1);
+    }
+    if (d_in) {
+        X0 = _u32(d_in);    ///< image magic number 0x0803
+        N  = _u32(d_in);
+        H  = _u32(d_in);
+        W  = _u32(d_in);
+        C  = 1;
+        DS_LOG1("\n\tMNIST image: magic=%08x => [%d][%d,%d,%d]",
+               X0, N, H, W, C);
+    }
+    if (N != N1) {
+        DS_ERROR("ERROR: Mnist label count %d != image count %d\n", N1, N);
+        return NULL;
+    }
+    return this;
+}
+
+Corpus *Mnist::fetch(int batch_id, int batch_sz) {
     static int tick = 0;
 
-    if (N == 0 && _setup()) return NULL;           /// * setup once only
     eof = 0;
-
-    int bsz = batch_sz ? batch_sz : N;             ///< full batch if batch_sz==0
-
-    if (bsz * batch_id >= N) { eof = 1; return this; }
-
-    int b0  = _get_labels(bsz, batch_id);          ///< load batch labels
-    int b1  = _get_images(bsz, batch_id);          ///< load batch images
+    int bsz = batch_sz ? batch_sz : N;       ///< batch_sz==0 => entire batch
+    if (bsz==0 || (bsz * batch_id) >= N) {   ///< beyond total sample count
+        eof=1; return this;
+    }  
+    ///
+    /// fetch labels and images (and set eof if any of EOF reached)
+    ///
+    int b0  = _get_labels(batch_id, bsz);    ///< load batch labels
+    int b1  = _get_images(batch_id, bsz);    ///< load batch images
     if (b0 != b1) {
         DS_ERROR("ERROR: Mnist::fetch #label=%d != #image=%d\n", b0, b1);
         return NULL;
     }
     if ((tick++ % LOG_COUNT) == 0) {
         DS_LOG1("\n\tMnist batch[%d] loaded (size=%d)\n", batch_id, b0);
-        _preview(bsz < 3 ? bsz : 3);               /// * debug print
+        _preview(bsz < 3 ? bsz : 3);          /// * debug print
     }
-    if (MAX_BATCH && (tick * batch_sz > MAX_BATCH)) eof = 1;
+    if (MAX_BATCH && (tick * bsz > MAX_BATCH)) eof |= 1; /// forced stop (debug)
 
     return this;
 }
@@ -51,41 +87,6 @@ int Mnist::_open() {
 int Mnist::_close() {
     if (d_in.is_open()) d_in.close();
     if (t_in.is_open()) t_in.close();
-    return 0;
-}
-
-int Mnist::_setup() {
-    auto _u32 = [this](std::ifstream &fs) {
-        U32 v = 0;
-        char x;
-        for (int i = 0; i < 4; i++) {
-            fs.read(&x, 1);
-            v <<= 8;
-            v += (U32)*(U8*)&x;
-        }
-        return v;
-    };
-    if (_open()) return -1;
-
-    U32 X0, X1, N1=0;
-    if (t_in) {
-        X1 = _u32(t_in);    ///< label magic number 0x0801
-        N1 = _u32(t_in);
-        DS_LOG1("\n\tMNIST label: magic=%08x => [%d]", X1, N1);
-    }
-    if (d_in) {
-        X0 = _u32(d_in);    ///< image magic number 0x0803
-        N  = _u32(d_in);
-        H  = _u32(d_in);
-        W  = _u32(d_in);
-        C  = 1;
-        DS_LOG1("\n\tMNIST image: magic=%08x => [%d][%d,%d,%d]",
-               X0, N, H, W, C);
-    }
-    if (N != N1) {
-        DS_ERROR("ERROR: Mnist label count %d != image count %d\n", N1, N);
-        return -2;
-    }
     return 0;
 }
 
@@ -114,21 +115,23 @@ int Mnist::_preview(int N) {
     return 0;
 }
 
-int Mnist::_get_labels(int bsz, int bid) {
+int Mnist::_get_labels(int bid, int bsz) {
     int hdr = sizeof(U32) * 2;                     ///< header to skip over
 
     if (!label) DS_ALLOC(&label, bsz);
 
     t_in.seekg(hdr + bid * bsz);                   /// * seek by batch
     t_in.read((char*)label, bsz);                  /// * fetch batch labels
-    eof |= t_in.eof();                             /// * set EOF flag
 
-    int cnt = eof ? d_in.gcount() : bsz;
+    int cnt = t_in.gcount();
+
+    char c = t_in.peek();                          ///< check EOF
+    eof |= t_in.eof();                             /// * set EOF flag
 
     return cnt;
 }
 
-int Mnist::_get_images(int bsz, int bid) {
+int Mnist::_get_images(int bid, int bsz) {
     int hdr = sizeof(U32) * 4;                     ///< header to skip over
     int xsz = bsz * dsize();                       ///< image block size
 
@@ -136,9 +139,11 @@ int Mnist::_get_images(int bsz, int bid) {
 
     d_in.seekg(hdr + bid * xsz);                   /// * seek by batch id
     d_in.read((char*)data, xsz);                   /// * fetch batch images
-    eof |= d_in.eof();                             /// * set EOF flag
 
-    int cnt = eof ? d_in.gcount() / dsize() : bsz;
+    int cnt = d_in.gcount() / dsize();
+    
+    char c = t_in.peek();                          ///< check EOF
+    eof |= d_in.eof();                             /// * set EOF flag
 
     return cnt;
 }
