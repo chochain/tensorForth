4760e3ae646a4eb727e402ecca30d0c952c053db
gnii <chochain@yahoo.com>
2025-01-27 20:53:54 +0800

    ~/src/sys.cu focus on event loop

--- src/sys.cu
+++ src/sys.cu
@@ -4,7 +4,6 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
-#include <iomanip>                     /// setw, setbase
 #include "sys.h"
 ///
 /// random number generator setup
@@ -46,168 +45,78 @@ System::~System() {
     MM_FREE(_seed);
 }
 
-__GPU__ DU
-System::rand(DU d, t4_rand_opt ntype) {
-    if (!IS_OBJ(d)) return d * curand_uniform(&_seed[0]);
-    random((Tensor&)du2obj(d), ntype);
-    return d;
-}
-///
-/// Object debugging methods
-///
 __HOST__ int
-System::to_s(std::ostream &fout, T4Base &t, bool view) {
-    static const char tn[2][4] = {                   ///< sync with t4_obj
-        { 'T', 'N', 'D', 'X' }, { 't', 'n', 'd', 'x' }
-    };
-    auto t2 = [&fout](Tensor &t) { fout << t.H() << ',' << t.W() << ']'; };
-    auto t4 = [&fout](Tensor &t) {
-        fout << t.N() << ',' << t.H() << ',' << t.W() << ',' << t.C() << ']';
-    };
-    fout << tn[view][t.ttype];
-    switch(t.rank) {
-    case 0: fout << "["  << (t.numel - 1) << "]"; break; // network model
-    case 1: fout << "1[" << t.numel << "]";       break;
-    case 2: fout << "2["; t2((Tensor&)t);         break;
-    case 4: fout << "4["; t4((Tensor&)t);         break;
-    case 5: fout << "5[" << t.parm << "]["; t4((Tensor&)t); break;
-    }
-    return 1;
+System::readline() {
+    io->d_istr->clear();
+    char *tib = io->d_istr->rdbuf();
+    fin.getline(tib, T4_IBUF_SZ, '\n');
+    return strlen(tib);
 }
 
-__HOST__ __INLINE__ int
-System::to_s(std::ostream &fout, DU s) {
-    return to_s(fout, du2obj(s), IS_VIEW(s));
+#define NEXT_EVENT(n) ((io_event*)((char*)&ev->data[0] + ev->sz))
+
+__HOST__ io_event*
+System::process_event(io_event *ev) {
+    cudaDeviceSynchronize();        /// * make sure data is completely written
+    
+    char *v = (char*)ev->data;      ///< fetch payload in buffered print node
+    switch (ev->gt) {
+    case GT_INT:   fout << (*(S32*)v);                 break;
+    case GT_U32:   fout << static_cast<U32>(*(U32*)v); break;
+    case GT_FLOAT: fout << (*(F32*)v);                 break;
+    case GT_STR:   fout << v;                          break;
+    case GT_FMT:   {
+        obuf_fmt *f = (obuf_fmt*)v;
+        //printf("FMT: b=%d, w=%d, p=%d, f='%c'\n", f->base, f->width, f->prec, f->fill);
+        fout << std::setbase(_radix = f->base)
+             << std::setw(f->width)
+             << std::setprecision(f->prec ? f->prec : -1)
+             << std::setfill((char)f->fill);
+    } break;
+    case GT_OBJ: io->_print_obj(fout, *(DU*)v); break;
+    case GT_OPX: {
+        _opx *o = (_opx*)v;
+        // printf("OP=%d a=%d, n=0x%08x=%f\n", o->op, o->a, DU2X(o->n), o->n);
+        switch (o->op) {
+        case OP_WORDS: dbg->words();                             break;
+        case OP_SEE:   dbg->see((IU)o->a);                       break;
+        case OP_DUMP:  dbg->mem_dump((IU)o->a, (IU)o->n);        break;
+        case OP_SS:    dbg->ss_dump((IU)ev->id, o->a, _radix); break;
+#if T4_ENABLE_OBJ // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        case OP_TSAVE:
+            ev = NEXT_EVENT(ev);
+            io->_tsave(o->n, o->a, (char*)ev->data);
+            break;
+#if T4_ENABLE_NN  //==========================================================
+        case OP_DATA:
+            ev = NEXT_EVENT(ev);                         ///< get dataset repo name
+            io->_dsfetch(o->n, o->a, (char*)ev->data);   /// * fetch first batch
+            break;
+        case OP_FETCH: io->_dsfetch(o->n, o->a); break;  /// * fetch/rewind dataset batch
+        case OP_NSAVE:
+            ev = NEXT_EVENT(ev);                         ///< get dataset repo name
+            io->_nsave(o->n, o->a, (char*)ev->data);
+            break;
+        case OP_NLOAD:
+            ev = NEXT_EVENT(ev);
+            io->_nload(o->n, o->a, (char*)ev->data);
+            break;
+#endif // T4_ENABLE_NN =======================================================
+#endif // T4_ENABLE_OBJ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        }
+    } break;
+    default: fout << "print type not supported: " << (int)ev->gt; break;
+    }
+    return NEXT_EVENT(ev);
 }
 
-#endif // T4_ENABLE_OBJ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-///
-/// Debugging methods
-///
-/// display dictionary word (wastefully one byte at a time)
-///
-__HOST__ int
-System::to_s(std::ostream &fout, IU w) {
-    /*
-     * TODO: not sure why copying 32 byte does not work?
-     * char name[36];
-     * cudaMemcpy(name, _dict[w].name, 32, D2H);
-     */
-    Code &code = _dict[w];
-    if (_trace) {
-        fout << (code.immd ? "*" : " ")
-             << "[" << std::setw(3) << w << "]"
-             << (code.colon ? (FPTR)&_pmem[code.nfa] : code.xt)
-             << (code.colon ? ':': '=');
-    }
-    U8 c, i=0;
-    cudaMemcpy(&c, code.name, 1, D2H);
-    fout << " ";
-    while (c) {
-        fout << c;
-        cudaMemcpy(&c, code.name+(++i), 1, D2H);
-    }
-    return (int)i;
-}
-///
-/// display dictionary word list
-///
 __HOST__ void
-System::words(std::ostream &fout) {
-    fout << std::setbase(10);
-    for (int i=0, sz=0; i<_didx; i++) {
-        fout << ' ';
-        sz += to_s(fout, (IU)i) + 1;
-        if (_trace || sz > 68) { fout << std::endl; sz = 0; } /// TODO: width configuable
+System::flush(std::ostream &fout) {
+    io_event *e = (io_event*)io->d_ostr->rdbuf();
+    while (e->gt != GT_EMPTY) {          // 0
+        e = process_event(fout, e);
     }
-    if (!_trace) fout << std::endl;
+    io->d_ostr->clear();
 }
-///
-/// recursively disassemble colon word
-///
-__HOST__ void
-System::see(std::ostream &fout, U8 *ip, int dp) {
-    while (*(IU*)ip) {                                              /// * loop until EXIT
-        fout << std::endl; for (int n=dp; n>0; n--) fout << "  ";   /// * indentation by level
-        fout << "[" << std::setw(4) << (IU)(ip - _pmem) << ":";
-        IU w = *(IU*)ip;                                            /// * fetch word index
-        to_s(fout, w);                                              /// * display word name
-        if (_dict[w].colon && dp < 2) {                             /// * check if is a colon word
-            see(fout, &_pmem[_dict[w].pfa], dp+1);                  /// * go one level deeper
-        }
-        ip += sizeof(IU);                                           /// * advance instruction pointer
-        switch (w) {
-        case DOVAR: case DOLIT: {                                   /// * fetch literal
-            DU v = *(DU*)ip;  ip += sizeof(DU);
-            fout << "= ";
-            if (IS_OBJ(v)) to_s(fout, v);                           /// * handle object
-            else fout << v;                                         /// * display the literal
-        } break;
-        case DOSTR: case DOTSTR: {
-            char *s = (char*)ip;
-            int  sz = strlen(s)+1;
-            ip += ALIGN2(sz);                                       /// fetch string
-            fout << "= \"" << s << "\"";
-        } break;
-        case BRAN: case ZBRAN: case DONEXT:
-            fout << "= " << *(IU*)ip; ip += sizeof(IU); break;      /// fetch jump target
-        }
-        fout << " ] ";
-    }
-}
-__HOST__ void
-System::see(std::ostream &fout, U16 w) {
-    fout << "["; to_s(fout, w);
-    if (_dict[w].colon) see(fout, &_pmem[_dict[w].pfa]);
-    fout << "]" << std::endl;
-}
-///
-/// dump data stack content
-///
-__HOST__ void
-System::ss_dump(std::ostream &fout, U16 vid, U16 n, int radix) {
-    bool rx = radix != 10;
-    auto show = [this, &fout, rx](DU s) {
-        if (IS_OBJ(s)) to_s(fout, s);
-        else if (rx)   fout << static_cast<int>(s);
-        else           fout << s;
-    };
-    DU *ss = &_vmss[vid * T4_SS_SZ];
-    if (_trace) fout << vid << "}";
-    fout << std::setprecision(-1) << " <";
-    if (rx) fout << std::setbase(radix);
-    for (U16 i=0; i<n; i++) {
-        show(ss[i]);                 /// * show stack elements
-        fout << " ";
-    }
-    show(ss[T4_SS_SZ-1]);            /// * show top
-    fout << "> ok" << std::endl;
-}
-///
-/// Forth pmem memory dump
-/// TODO: dynamic parallel
-///
-#define C2H(c) { buf[x++] = i2h[(c)>>4]; buf[x++] = i2h[(c)&0xf]; }
-#define IU2H(i){ C2H((i)>>8); C2H((i)&0xff); }
-__HOST__ void
-System::mem_dump(std::ostream &fout, U16 p0, U16 sz) {
-    static const char *i2h = "0123456789abcdef";
-    char buf[80];
-    for (U16 i=ALIGN16(p0); i<=ALIGN16(p0+sz); i+=16) {
-        int x = 0;
-        buf[x++] = '\n'; IU2H(i); buf[x++] = ':'; buf[x++] = ' ';  // "%04x: "
-        for (U16 j=0; j<16; j++) {
-            //U8 c = *(((U8*)&_dict[0])+i+j) & 0x7f;               // to dump _dict
-            U8 c = _pmem[i+j];
-            C2H(c);                                                // "%02x "
-            c &= 0x7f;                                             // mask off high bit
-            buf[x++] = ' ';
-            if (j%4==3) buf[x++] = ' ';
-            buf[59+j]= (c==0x7f||c<0x20) ? '.' : c;                // %c
-        }
-        buf[75] = '\0';
-        fout << buf;
-    }
-    fout << std::endl;
-}
+
 
