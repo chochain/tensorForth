0ddd359955e36b5e33d5ca441802b5ae1bebf8a9
gnii <chochain@yahoo.com>
2025-01-27 20:53:10 +0800

    ~/src/sys.h add io code

--- src/sys.h
+++ src/sys.h
@@ -7,75 +7,103 @@
 #ifndef TEN4_SRC_SYS_H
 #define TEN4_SRC_SYS_H
 #include <curand_kernel.h>
-#include "ten4_types.h"
-#include "mmu/mmu.h"
-#include "aio/aio.h"
 #include "ldr/loader.h"
-
-typedef enum { RDX=0, CR, DOT, UDOT, EMIT, SPCS } io_op;
+#include "debug.h"                              ///< include mmu/mmu.h, io/aio.h
 
 class System : public Managed {                 ///< singleton class
-    AIO            *aio;                        ///< async IO manager
-    MMU            *mmu;                        ///< memory management unit
-    
+private:    
     int            _khz;                        ///< GPU clock speed
-    int            _trace;                      ///< debug tracing verbosity level
     curandState    *_seed;                      ///< for random number generator
+    Istream        *_istr;                      ///< managed input stream
+    Ostream        *_ostr;                      ///< managed output stream
+    char           _pad[T4_STRBUF_SZ];          ///< terminal input buffer
     
 public:
-    __HOST__ System(int khz, int verbose=0)
-        : _khz(khz), _trace(verbose) {
-        mmu = new MMU();                        ///> instantiate memory manager
-        aio = new AIO(mmu);                     ///> instantiate async IO manager
+    MMU            *mu;                         ///< memory management unit
+    AIO            *io;                         ///< HOST IO manager
+    Debug          *db;
+    
+    __HOST__ System(int khz, int verbo=0)
+        : _khz(khz), _istr(new Istream()), _ostr(new Ostream()) {
+        mu = new MMU();                         ///> instantiate memory manager
+        io = new AIO(verbo);                    ///> instantiate async IO manager
+        db = new Debug(mu, io);
         
 #if (T4_ENABLE_OBJ && T4_ENABLE_NN)
-        Loader::init(verbose);
+        Loader::init(verbo);
 #endif
     }
     __HOST__ ~System() {
-        delete aio;
+        delete db;
+        delete io;
         cudaDeviceReset();
     }
+    __HOST__ void      fin_setup(const char *line);
+    __HOST__ void      fout_setup(void (*hook)(int, const char*));
     ///
     /// System functions
     ///
-    __GPU__  __INLINE__ DU   ms()           { return static_cast<double>(clock64()) / _khz; }
-    __GPU__  DU   rand(DU d, t4_rand_opt n);              ///< randomize a tensor
+    __HOST__ int       readline();
+    __HOST__ io_event  *process_event(io_event *ev);
+    __HOST__ void      flush();
     ///
-    /// debugging methods (implemented in .cu)
+    /// debuging controls
     ///
     __BOTH__ __INLINE__ int  khz()          { return _khz;   }
-    __BOTH__ __INLINE__ int  trace()        { return _trace; }
-    __BOTH__ __INLINE__ void trace(int lvl) { _trace = lvl;  }
+    __BOTH__ __INLINE__ int  trace()        { return io->trace; }
+    __BOTH__ __INLINE__ void trace(int lvl) { io->trace = lvl;  }
     
-    __HOST__ void fin_setup(const char *line);
-    __HOST__ void fout_setup(void (*hook)(int, const char*));
+    ///==============================================================================
+    /// Device methods
     ///
-    ///> IO functions
+    ///> System functions
     ///
-    __GPU__  char *scan(char c);                          ///< scan input stream for a given char
+    __GPU__  DU   ms() { return static_cast<double>(clock64()) / _khz; }
+    __GPU__  DU   rand(DU d, rand_opt n) {                ///< randomize a tensor
+#if T4_ENABLE_OBJ    
+        if (IS_OBJ(d)) { random((Tensor&)du2obj(d), ntype); return d; }
+#endif // T4_ENABLE_OBJ    
+        return d * curand_uniform(&_seed[0]);
+    }
+    ///
+    /// input stream handler
+    ///
+    __GPU__  char key() {                                 ///< read key from console
+        char c; *_istr >> c; return c;
+    }
+    __GPU__  char *scan(char delim)  {
+        _istr->get_idiom(_pad, delim); return _pad;       ///< scan input stream for a given char
+    }
+    __GPU__  char *next_idiom()      {                    ///< fetch next idiom
+        *_istr >> _pad; return _pad;
+    }
     __GPU__  int  fetch(string &idiom);                   ///< read input stream into string
-    __GPU__  char *word();                                ///< get next idiom
-    
-    __GPU__  char key();                                  ///< read key from console
-    __GPU__  void load(VM &vm, const char* fn);           ///< load external Forth script
-    __GPU__  void spaces(int n);                          ///< show spaces
-    __GPU__  void dot(io_op op, DU v=DU0);                ///< print literals
-    __GPU__  void dotr(int w, DU v, int b, bool u=false); ///< print fixed width literals
-    __GPU__  void pstr(const char *str, io_op op=SPCS);   ///< print string
+//    __GPU__  void load(VM &vm, const char* fn);           ///< load external Forth script
     ///
-    ///> Debug functions
+    /// output methods
     ///
-#if T4_ENABLE_OBJ // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    __HOST__ int  to_s(std::ostream &fout, DU s);         ///< dump object from descriptor
-    __HOST__ int  to_s(std::ostream &fout, T4Base &t, bool view); ///< dump object on stack
-#endif    
-    __HOST__ void ss_dump(VM &vm, bool forced=false);     ///< show data stack content
-    __HOST__ void see(IU pfa, int base);                  ///< disassemble user defined word
-    __HOST__ void words(int base);                        ///< list dictionary words
-    __HOST__ void dict_dump(int base);                    ///< dump dictionary
-    __HOST__ void mem_dump(U32 addr, IU sz, int base);    ///< dump memory frm addr...addr+sz
-    __HOST__ void mem_stat();                             ///< display memory statistics
+    __GPU__  void spaces(int n) {                         ///< show spaces
+        for (int i = 0; i < n; i++) *_ostr << " ";
+    }
+    __GPU__  void dot(io_op op, DU v) {                   ///< print literals
+        switch (op) {
+        case RDX:   *_ostr << setbase(UINT(v));               break;
+        case CR:    *_ostr << ENDL;                           break;
+        case DOT:   *_ostr << v << " ";                       break;
+        case UDOT:  *_ostr << static_cast<U32>(v) << " ";     break;
+        case EMIT:  { char b = (char)UINT(v); *_ostr << b; }  break;
+        case SPCS:  spaces(UINT(v));                          break;
+        default:    *_ostr << "unknown io_op=" << op << ENDL; break;
+        }
+    }
+    __GPU__  void pstr(const char *str, io_op op=SPCS) {  ///< print string
+        *_ostr << str;
+        if (op==CR) { *_ostr << ENDL; }
+    }
+    __GPU__  void ss_dump(DU *ss, int n=0)   {
+        ss[T4_SS_SZ-1] = tos;        /// * put TOS at the tail of ss (for host display)
+        *_ostr << opx(OP_SS, n ? n : ss.idx);
+    }
 };
 #endif // TEN4_SRC_SYS_H
 
