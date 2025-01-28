9db9bafc07fe0f6bc4ef52fa88d0a855b1cd9e05
gnii <chochain@yahoo.com>
2025-01-28 22:32:11 +0800

    ~/src/sys.cu pass h_io to AIO; dispatch d_io

--- src/sys.cu
+++ src/sys.cu
@@ -4,6 +4,7 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
+#include <iomanip>        /// setw, setprec, setbase...
 #include "sys.h"
 ///
 /// random number generator setup
@@ -16,7 +17,7 @@ k_rand_init(curandState *st, U64 seed) {
 }
 
 __KERN__ void
-k_rand(DU *mat, int sz, DU bias, DU scale, curandState *st, t4_rand_opt ntype) {
+k_rand(DU *mat, int sz, DU bias, DU scale, curandState *st, rand_opt ntype) {
     int tx = threadIdx.x;             ///< thread idx
     int n  = (sz / blockDim.x) + 1;   ///< loop counter
     
@@ -34,22 +35,35 @@ k_rand(DU *mat, int sz, DU bias, DU scale, curandState *st, t4_rand_opt ntype) {
 /// Forth Virtual Machine operational macros to reduce verbosity
 ///
 __HOST__
-System::System() {
+System::System(h_istr &i, h_ostr &o, int khz, int verbo)
+    : _khz(khz), _istr(new Istream()), _ostr(new Ostream()) {
+    mu = new MMU();                  ///> instantiate memory manager
+    io = new AIO(i, o, verbo);       ///> instantiate async IO manager
+    db = new Debug(mu, io);
+        
+#if (T4_ENABLE_OBJ && T4_ENABLE_NN)
+    Loader::init(verbo);
+#endif
+    ///
+    ///> setup randomizer
+    ///
     MM_ALLOC(&_seed, sizeof(curandState) * T4_RAND_SZ);
-    
     k_rand_init<<<1, T4_RAND_SZ>>>(_seed, time(NULL));  /// serialized randomizer
     GPU_CHK();
 }
 
 System::~System() {
+    delete db;
+    delete io;
     MM_FREE(_seed);
+    cudaDeviceReset();
 }
 
 __HOST__ int
 System::readline() {
-    io->d_istr->clear();
-    char *tib = io->d_istr->rdbuf();
-    fin.getline(tib, T4_IBUF_SZ, '\n');
+    _istr->clear();
+    char *tib = _istr->rdbuf();
+    io->fin.getline(tib, T4_IBUF_SZ, '\n');
     return strlen(tib);
 }
 
@@ -58,8 +72,9 @@ System::readline() {
 __HOST__ io_event*
 System::process_event(io_event *ev) {
     cudaDeviceSynchronize();        /// * make sure data is completely written
-    
-    char *v = (char*)ev->data;      ///< fetch payload in buffered print node
+
+    char   *v    = (char*)ev->data; ///< fetch payload in buffered print node
+    h_ostr &fout = io->fout;        ///< host output stream
     switch (ev->gt) {
     case GT_INT:   fout << (*(S32*)v);                 break;
     case GT_U32:   fout << static_cast<U32>(*(U32*)v); break;
@@ -68,20 +83,20 @@ System::process_event(io_event *ev) {
     case GT_FMT:   {
         obuf_fmt *f = (obuf_fmt*)v;
         //printf("FMT: b=%d, w=%d, p=%d, f='%c'\n", f->base, f->width, f->prec, f->fill);
-        fout << std::setbase(_radix = f->base)
+        fout << std::setbase(f->base)
              << std::setw(f->width)
              << std::setprecision(f->prec ? f->prec : -1)
              << std::setfill((char)f->fill);
     } break;
-    case GT_OBJ: io->_print_obj(fout, *(DU*)v); break;
+    case GT_OBJ: io->print(*(DU*)v); break;
     case GT_OPX: {
         _opx *o = (_opx*)v;
-        // printf("OP=%d a=%d, n=0x%08x=%f\n", o->op, o->a, DU2X(o->n), o->n);
+        printf("OP=%d a=%d, n=0x%08x=%f\n", o->op, o->a, DU2X(o->n), o->n);
         switch (o->op) {
-        case OP_WORDS: dbg->words();                             break;
-        case OP_SEE:   dbg->see((IU)o->a);                       break;
-        case OP_DUMP:  dbg->mem_dump((IU)o->a, (IU)o->n);        break;
-        case OP_SS:    dbg->ss_dump((IU)ev->id, o->a, _radix); break;
+        case OP_WORDS: db->words();                      break;
+        case OP_SEE:   db->see((IU)o->a);                break;
+        case OP_DUMP:  db->mem_dump((IU)o->a, (IU)o->n); break;
+        case OP_SS:    db->ss_dump((IU)ev->id, o->a);    break;
 #if T4_ENABLE_OBJ // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
         case OP_TSAVE:
             ev = NEXT_EVENT(ev);
@@ -111,12 +126,16 @@ System::process_event(io_event *ev) {
 }
 
 __HOST__ void
-System::flush(std::ostream &fout) {
-    io_event *e = (io_event*)io->d_ostr->rdbuf();
+System::flush() {
+    io_event *e = (io_event*)_ostr->rdbuf();
     while (e->gt != GT_EMPTY) {          // 0
-        e = process_event(fout, e);
+        e = process_event(e);
     }
-    io->d_ostr->clear();
+    _ostr->clear();
+    ///
+    /// clean up marked free tensors
+    ///
+    mu->sweep();
 }
 
 
