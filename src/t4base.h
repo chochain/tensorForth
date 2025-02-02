141117a56d194ee0e35096927dfdb185e65e2731
gnii <chochain@yahoo.com>
2025-02-02 10:20:02 +0800

    ~/src mv mmu/t4base.h to root-level

--- src/t4base.h
+++ src/t4base.h
@@ -36,6 +36,18 @@ struct T4Base : public Managed {
     };
     DU  *data;    ///< managed memory block pointer (Note: instead of from TLSF)
     ///
+    /// static short hands for eforth tensor ucodes (for DU <-> Tensor conversion)
+    ///
+    static __BOTH__ T4Base &du2obj(DU d) {                         ///< DU to Obj convertion
+        U32    off = DU2X(d) & ~T4_TYPE_MSK;
+        T4Base *t  = (T4Base*)(_obj + off);
+        return *t;
+    }
+    static __BOTH__ DU     obj2du(T4Base &t) {                     ///< conver Obj to DU
+        U32 o = ((U32)((U8*)&t - _obj)) | T4_TT_OBJ;
+        return *(DU*)&o;
+    }
+    ///
     /// class contructors
     ///
     __HOST__ T4Base() :
