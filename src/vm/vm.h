3bd3fa0791325cd0e87368244256f43b9244f922
gnii <chochain@yahoo.com>
2025-02-16 19:52:18 +0800

    ~/src/vm/vm.h mv ss,rs back (Debug needs them); combine parse/number => process

--- src/vm/vm.h
+++ src/vm/vm.h
@@ -4,8 +4,8 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
-#ifndef TEN4_SRC_VM_H
-#define TEN4_SRC_VM_H
+#ifndef TEN4_SRC_VM_VM_H
+#define TEN4_SRC_VM_VM_H
 #include "sys.h"                    /// system interface
 ///
 ///@name Cross platform support
@@ -25,12 +25,13 @@ typedef enum { STOP=0, HOLD, QUERY, NEST } vm_state;   ///< ten4 states
 class VM {                            ///< VM (created in kernal mode)
 public:
     IU        id;                     ///< VM id
-    vm_state  state;                  ///< VM state
+    vm_state  state   = STOP;         ///< VM state
+    
     System    *sys;                   ///< system interface
     MMU       *mmu;                   ///< cached MMU interface
-    
-    Vector<DU, 0> ss;                 ///< parameter stack (setup in ten4.cu)
-    Vector<DU, 0> rs;                 ///< return stack
+
+    Vector<DU, 0> SS;                 ///< parameter stack (setup in ten4.cu)
+    Vector<DU, 0> RS;                 ///< return stack
 
     __GPU__  VM(int id, System *sys);
     __GPU__  ~VM() { VLOG1("%d ", id); }
@@ -53,7 +54,17 @@ public:
     __GPU__ __INLINE__ void  DROP(DU d) {}
 #endif // T4_ENABLE_OBJ
     
-#if DO_MULTITASK
+protected:
+    bool  compile = false;            ///< compiling flag
+    ///
+    /// inner interpreter handlers
+    ///
+    __GPU__ virtual int resume()           { return 0; }
+    __GPU__ virtual int pre(char *str)     { return 0; }
+    __GPU__ virtual int process(char *str) { return 0; }
+    __GPU__ virtual int post()             { return 0; }
+
+#if DO_MULTITASK    
     static MUTEX    tsk;              ///< mutex for tasker
     static COND_VAR cv_tsk;           ///< tasker control
     
@@ -71,17 +82,9 @@ public:
     void recv();                      ///< receive data from any sending VM's stack (blocking, wait for sender's message)
     void bcast(int n);                ///< broadcast to all receivers
     void pull(int tid, int n);        ///< pull n items from the stack of a stopped task
-#endif // DO_MULTITASK
-
-protected:
-    bool  compile = false;            ///< compiling flag
-    ///
-    /// inner interpreter handlers
-    ///
-    __GPU__ virtual int resume()          { return 0; }
-    __GPU__ virtual int pre(char *str)    { return 0; }
-    __GPU__ virtual int parse(char *str)  { return 0; }
-    __GPU__ virtual int number(char *str) { return 0; }
-    __GPU__ virtual int post()            { return 0; }
+#else  // DO_MULTITASK
+    
+    void set_state(vm_state st) { state = st; }
+#endif // DO_MULTITASK    
 };
-#endif // TEN4_SRC_VM_H
+#endif // TEN4_SRC_VM_VM_H
