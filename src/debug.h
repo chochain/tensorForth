0899705d7174f45468165a235d7ba88bbbcb5071
gnii <chochain@yahoo.com>
2025-02-19 10:29:50 +0800

    ~/src/debug.h add keep_fmt, reset_fmt; update ss_dump, word, dict_dump interface

--- src/debug.h
+++ src/debug.h
@@ -6,6 +6,7 @@
  */
 #ifndef TEN4_SRC_DEBUG_H
 #define TEN4_SRC_DEBUG_H
+#include <iomanip>                                        /// setw, setprec, setbase...
 #include "t4base.h"                                       ///< include ten4_types.h
 #include "io/aio.h"
 #include "vm/param.h"
@@ -16,21 +17,24 @@
 ///@name Debugger/Tracer class
 ///@{
 class Debug {                                             ///< friend class to MMU and AIO
-    MMU *mu;                                              ///< memory management unit
-    AIO *io;                                              ///< async IO unit
+    MMU      *mu;                                         ///< memory management unit
+    AIO      *io;                                         ///< async IO unit
     
 public:
     __HOST__ Debug(MMU *mmu, AIO *aio) : mu(mmu), io(aio) {}
     __HOST__ ~Debug() {}
+
+    __HOST__ void keep_fmt();
+    __HOST__ void reset_fmt();
     
-    __HOST__ void ss_dump(IU id, int n, int base=10);     ///< show data stack content
+    __HOST__ void ss_dump(IU id, int sz, DU tos, int base=10); ///< show data stack content
     __HOST__ int  p2didx(Param *p);                       ///< reverse lookup
     __HOST__ int  to_s(IU w, int base=10);                ///< show dictionary info from descriptor
     __HOST__ int  to_s(Param *p, int nv, int base);
-    __HOST__ void words(int base=10);                     ///< list dictionary words
-    __HOST__ void mem_dump(IU addr, int sz, int base=10); ///< dump memory frm addr...addr+sz
+    __HOST__ void words();                                ///< list dictionary words
+    __HOST__ void mem_dump(IU addr, int sz);              ///< dump memory frm addr...addr+sz
     __HOST__ void see(IU w, int base=10);                 ///< disassemble user defined word
-    __HOST__ void dict_dump(int base=10);                 ///< dump dictionary
+    __HOST__ void dict_dump();                            ///< dump dictionary
     __HOST__ void mem_stat();                             ///< display memory statistics
     ///
     /// self tests
