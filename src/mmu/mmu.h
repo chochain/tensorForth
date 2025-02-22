a3d9be77f86311bc2ac7974118e946426e80bfe9
gnii <chochain@yahoo.com>
2025-02-22 08:28:58 +0800

    ~/src/mmu/mmu.h update drop (WIP)

--- src/mmu/mmu.h
+++ src/mmu/mmu.h
@@ -60,7 +60,7 @@ public:
     __GPU__  void status();                                        ///< display current MMU status
     __GPU__  void dict_dump();
     template <typename F>
-    __GPU__ void  add_word(const char *name, F &f, int im) {       ///< append or merge a new word
+    __GPU__  void add_word(const char *name, F &f, int im) {       ///< append or merge a new word
         IU   w  = find(name);                                      ///< check whether word exists
         Code &c = _dict[w ? w : _didx++];                          ///< new or exist Code object
         c.set(name, f, im);                                        /// * hardcopy Code object
@@ -85,14 +85,6 @@ public:
     /// low level memory access
     ///
     __BOTH__ __INLINE__ IU   here()     { return _midx; }
-//    __BOTH__ __INLINE__ IU   ri(U8 *c)  { return ((IU)(*(c+1)<<8)) | *c; }
-//    __BOTH__ __INLINE__ IU   ri(U8 *c)  { return *(IU*)c; }
-//    __BOTH__ __INLINE__ IU   ri(IU i)   {
-//        if (i < T4_PMEM_SZ) return ri(&_pmem[i]);
-//        ERROR("\nmmu.wi[%d]", i);
-//        return 0;
-//    }
-//    __BOTH__ __INLINE__ DU   rd(U8 *c)  { DU d; MEMCPY(&d, c, sizeof(DU)); return d; }
     __BOTH__ __INLINE__ DU   rd(U8 *c)  { return *(DU*)c; }
     __BOTH__ __INLINE__ DU   rd(IU i)   {
         if (i < T4_PMEM_SZ) return rd(&_pmem[i]);
@@ -101,7 +93,7 @@ public:
     }
     __GPU__  __INLINE__ void wd(U8 *c, DU d)   {
         DU v = rd(c);
-        drop(v);
+        drop(v);                                              /// * reduce obj ref counter
         *(DU*)c = d;
 //        MEMCPY(c, &d, sizeof(DU));
     }
@@ -109,7 +101,6 @@ public:
         if (i < T4_PMEM_SZ) wd(&_pmem[i], d);
         else ERROR("\nmmu.wd[%d]", i);
     }
-//    __GPU__  __INLINE__ void wi(U8 *c, IU n)   { *c++ = n&0xff; *c = (n>>8)&0xff; }
     __GPU__  __INLINE__ void wi(U8 *c, IU n)   { *(IU*)c = n; }
     __GPU__  __INLINE__ void wi(IU i, IU n)    {
         if (i < T4_PMEM_SZ) wi(&_pmem[i], n);
@@ -132,8 +123,9 @@ public:
     ///
     /// tensor object life-cycle methods
     ///
-    __GPU__  void   mark_free(DU v);                        ///< mark an object to be freed in host
     __GPU__  void   sweep();                                ///< free marked tensor
+    __GPU__  void   drop(Tensor &t0);                       ///< reduce ref counter
+    __GPU__  void   mark_free(DU v);                        ///< mark an object to be freed in host
     __GPU__  Tensor &talloc(U64 sz);                        ///< allocate from tensor space
     __GPU__  Tensor &tensor(U64 sz);                        ///< create an vector
     __GPU__  Tensor &tensor(U32 h, U32 w);                  ///< create a matrix
@@ -142,17 +134,11 @@ public:
     __GPU__  void   free(Tensor &t);                        ///< free the tensor
     __GPU__  Tensor &copy(Tensor &t0);                      ///< hard copy a tensor
     __GPU__  Tensor &slice(Tensor &t0, IU x0, IU x1, IU y0, IU y1);     ///< a slice of a tensor
-#if   T4_ENABLE_NN    
+#if T4_ENABLE_NN    
     __GPU__  Dataset&dataset(U32 batch_sz);                 ///< create a NN dataset
     __GPU__  Model  &model(U32 sz=T4_NET_SZ);               ///< create a NN model
     __GPU__  void   free(Model &m);
 #endif // T4_ENABLE_NN
-    __GPU__  void   drop(DU d) {
-        if (!IS_OBJ(d) || IS_VIEW(d)) return;               /// non-object, just drop
-        T4Base &t = du2obj(d);                              /// check reference count
-        if (t.is_model()) free((Model&)t);                  /// release TLSF memory block
-        else              free((Tensor&)t);
-    }
 #else  // T4_ENABLE_OBJ ===========================================================
     __GPU__  void   sweep()    {}                           ///< holder for no object
     __GPU__  void   drop(DU v) {}
