0c5809ab850070f0c77b73e57582b714857575f4
gnii <chochain@yahoo.com>
2025-02-22 11:27:32 +0800

    ~/src/io/aio.cu show=>to_s; _show_obj with rdx

--- src/io/aio.cu
+++ src/io/aio.cu
@@ -10,44 +10,40 @@
 #include "aio.h"
 
 __HOST__ void
-AIO::show(DU v, int rdx) {
-#if T4_ENABLE_OBJ    
-    if (IS_OBJ(v)) { _show_obj(T4Base::du2obj(v), IS_VIEW(v)); return; }
-#endif
-    if (rdx != 10) fout << static_cast<int>(v);
-    else           fout << v;
-}
-
-__HOST__ void
-AIO::print(DU v) {
-#if T4_ENABLE_OBJ
-    T4Base &o = T4Base::du2obj(v);
-    switch (o.ttype) {
-    case T4_TENSOR:
-    case T4_DATASET: _print_tensor((Tensor&)o); break;
-#if T4_ENABLE_NN        
-    case T4_MODEL:   _print_model((Model&)o);   break;
-#endif // T4_ENABLE_NN        
-    case T4_XXX:     /* reserved */             break;
-    }
-    return;
-#endif // T4_ENABLE_OBJ
-    fout << v;
+AIO::to_s(DU v, int rdx) {
+    fout << std::setbase(rdx) << v << std::setbase(_radix);
 }
 ///
 /// Object debugging methods
 ///
 #if T4_ENABLE_OBJ
+__HOST__ void
+AIO::to_s(T4Base &t, bool is_view, int rdx) {
+    _show_obj(t, is_view, rdx);
+}
+
+__HOST__ void
+AIO::print(T4Base &t) {
+    switch (t.ttype) {
+    case T4_TENSOR:
+    case T4_DATASET: _print_tensor(fout, (Tensor&)t); break;
+#if T4_ENABLE_NN        
+    case T4_MODEL:   _print_model(fout, (Model&)t);   break;
+#endif // T4_ENABLE_NN        
+    case T4_XXX:     /* reserved */                   break;
+    }
+}
+
 __HOST__ int
-AIO::_show_obj(T4Base &t, bool view) {
+AIO::_show_obj(T4Base &t, bool is_view, int rdx) {
     static const char tn[2][4] = {                   ///< sync with t4_obj
         { 'T', 'N', 'D', 'X' }, { 't', 'n', 'd', 'x' }
     };
-    auto t2 = [&fout](Tensor &t) { fout << t.H() << ',' << t.W() << ']'; };
-    auto t4 = [&fout](Tensor &t) {
+    auto t2 = [this](Tensor &t) { fout << t.H() << ',' << t.W() << ']'; };
+    auto t4 = [this](Tensor &t) {
         fout << t.N() << ',' << t.H() << ',' << t.W() << ',' << t.C() << ']';
     };
-    fout << tn[view][t.ttype];
+    fout << std::setbase(rdx) << tn[is_view][t.ttype];
     switch(t.rank) {
     case 0: fout << "["  << (t.numel - 1) << "]"; break; // network model
     case 1: fout << "1[" << t.numel << "]";       break;
@@ -55,6 +51,7 @@ AIO::_show_obj(T4Base &t, bool view) {
     case 4: fout << "4["; t4((Tensor&)t);         break;
     case 5: fout << "5[" << t.parm << "]["; t4((Tensor&)t); break;
     }
+    fout << std::setbase(_radix);
     return 1;
 }
 #endif // T4_ENABLE_OBJ    
