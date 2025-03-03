9ee4677c14c2cf13ef6f45b0c70f09dc1f260cd9
gnii <chochain@yahoo.com>
2025-03-03 15:31:06 +0800

    ~/src/debug.h get mu,io from singletons (instead of passing)

--- src/debug.h
+++ src/debug.h
@@ -17,27 +17,35 @@
 ///@name Debugger/Tracer class
 ///@{
 class Debug {                                             ///< friend class to MMU and AIO
-    MMU      *mu;                                         ///< memory management unit
-    AIO      *io;                                         ///< async IO unit
+    MMU      *mu;                                         ///< memory controller
+    AIO      *io;                                         ///< streaming io controller
+    h_ostr   &fout;                                       ///< host output stream
+    int      trace;                                       ///< debug tracing verbosity level
+    
     char     tmp[256];                                    ///< tmp string buffer
     
-    __HOST__ Debug(MMU *mmu, AIO *aio) : mu(mmu), io(aio) {}
+    __HOST__ Debug(h_ostr &o, int verbo) : fout(o), trace(verbo) {
+        mu = MMU::get_mmu();
+        io = AIO::get_io();
+    }
     __HOST__ ~Debug() { TRACE("\\   Debug: instance freed\n"); }
     
 public:
-    static __HOST__ Debug *get_db(MMU *mmu, AIO *aio);    ///< singleton contructor
+    static __HOST__ Debug *get_db(h_ostr &o, int verbo);  ///< singleton contructor
     static __HOST__ Debug *get_db();                      ///< singleton getter
     static __HOST__ void  free_db();                      ///< singleton destructor
     
     __HOST__ void keep_fmt();
     __HOST__ void reset_fmt();
-    
+
+    __HOST__ void print(DU v, int base);                       ///< show value and obj token for ss_dump
+    __HOST__ void print(void *vp, U8 gt);                      ///< display value, io proxy
     __HOST__ void ss_dump(IU id, int sz, DU tos, int base=10); ///< show data stack content
-    __HOST__ void words();                                ///< list dictionary words
-    __HOST__ void mem_dump(IU addr, int sz);              ///< dump memory frm addr...addr+sz
-    __HOST__ void see(IU w, int base=10);                 ///< disassemble user defined word
-    __HOST__ void dict_dump();                            ///< dump dictionary
-    __HOST__ void mem_stat();                             ///< display memory statistics
+    __HOST__ void words();                                     ///< list dictionary words
+    __HOST__ void mem_dump(IU addr, int sz);                   ///< dump memory frm addr...addr+sz
+    __HOST__ void see(IU w, int base=10);                      ///< disassemble user defined word
+    __HOST__ void dict_dump();                                 ///< dump dictionary
+    __HOST__ void mem_stat();                                  ///< display memory statistics
     ///
     /// self tests
     ///
@@ -47,11 +55,10 @@ private:
     ///
     /// methods for supporting see
     ///
-    __HOST__ char *_d2h(const char *d_str);               ///< convert from device string
-    __HOST__ int  _p2didx(Param *p);                      ///< reverse lookup
-    __HOST__ int  _to_s(IU w, int base);                  ///< show dictionary entry
-    __HOST__ int  _to_s(Param *p, int nv, int base);      ///< show by parameter memory pointer
-    __HOST__ void _ss(DU v, int base);                    ///< display value by ss_dump
+    __HOST__ char *_d2h(const char *d_str);                   ///< convert from device string
+    __HOST__ int  _p2didx(Param *p);                          ///< reverse lookup
+    __HOST__ int  _to_s(IU w, int base);                      ///< show dictionary entry
+    __HOST__ int  _to_s(Param *p, int nv, int base);          ///< show by parameter memory pointer
 };
 ///@}
 #endif // __DEBUG_H
