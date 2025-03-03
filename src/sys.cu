1b17a4c673d717abd4b47f741af2a20b2ca1ac43
gnii <chochain@yahoo.com>
2025-03-03 15:26:30 +0800

    ~/src/sys.cu call simplified debug interface; pass print/debug to io

--- src/sys.cu
+++ src/sys.cu
@@ -4,6 +4,7 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
+#include <iostream>
 #include "sys.h"
 #include "ldr/loader.h"
 
@@ -38,10 +39,10 @@ k_rand(DU *mat, int sz, DU bias, DU scale, curandState *st, rand_opt ntype) {
 ///
 __HOST__
 System::System(h_istr &i, h_ostr &o, int khz, int verbo)
-    : _khz(khz), _istr(new Istream()), _ostr(new Ostream()), _trace(verbo) {
-    mu = MMU::get_mmu();             ///> instantiate memory manager
-    io = AIO::get_io(i, o, verbo);   ///> instantiate async IO manager
-    db = Debug::get_db(mu, io);      ///> tracing instrumentation
+    : _khz(khz), fin(i), _istr(new Istream()), _ostr(new Ostream()), _trace(verbo) {
+    mu = MMU::get_mmu();             ///> instantiate memory controller
+    io = AIO::get_io();              ///> instantiate async IO controler
+    db = Debug::get_db(o, verbo);    ///> tracing instrumentation
         
 #if (T4_ENABLE_OBJ && T4_ENABLE_NN)
     Loader::init(verbo);
@@ -93,13 +94,14 @@ System::rand(DU *d, U64 sz, rand_opt n, DU bias, DU scale) {
 ///
 ///> feed device input stream with a line from host input
 ///
-#include <string.h>
+#include <iostream>
+#include <string>
 __HOST__ int
 System::readline() {
     _istr->clear();                          /// * clear device inpugt stream
-    char *tib = _istr->rdbuf();              ///< device input buffer
-    io->fin.getline(tib, T4_IBUF_SZ, '\n');  /// * feed input buffer
-    return !io->fin.eof();                   /// * end of file
+    char *tib = _istr->rdbuf();              ///< set device input buffer
+    fin.getline(tib, T4_IBUF_SZ, '\n');      /// * feed input from host to device
+    return fin.eof();                        /// * end of input flag
 }
 
 #define NEXT_EVENT(n) ((io_event*)((char*)&ev->data[0] + ev->sz))
@@ -108,24 +110,14 @@ __HOST__ io_event*
 System::process_event(io_event *ev) {
     GPU_SYNC();                     /// * make sure data is completely written
 
-    char   *v    = (char*)ev->data; ///< fetch payload in buffered print node
-    h_ostr &fout = io->fout;        ///< host output stream
+    void *v = (void*)ev->data;      ///< fetch payload in buffered print node
     switch (ev->gt) {
-    case GT_INT:   fout << (*(S32*)v);                 break;
-    case GT_U32:   fout << static_cast<U32>(*(U32*)v); break;
-    case GT_FLOAT: fout << (*(F32*)v);                 break;
-    case GT_STR:   fout << v;                          break;
-    case GT_FMT:   {
-        obuf_fmt *f = (obuf_fmt*)v;
-        DEBUG("FMT: b=%d, w=%d, p=%d, f='%c'\n", f->base, f->width, f->prec, f->fill);
-        fout << std::setbase(f->base)
-             << std::setw(f->width)
-             << std::setprecision(f->prec ? f->prec : -1)
-             << std::setfill((char)f->fill);
-    } break;
-#if T4_ENABLE_OBJ
-    case GT_OBJ: io->print(fout, mu->du2obj(*(DU*)v));     break;
-#endif // T4_ENABLE_OBJ        
+    case GT_INT:
+    case GT_U32:
+    case GT_FLOAT:
+    case GT_STR:
+    case GT_FMT:
+    case GT_OBJ: db->print(v, ev->gt);                     break;
     case GT_OPX: {
         _opx *o = (_opx*)v;
         DEBUG("OP=%d, m=%d, i=%d, n=0x%08x=%g\n", o->op, o->m, o->i, DU2X(o->n), o->n);
@@ -172,7 +164,8 @@ System::process_event(io_event *ev) {
             Model &m = (Model&)mu->du2obj(o->n);
             if (m.is_model()) {
                 ev = NEXT_EVENT(ev);
-                io->nload(m, (char*)ev->data, o->m);
+                _istr->clear();
+                io->nload(m, (char*)ev->data, o->m, _istr->rdbuf());
             }
             else ERROR("%x is not a model\n", DU2X(o->n));
         } break;
@@ -180,7 +173,7 @@ System::process_event(io_event *ev) {
 #endif // T4_ENABLE_OBJ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
         }
     } break;
-    default: fout << "print type not supported: " << (int)ev->gt; break;
+    default: ERROR("event type not supported: %d\n", (int)ev->gt); break;
     }
     return NEXT_EVENT(ev);
 }
