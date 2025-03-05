05b793fa176561bffe1ea11ff776e122ed309009
gnii <chochain@yahoo.com>
2025-03-05 17:19:21 +0800

    ~/src/sys.cu mv rand trace to nn/model

--- src/sys.cu
+++ src/sys.cu
@@ -96,8 +96,6 @@ System::delay(int ticks) {
 
 __GPU__ void
 System::rand(DU *d, U64 sz, rand_opt n, DU bias, DU scale) {
-    DEBUG("sys#rand(T%d) numel=%ld bias=%.2f, scale=%.2f\n",
-          t.rank, t.numel, bias, scale);
     k_rand<<<1, T4_RAND_SZ>>>(d, sz, bias, scale, n);
 }
 
