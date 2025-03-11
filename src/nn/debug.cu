40a9c94ff5c8e6565b8b7c4ac3ea883b25eb6af1
gnii <chochain@yahoo.com>
2025-03-11 17:35:13 +0800

    ~/src/nn/debug.cu printf=>INFO

--- src/nn/debug.cu
+++ src/nn/debug.cu
@@ -16,47 +16,47 @@ Model::_dump_dbdf(Tensor &db, Tensor &df) {
     DU sum = DU0;
     for (U32 n = 0; n < df.N(); n++) {
         DU *v = df.slice(n), fsum = DU0;
-        printf("\n\tdf[%d]=", n);
+        INFO("\n\tdf[%d]=", n);
         for (U32 i = 0; i < df.HWC(); i++) {
             sum  += *v;
             fsum += *v;
-            printf("%5.2f ", *v++);
+            INFO("%5.2f ", *v++);
         }
-        printf("Σ=%5.2f", fsum);
+        INFO("Σ=%5.2f", fsum);
     }
-    printf("\n\tΣΣ=%6.3f", sum);
+    INFO("\n\tΣΣ=%6.3f", sum);
 }
 
 __GPU__ void
 Model::_dump_db(Tensor &db) {
     DU sum = DU0;
-    printf("\n\tdb=");
+    INFO("\n\tdb=");
     DU *v = db.data;
     for (U32 i = 0; i < db.H(); i++) {
-        printf("%6.3f ", *v);
+        INFO("%6.3f ", *v);
         sum += *v++;
     }
-    printf(" Σ=%6.3f", sum);
+    INFO(" Σ=%6.3f", sum);
 }
 
 __GPU__ void
 Model::_dump_dw(Tensor &dw, bool full) {
     const U32 H = dw.H(), W = dw.W();
     DU hsum = DU0, *p = dw.data;
-    if (full) printf("\ndw[%d,%d]=", W, H);
-    else      printf("\n\tdwΣ=");
+    if (full) INFO("\ndw[%d,%d]=", W, H);
+    else      INFO("\n\tdwΣ=");
     for (U32 i = 0; i < H; i++) {
-        if (full) printf("\n\tdw[%d]=", i);
+        if (full) INFO("\n\tdw[%d]=", i);
         DU sum = DU0;
         for (U32 j = 0; j < W; j++, p++) {
             sum  += *p;
             hsum += *p;
-            if (full) printf("%6.3f", *p);
+            if (full) INFO("%6.3f", *p);
         }
-        if (full) printf(" Σ=%6.3f", sum);
-        else      printf("%6.3f ", sum);
+        if (full) INFO(" Σ=%6.3f", sum);
+        else      INFO("%6.3f ", sum);
     }
-    if (H > 1) printf("%sΣΣ=%6.3f", full ? "\n\t" : " ", hsum);
+    if (H > 1) INFO("%sΣΣ=%6.3f", full ? "\n\t" : " ", hsum);
 }
 #endif  // (T4_DO_OBJ && T4_DO_NN)
 //==========================================================================
