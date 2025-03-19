1b5293d0a2b6712bd905d69bed73a237094c6a10
gnii <chochain@yahoo.com>
2025-03-19 11:04:40 +0800

    ~/src/vm/netvm.cu nsum,nvar=>bsum,bvar (for batchnorm debug)

--- src/vm/netvm.cu
+++ src/vm/netvm.cu
@@ -386,16 +386,16 @@ NetVM::init() {
     CODE("nn.w=",   _set_parm(0));                 ///< populate tensor.weight
     CODE("nn.b=",   _set_parm(1));                 ///< populate tensor.bias
     CODE("network", if (IS_M(tos)) sys.dot(DOT, tos));  ///< non destructive
-    CODE("nsum",
+    CODE("bsum",
          if (!IS_OBJ(tos)) return;
          Tensor &A = TTOS;
-         Tensor &O = mmu.tensor(A.N(), A.C());
+         Tensor &O = mmu.tensor(A.C());
          Tensor::sum(A, O);
          PUSH(O));
-    CODE("nvar",
+    CODE("bvar",
          if (!IS_OBJ(tos)) return;
          Tensor &A = TTOS;
-         Tensor &O = mmu.tensor(A.N(), A.C());
+         Tensor &O = mmu.tensor(A.C());
          Tensor &G = COPY(O);
          Tensor::var(A, G, O);
          PUSH(O);
