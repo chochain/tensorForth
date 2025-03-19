f8ad9cf59e54571368f01afce5851f91fe3b82a1
gnii <chochain@yahoo.com>
2025-03-19 11:04:02 +0800

    ~/src/ten4_types.h enable NN_DB (trace NN work)

--- src/ten4_types.h
+++ src/ten4_types.h
@@ -35,7 +35,7 @@
 #define NN_DB(...)          TRACE(__VA_ARGS__)
 #else  // !MM_DEBUG
 #define MM_DB(...)
-#define NN_DB(...)
+#define NN_DB(...)          INFO(__VA_ARGS__)
 #endif // MM_DEBUG
 ///@}
 ///@name CUDA support macros
