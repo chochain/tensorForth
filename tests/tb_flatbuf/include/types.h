9bd235da627ebad374d391c3923df91c858425ea
gnii <chochain@yahoo.com>
2026-02-12 20:13:21 +0800

    ~/tests/tb_flatbuf/include add types.h (sync with ten4)

---
+++ tests/tb_flatbuf/include/types.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <unistd.h>
+#include <cstring>
+#include <string>
+
+typedef bool              BOOL;
+typedef size_t            USZ;
+typedef uint8_t           U8;
+typedef uint16_t          U16;
+typedef uint32_t          U32;
+typedef uint64_t          U64;
+typedef int32_t           S32;
+typedef int64_t           S64;
+typedef float             F32;
+typedef double            F64;
+
+typedef std::string       STR;
+typedef std::vector<U8>   U8V;
+typedef std::vector<U16>  U16V;
+typedef std::vector<F32>  F32V;
+typedef std::vector<F64>  F64V;
+
