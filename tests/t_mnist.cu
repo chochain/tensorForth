9ef62153050d54d921526ee993b3da5f7ba34a24
gnii <chochain@yahoo.com>
2026-02-14 11:10:09 +0800

    ~/tests/t_mnist param N,H,W; add traces (sync 4.0)

--- tests/t_mnist.cu
+++ tests/t_mnist.cu
@@ -4,20 +4,33 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
+#define T4_DO_OBJ 1
+#define T4_DO_NN  1
+
 #include "loader.h"
 #include "mnist_vu.h"
 
+#define VU_W 40
+#define VU_H 30
+#define VU_N (VU_W*VU_H)
+
 int main(int argc, char **argv) {
+    const char *ds_name = "mnist_test";
+    
     setenv("DISPLAY", ":0", 0);
     cudaSetDevice(0);
 
-    Loader::init(1);
+    Loader::init();
     if (gui_init(&argc, argv)) return -1;
-    
-    Corpus *nd = Loader::get(0, "mnist_test");
+
+    Dataset *ds = new Dataset(VU_N, 28, 28, 1);
+    Corpus  *nd = Loader::get(*ds, ds_name);
     if (!nd) return 0;
 
-    MnistVu *vu = new MnistVu(*nd->fetch(1200));  // 1200 samples a shot
+    nd->init(true);
+    printf("\n%s => [%d,%d,%d,%d]\n", ds_name, nd->N, nd->H, nd->W, nd->C);
+
+    MnistVu *vu = new MnistVu(*nd->fetch(0, VU_N, 1), VU_W, VU_H);  // 1200 samples a shot
     gui_add(vu);
         
     return gui_loop();
