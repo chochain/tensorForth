98bc9c4ce598fcfe55baf2993c12a82332bc560a
gnii <chochain@yahoo.com>
2026-04-06 16:29:28 +0000

    ~/src/vm/vm.h mv ip,tos from eforthvm; add xop1,xop2 macros

--- src/vm/vm.h
+++ src/vm/vm.h
@@ -12,9 +12,46 @@
 
 namespace t4::vm {
 ///
+///@name ALU opcodes (1-operand and 2-operand)
+///@{
+#define XOP1(op, v)                             \
+    DU t = tos;                                 \
+    switch (op) {                               \
+    case ABS:  t = ABS(t);          break;      \
+    case NEG:  t = NEG(t);          break;      \
+    case EXP:  t = EXP(t);          break;      \
+    case LN:   t = LN(t);           break;      \
+    case LOG:  t = LOG(t);          break;      \
+    case TANH: t = TANH(t);         break;      \
+    case RELU: t = MAX(t, DU0);     break;      \
+    case SIGM: t = SIGMOID(t);      break;      \
+    case SQRT: t = SQRT(t);         break;      \
+    case RCP:  t = RCP(t);          break;      \
+    case SAT:  t = SAT(t);          break;      \
+    case POW:  t = POW(t, v);       break;      \
+    }                                           \
+    SCALAR(t); tos = t
+
+#define XOP2(op)                                \
+    DU t = tos, n = ss.pop();                   \
+    switch (op) {                               \
+    case ADD:  t = ADD(n, t);       break;      \
+    case MUL:  t = MUL(n, t);       break;      \
+    case SUB:  t = SUB(n, t);       break;      \
+    case DIV:  t = DIV(n, t);       break;      \
+    case MOD:  t = MOD(n, t);       break;      \
+    case MAX:  t = MAX(n, t);       break;      \
+    case MIN:  t = MIN(n, t);       break;      \
+    case MUL2: t = MUL2(n,t);       break;      \
+    case MOD2: t = MOD2(n,t);       break;      \
+    }                                           \
+    SCALAR(t); tos = t
+
+///@}
 ///@name virtual machine base class
 ///@{
 typedef enum { STOP=0, HOLD, QUERY, NEST, VM_STATE_SZ } vm_state;   ///< ten4 states
+
 class VM {                            ///< VM (created in kernal mode)
 public:
     IU        id;                     ///< VM id
@@ -25,13 +62,23 @@ public:
 
     mu::Vector<DU, 0> ss;             ///< parameter stack (setup in ten4.cu)
     mu::Vector<DU, 0> rs;             ///< return stack
+    
+    IU    ip     = 0;                 ///< instruction pointer
+    DU    tos    = -DU1;              ///< cached top of stack
 
     __HOST__  VM(int id, System &sys);
     __HOST__  ~VM() { TRACE("%d ", id); }
-    
+    ///
+    /// VM life-cycle controls
+    ///
     __HOST__  virtual void   init()   { TRACE("VM[%d]::init ok\n", id); }
     __HOST__  virtual void   resume() {}
     __HOST__  virtual void   outer();
+    ///
+    /// ALU operators
+    ///
+    __HOST__ virtual void xop1(math_op op, DU v=DU0) { XOP1(op, v); }           ///< single operand operator
+    __HOST__ virtual void xop2(math_op op, t4_drop_opt x=T_KEEP) { XOP2(op); }  ///< 2-operand operator
     
 protected:
     bool  compile = false;            ///< compiling flag
@@ -41,7 +88,7 @@ protected:
     __HOST__ virtual int pre(char *str)     { return 0; }
     __HOST__ virtual int process(char *str) { return 0; }
     __HOST__ virtual int post()             { return 0; }
-    
+
 #if DO_MULTITASK    
     static MUTEX    tsk;              ///< mutex for tasker
     static COND_VAR cv_tsk;           ///< tasker control
