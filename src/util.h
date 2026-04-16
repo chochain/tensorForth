da2f8c670146b107f260028730d2606d805f3154
gnii <chochain@yahoo.com>
2026-04-16 00:55:49 +0000

    ~/src/util (refactor math functions to t4math)

--- src/util.h
+++ src/util.h
@@ -6,106 +6,18 @@
  */
 #ifndef __UTIL_H_
 #define __UTIL_H_
-#include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
 #include "ten4_config.h"
 
 namespace t4 {
-///@}
+
 ///@name Alignment macros
 ///@{
 #define ALIGN2(sz)  ((sz) + (sz & 0x1))
 #define ALIGN4(sz)  ((sz) + (-(sz) & 0x3))
 #define ALIGN8(sz)  ((sz) + (-(sz) & 0x7))
 #define ALIGN16(sz) ((sz) + (-(sz) & 0xf))
-///@}
-///@name cross platform floating-point ALU support (see nvcc -use_fast_math flag)
-///@{
-typedef enum {
-    /// 1-operand arithmetic ops
-    ABS = 0,
-    NEG,
-    EXP,
-    LN,
-    LOG,
-    TANH,
-    RELU,
-    SIGM,
-    SQRT,
-    RCP,
-    SAT,
-    IDEN,
-    /// 1-operand + a constant
-    FILL,
-    GFILL,
-    SCALE,
-    POW,
-    /// 2-operand ops
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    MOD,
-    MAX,
-    MIN,
-    MUL2,
-    MOD2
-} math_op;
-
-#define MATH_OP "abs","neg","exp","ln","log","tanh","relu","sigmoid","sqrt","rcp","sat","iden","fill","gfill","scale","pow","+","-","*","/","mod","max","min","mul2","mod2"
-
-#if 0 // GPU mode __CUDA_ARCH__
-#define ABS(d)       (fabsf(d))                 /**< absolute value         */
-#define NEG(d)       (-d)                       /**< negate                 */
-#define EXP(d)       (__expf(d))                /**< exponential(float)     */
-#define LN(d)        (__logf(d))                /**< natural logrithm       */
-#define LOG(d)       (__log10f(d))              /**< log10                  */
-#define TANH(d)      (atanhf(d))                /**< tanh(float)            */
-#define RELU(d)      (MAX(0.0, d))              /**< relu(float)            */
-#define SIGMOID(d)   (RCP(1.0+EXP(-(d))))       /**< sigmoid(float)         */
-#define SQRT(d)      (__fsqrt_rn(d))            /**< square root            */
-#define RCP(x)       (__frcp_rn(x))             /**< reciprocol 1/x         */
-#define SAT(d)       (__saturatef(d))           /**< clamp into [0.0..1.0]  */
-#define POW(d,e)     (__powf(d,e))              /**< power d^(e)            */
-#define ADD(x,y)     (__fadd_rn(x,y))           /**< addition               */
-#define SUB(x,y)     (__fsub_rn(x,y))           /**< subtraction            */
-#define MUL(x,y)     (__fmul_rn(x,y))           /**< multiplication         */
-#define DIV(x,y)     (__fdiv_rn(x,y))           /**< division               */
-#define MOD(t,n)     (fmodf(t,n))               /**< fmod two floats        */
-#define MAX(x,y)     (fmax(x,y))                /**< maximum of the two     */
-#define MIN(x,y)     (fmin(x,y))                /**< minimum of the two     */
-#define MUL2(x2,y2)  (__dmul_rn(x2,y2))         /**< double precision mul   */
-#define MOD2(x2,y2)  (fmod(x2,y2))              /**< double precision mod   */
-#else // (HOST mode) !__CUDA_ARCH__
-#include <cmath>
-#define ABS(d)       (fabs(d))                  /**< absolute value         */
-#define NEG(d)       (-d)                       /**< negate                 */
-#define EXP(d)       (expf(d))                  /**< exponential(float)     */
-#define LN(d)        (logf(d))                  /**< natural logrithm       */
-#define LOG(d)       (log10f(d))                /**< log10                  */
-#define TANH(d)      (atanhf(d))                /**< tanh(float)            */
-#define RELU(d)      (MAX(0.0f, (d)))           /**< relu(float)            */
-#define SIGMOID(d)   (RCP(1.0f+EXP(-(d))))      /**< sigmoid(float)         */
-#define SQRT(d)      (sqrtf(d))                 /**< square root            */
-#define RCP(x)       (1.0f/(x))                 /**< reciprocol 1/x         */
-#define SAT(d)       (MIN(1.0f,MAX(0.0f,(d))))  /**< clamp into [0.0..1.0]  */
-#define POW(d,e)     (powf((d),(e)))            /**< power d^(e)            */
-#define ADD(x,y)     ((x)+(y))                  /**< addition               */
-#define SUB(x,y)     ((x)-(y))                  /**< subtraction            */
-#define MUL(x,y)     ((x)*(y))                  /**< multiplication         */
-#define DIV(x,y)     ((x)/(y))                  /**< division               */
-#define MOD(t,n)     ((DU)fmodf((t),(n)))       /**< fmod two floats        */
-#define MAX(x,y)     (fmaxf((x),(y)))           /**< maximum of the two     */
-#define MIN(x,y)     (fminf((x),(y)))           /**< minimum of the two     */
-#define MUL2(x2,y2)  ((DU2)(x2)*(y2))           /**< double precision mul   */
-#define MOD2(x2,y2)  (fmod((DU2)(x2),(DU2)(y2)))/**< double precision mod   */
-#endif // (GPU mode) __CUDA_ARCH__
-
-typedef enum {
-    T_DROP = 0,
-    T_KEEP
-} t4_drop_opt;
 ///@}
 #define __HOST__     __host__
 #define __KERN__     __global__
@@ -115,8 +27,8 @@ typedef enum {
 extern "C" {
 #endif
 
-uint32_t hbin_to_u32(const void *bin);
-uint16_t hbin_to_u16(const void *bin);
+__HOST__ uint32_t    hbin_to_u32(const void *bin);
+__HOST__ uint16_t    hbin_to_u16(const void *bin);
 #if defined(__CUDACC__)
 ///
 ///@name Endianess conversion
@@ -147,30 +59,10 @@ __GPU__ char*        d_strcut(const char *s, int n);                           /
 ///@}
 ///@name Numeric conversion
 ///@{
-__GPU__ inline float d__stride_sum(float *src, long numel, long tid);          /// stride sum per thread
-__GPU__ inline float d__stride_var(float *src, float avg, long numel, long tid);
-__GPU__ inline float d__warp_sum(float v);                                     /// reduce sum up a warp
-__GPU__ inline float d__rollup_sum(float *smem);
 __GPU__ int          d_itoa(int v, char *s, int base=10);
 __GPU__ long         d_strtol(const char *s, char **p, int base=10);
 __GPU__ double       d_strtof(const char *s, char **p);
 __GPU__ int          d_hash(const char *s);
-///@}
-///@name Tensor ops (kernel mode)
-///@{
-__KERN__ void        k_sum(float* __restrict__ src, float* __restrict__ sum, long numel);
-__KERN__ void        k_nvar(float *src, float *avg, float var, long numel);    /// n * variance
-__KERN__ void        k_batchsum(float *src, float *sum, long numel);
-__KERN__ void        k_batchnvar(float *src, float *avg, float *var, long numel);
-__KERN__ void        k_copy(float *src, float *dst, long n);                   ///< Note: (src, dst)
-__KERN__ void        k_transpose(float *src, float *dst, int h, int w);        ///< Note: (src, dst), TODO: CDP
-__KERN__ void        k_identity(float *t, int h, int w);
-__KERN__ void        k_math(math_op op, float *dst, float v, long n);          ///< tensor math ops
-__KERN__ void        k_ts_op(math_op op, float *A, float v, float *O, long n); ///< tensor-scalar ops
-__KERN__ void        k_tt_op(math_op op, float *A, float *B, float *O, long n);///< tensor-tensor ops
-__KERN__ void        k_bce(float *O, float *T, long n);
-__KERN__ void        k_nan_inf(float *src, int *n, long numel);
-__KERN__ void        k_dummy();
 ///@}
 #if defined(__CUDA_ARCH__)
 ///==========================================================================
@@ -202,7 +94,7 @@ __KERN__ void        k_dummy();
 #define HASH(s)         d_hash((const char*)(s))
 ///@}
 #else  // !defined(__CUDA_ARCH__)
-#include <stdio.h>
+//#include <stdio.h>
 ///
 ///@name Unified memory ops
 ///@{
