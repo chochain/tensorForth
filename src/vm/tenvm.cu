a1a695e9281e6c0578afbc89ff5596f780a1a750
gnii <chochain@yahoo.com>
2026-04-20 23:30:28 +0000

    ~/src/vm/tenvm.cu gemm4,5=>gemm3,4

--- src/vm/tenvm.cu
+++ src/vm/tenvm.cu
@@ -227,7 +227,6 @@ TensorVM::gemm(int opt) {                           ///< GEMM ( a b A B C -- a b
         case 2: Tensor::gemm2(A, B, O, a, b); break;
         case 3: Tensor::gemm3(A, B, O, a, b); break;
         case 4: Tensor::gemm4(A, B, O, a, b); break;
-        case 5: Tensor::gemm5(A, B, O, a, b); break;
         }
         PUSH(O);
         VLOG("} tenvm#gemm => O[%d,%d]\n", O.H(), O.W());
@@ -515,9 +514,8 @@ TensorVM::init() {
     CODE("gemm",      gemm(0));               ///< (a b A B C -- a b A B C O) GEMM x86
     CODE("gemm1",     gemm(1));               ///< (a b A B C -- a b A B C O) GEMM k_gemm (gemini)
     CODE("gemm2",     gemm(2));               ///< (a b A B C -- a b A B C O) GEMM k_gemm_claude (tiled)
-    CODE("gemm3",     gemm(3));               ///< (a b A B C -- a b A B C O) GEMM k_gemm_tile_gemini (tiled/reg)
-    CODE("gemm4",     gemm(4));               ///< (a b A B C -- a b A B C O) GEMM k_gemm_tile_claude (tiled/reg)
-    CODE("gemm5",     gemm(5));               ///< (a b A B C -- a b A B C O) GEMM k_gemm_tile_claude_x2 (tiled/reg)
+    CODE("gemm3",     gemm(3));               ///< (a b A B C -- a b A B C O) GEMM k_gemm_tile_claude (tiled/reg)
+    CODE("gemm4",     gemm(4));               ///< (a b A B C -- a b A B C O) GEMM k_gemm_tile_claude_x2 (tiled/reg, 2buf)
     ///@}
     ///@defgroup Tensor persistance
     ///@brief - stick to PyTorch naming when possible
