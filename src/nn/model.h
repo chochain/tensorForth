708d1689ebf99d3b033816885c22b818e01fd5cb
gnii <chochain@yahoo.com>
2026-04-24 23:52:44 +0000

    ~/src/nn/model.h fix conditional compile tag

--- src/nn/model.h
+++ src/nn/model.h
@@ -4,11 +4,12 @@
  *
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
+#ifndef __NN_MODEL_H
+#define __NN_MODEL_H
 #pragma once
 #include "ten4_config.h"
 
-#if (!defined(__MMU_MODEL_H) && T4_DO_OBJ && T4_DO_NN)
-#define __MMU_MODEL_H
+#if (T4_DO_OBJ && T4_DO_NN)
 #include "sys.h"             /// * ms, rand
 #include "mu/mmu.h"
 
@@ -164,4 +165,5 @@ private:
 
 } // namespace t4::nn
 
-#endif // (!defined(__NN_MODEL_H) && T4_DO_OBJ && T4_DO_NN)
+#endif // (T4_DO_OBJ && T4_DO_NN)
+#endif // __NN_MODEL_H
