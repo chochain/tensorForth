862715229a604a086297c4c376d30a84ff793ff7
gnii <chochain@yahoo.com>
2026-05-20 00:04:31 +0000

    ~/src/tb/graph.h namespace=>t4::tb::graph; proto::Encoder=>Encoder; update comments

--- src/tb/graph.h
+++ src/tb/graph.h
@@ -4,10 +4,9 @@
  @note - see schema.h for details
  */
 #pragma once
+#include "encoder.h"
 
-#include "writer.h"
-
-namespace graph {
+namespace t4::tb::graph {
 
 // ── GraphDef ───────────────────────────────────────────────────────────
 struct AttrValue {
@@ -28,7 +27,7 @@ struct AttrValue {
     AttrValue(STR& s)        : AttrValue(s.c_str()) {}
 };
 
-class Node : public proto::Encoder {
+class Node : public Encoder {
 public:    
     explicit Node(const STR& name, const STR& op) {
         str(1, name);
@@ -43,13 +42,13 @@ public:
         if (input.size() > 0) str(3, input);
     }
     void add_type(const STR& name, int dt_type=1) {  // DT_FLOAT
-        proto::Encoder dt;
+        Encoder dt;
         dt.s32(6, dt_type);
 
         _attr(name, dt.buf());
     }
     void add_value(const STR& k, const AttrValue& av) {
-        proto::Encoder v;
+        Encoder v;
         switch (av.type) {
         case AttrValue::AV_FLOAT: v.f32(4, av.f);        break;
         case AttrValue::AV_INT:   v.s64(3, av.i);        break;
@@ -59,59 +58,59 @@ public:
         _attr(k, v.buf());
     }
     void add_stride(U16V stride) {
-        proto::Encoder v;
-        for (U64 s : stride) {          // raw numbers
+        Encoder v;
+        for (U64 s : stride) {          /// * add raw numbers
             v.pack(s);
         }
-        proto::Encoder t;               // ListValue
-        t.raw(3, v.buf());              // 3 = repeated int64
+        Encoder t;                      ///< ListValue
+        t.raw(3, v.buf());              /// * 3 = repeated int64
 
-        proto::Encoder av;              // AttrValue
-        av.raw(1, t.buf());             // ListValue as length limited
+        Encoder av;                     ///< AttrValue
+        av.raw(1, t.buf());             /// * ListValue as length limited
 
         _attr("strides", av.buf());
     }
     void add_shape(U32V shape) {
-        proto::Encoder ts;              // TensorShapeProto
+        Encoder ts;                     ///< TensorShapeProto
         ts.raw(7, _dim(shape));
 
         _attr("shape", ts.buf());
     }
     void add_tensor(U32V shape, F32V value) {
-        proto::Encoder v;               // collect tensor values
+        Encoder v;                      ///< collect tensor values
         for (F32 f : value) {
             v.pack(f);
         }
-        proto::Encoder tv;              // TensorProto.Value
-        tv.s32(1, 1);                   // DataType 1:DT_FLOAT
+        Encoder tv;                     ///< TensorProto.Value
+        tv.s32(1, 1);                   /// * DataType 1:DT_FLOAT
         tv.raw(2, _dim(shape));
-        tv.raw(5, v.buf());             // repeated float
+        tv.raw(5, v.buf());             /// * repeated float
         
-        proto::Encoder t;               // TensorProto
+        Encoder t;                      ///< TensorProto
         t.raw(8, tv.buf());
 
         _attr("value", t.buf());
     }
     
 private:
-    U8V _dim(U32V shape) {
-        proto::Encoder v;
+    U8V _dim(U32V shape) {             ///< add tensor dimensions
+        Encoder v;
         for (auto s : shape) {
-            proto::Encoder sv;
+            Encoder sv;
             sv.s32(1, s);
-//            sv.str(2, "size");        // by default
+//            sv.str(2, "size");       /// * by default
             v.raw(2, sv.buf());
         }
         return v.buf();
     }
-    void _attr(const STR& k, U8V buf) { // add attribute
-        proto::Encoder at;
+    void _attr(const STR& k, U8V buf) { ///< add attribute
+        Encoder at;
         at.str(1, k);
         at.raw(2, buf);
 
         _dump(at.buf(), k.c_str(), "");
         
-        raw(5, at.buf());               // NodeDef.attr
+        raw(5, at.buf());               /// * NodeDef.attr
     }
     void _dump(const U8V& buf, const char *hdr, const char *pfx="") {
         int sz = (int)buf.size();
@@ -123,7 +122,7 @@ private:
                 printf(" %02x", c);
             }
             printf("  ");
-            for (int j=0; j < 16; j++) {   // print and advance to next byte
+            for (int j=0; j < 16; j++) { /// * print and advance to next byte
                 U8 c = ((i+j) < sz ? buf.data()[i+j] : 0) & 0x7f;
                 printf("%c", (char)((c==0x7f||c<0x20) ? '_' : c));
             }
@@ -132,4 +131,4 @@ private:
     }
 };  // class Node
 
-} // namespace graph
+} // namespace t4::tb::graph
