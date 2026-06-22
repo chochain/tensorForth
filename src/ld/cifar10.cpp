3127fd7acfd2d8df6c691481206d9b272a15a3ad
gnii <chochain@yahoo.com>
2026-06-22 12:15:47 +0000

    ~/src/ld/cifar10.cpp + vector (F8V); int => U32 (shut g++)

--- src/ld/cifar10.cpp
+++ src/ld/cifar10.cpp
@@ -5,7 +5,7 @@
  * <pre>Copyright (C) 2022- GreenII, this file is distributed under BSD 3-Clause License.</pre>
  */
 #include <sstream>
-#include <string>
+#include <vector>
 #include "ten4_config.h"
 
 #if (T4_DO_OBJ && T4_DO_NN)
@@ -18,7 +18,7 @@ namespace t4::ld {
 //#define MAX_BATCH 3          /**< debug, limit number of mini-batches */
 #define MAX_BATCH 0          /**< debug, limit number of mini-batches */
 
-Corpus *Cifar10::init(int mini_bsz, bool trace) {
+Corpus *Cifar10::init(U32 mini_bsz, bool trace) {
     if (_open()) return NULL;
     data  = NULL;
     label = NULL;
@@ -26,7 +26,7 @@ Corpus *Cifar10::init(int mini_bsz, bool trace) {
     max   = 256;
     N     = mini_bsz;
 
-    int n = 0;
+    U32 n = 0;
     if (_ds) {
         _ds.seekg(0, std::ios::end);         /// * read to the end
         std::streampos fsz = _ds.tellg();    ///< file size
@@ -49,8 +49,8 @@ Corpus *Cifar10::init(int mini_bsz, bool trace) {
     return this;
 }
 
-int Cifar10::fetch(int bid, bool trace) {
-    int off = N * bid;                          ///< batch offset index
+U32 Cifar10::fetch(U32 bid, bool trace) {
+    U32 off = N * bid;                          ///< batch offset index
     if (eof || off >= corpus_sz) {              /// * beyond total sample count?
         ERROR("Cifar10::fetch EOF reached (needs rewind)\n");
         eof=1; return 0;
@@ -58,7 +58,7 @@ int Cifar10::fetch(int bid, bool trace) {
     ///
     /// fetch labels and images (and set eof if any of EOF reached)
     ///
-    int n = batch_sz = _get_data(bid, trace);   ///< load, update actual size loaded
+    U32 n = batch_sz = _get_data(bid, trace);   ///< load, update actual size loaded
     
     if ((off += n) >= corpus_sz) eof = 1;       /// * EOF reached
     ///
@@ -87,7 +87,7 @@ int Cifar10::_close() {
     return 0;
 }
 
-int Cifar10::_get_data(int bid, bool trace) {
+U32 Cifar10::_get_data(U32 bid, bool trace) {
     bool first = label==NULL;
     
     if (!label) DS_ALLOC(&label, N * LABEL_BSZ);   ///< allocate label memory block
@@ -103,28 +103,28 @@ int Cifar10::_get_data(int bid, bool trace) {
     if (cnt % SAMPLE_BSZ) {
         ERROR("Cifar10::_get_data byte read %d != multiply of %d\n", cnt, SAMPLE_BSZ);
     }
-    int n = cnt / SAMPLE_BSZ;                      ///< actual samples loaded
+    U32 n = cnt / SAMPLE_BSZ;                      ///< actual samples loaded
     
     U8 *bp = buf.data(), *tp = label, *dp = data;
-    for (int i = 0; i < n; i++) {
+    for (U32 i = 0; i < n; i++) {
         *tp++ = *bp;                               /// * read label
         bp += LABEL_BSZ;
-        for (int j = 0; j < IMAGE_HW; j++) {       /// * image
+        for (U32 j = 0; j < IMAGE_HW; j++) {       /// * image
             *dp++ = *(bp + j);                     /// * R
             *dp++ = *(bp + IMAGE_HW + j);          /// * G
             *dp++ = *(bp + IMAGE_HW * 2 + j);      /// * B
         }
         bp += IMAGE_BSZ;
     }
-    char c = _ds.peek();
+    _ds.peek();
     eof |= _ds.eof();                              /// * set EOF flag, if done
 
-    auto dump_1st = [](U8 *label, int n) {
+    auto dump_1st = [](U8 *label, U32 n) {
         const char *nm[] = {
             "plane", "car",   "bird",  "cat",   "deer",
             "dog",   "frog ", "horse", "ship ", "truck", "ERROR"
         };
-        for (int i = 0; i < n; i++) {
+        for (U32 i = 0; i < n; i++) {
             int idx = (int)label[i];
             INFO("%-5s%c", nm[idx < 10 ? idx : 10], ((i+1)%16) ? ' ' : '\n');
         }
