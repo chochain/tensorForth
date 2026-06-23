f2a67200ded787e567b0470b5b060bf790021bae
gnii <chochain@yahoo.com>
2026-06-23 11:16:33 +0000

    ~/src/module.mk MODULE_RULE

--- src/module.mk
+++ src/module.mk
@@ -3,14 +3,14 @@
 #
 # Add inputs and outputs from these tool invocations to the build variables
 #
-T4_SRCS := \
-	./src/sys.cu \
-	./src/debug.cu \
-	./src/util.cu \
-	./src/t4math.cu \
-	./src/ten4.cu
+T4_CUSRCS :=  \
+	sys.cu    \
+	debug.cu  \
+	util.cu   \
+	t4math.cu \
+	ten4.cu
 
-T4_DIRS := src src/io src/mu src/vm src/nn src/tb
+T4_DIRS :=
 T4_LIBS :=
 
-$(eval $(call MODULE_RULES,T4,src))
+$(eval $(call MODULE_RULE,T4,src))
